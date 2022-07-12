1bbacd27222552973a5ed1889fa3856a18200d8c
Daniel Haselhan <daniel@bit3.ca>
2022-07-12 14:32:27 -0700

    Clean up old config files

--- alcs-api/src/app.module.ts
+++ alcs-api/src/app.module.ts
@@ -1,19 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { getEnvPath } from './common/helper/env.helper';
 import { TypeormConfigService } from './providers/typeormconfig/typeorm.service';
 
-const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
-
 @Module({
-  // TODO add db import here
-  imports: [
-    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
-    TypeOrmModule.forRootAsync({ useClass: TypeormConfigService }),
-  ],
+  imports: [TypeOrmModule.forRootAsync({ useClass: TypeormConfigService })],
   controllers: [AppController],
   providers: [AppService],
 })
