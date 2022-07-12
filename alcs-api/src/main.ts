cc9a02d864b3bba4dec7dfc5dfde36f467616af4
Mekhti <mekhti.huseinov.ssi@gmail.com>
2022-07-12 13:49:32 -0700

    wip init api

---
+++ alcs-api/src/main.ts
@@ -0,0 +1,60 @@
+import fastifyHelmet from '@fastify/helmet';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  // fastify
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+
+  //init config
+  const config: ConfigService = app.get(ConfigService);
+
+  // config variables
+  const port: number = config.get<number>('PORT');
+
+  // cors
+  app.enableCors();
+
+  // swagger
+  const documentBuilderConfig = new DocumentBuilder()
+    .setTitle('ALCS API')
+    .setDescription('ALCS - provide explanation for ALCS')
+    .setVersion('0.1')
+    .addTag('ALCS')
+    .build();
+  const document = SwaggerModule.createDocument(app, documentBuilderConfig);
+  SwaggerModule.setup('docs', app, document);
+
+  // helmet
+  await app.register(fastifyHelmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [
+          `'self'`,
+          `'unsafe-inline'`,
+          'cdn.jsdelivr.net',
+          'fonts.googleapis.com',
+        ],
+        fontSrc: [`'self'`, 'fonts.gstatic.com'],
+        imgSrc: [`'self'`, 'data:', 'cdn.jsdelivr.net'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`, `cdn.jsdelivr.net`],
+      },
+    },
+  });
+
+  // start app n port
+  await app.listen(port, '0.0.0.0', () => {
+    console.log('[WEB]', config.get<string>('BASE_URL'));
+  });
+}
+bootstrap();
---
+++ alcs-api/src/providers/typeormconfig/typeorm.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeormConfigService implements TypeOrmOptionsFactory {
+  @Inject(ConfigService)
+  private readonly config: ConfigService;
+
+  public createTypeOrmOptions(): TypeOrmModuleOptions {
+    console.log(this.config.get<string>('DATABASE_USER'));
+    console.log(this.config.get<string>('DATABASE_PASSWORD'));
+    return {
+      type: 'postgres',
+      host: this.config.get<string>('DATABASE_HOST'),
+      port: this.config.get<number>('DATABASE_PORT'),
+      database: this.config.get<string>('DATABASE_NAME'),
+      username: this.config.get<string>('DATABASE_USER'),
+      password: this.config.get<string>('DATABASE_PASSWORD'),
+      entities: ['dist/**/*.entity.{ts,js}'],
+      migrations: ['dist/migrations/*.{ts,js}'],
+      migrationsTableName: 'typeorm_migrations',
+      logger: 'file',
+      synchronize: false,
+    };
+  }
+}
