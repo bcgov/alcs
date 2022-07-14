3ea754ec9deea4b8118dc72969340445aef614ae
Paul Dittaro <pdittaro@gmail.com>
2022-07-14 10:55:58 -0700

    Code coverage publishing (and CI first cut) (#9)
    * Code coverage GA
    
    * Split CI and coverage actions

--- alcs-frontend/karma.conf.js
+++ alcs-frontend/karma.conf.js
@@ -25,9 +25,9 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/alcs-frontend'),
+      dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }, { type: 'lcovonly' }],
+      reporters: [{ type: 'html' }, { type: 'text-summary' }, { type: 'lcovonly', projectRoot: '..' }],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
