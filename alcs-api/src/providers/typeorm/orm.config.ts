57cde1da345a9b5d1d1e309a78509141340fb9f6
Mekhti <mekhti.huseinov.ssi@gmail.com>
2022-07-20 11:59:48 -0700

    move cli datasource to separate file

--- alcs-api/src/providers/typeorm/orm.config.ts
+++ alcs-api/src/providers/typeorm/orm.config.ts
@@ -1,8 +1,6 @@
 import { IConfig } from 'config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
 import { join } from 'path';
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 export const getTypeOrmModuleOptions = (
   config: IConfig,
@@ -22,25 +20,9 @@ export const getTypeOrmModuleOptions = (
 
 export const getOrmConfig = (config: IConfig) => ({
   ...getTypeOrmModuleOptions(config),
-  migrationsTableName: 'migrations',
+  migrationsTableName: config.get<string>('DATABASE_MIGRATION_TABLE'),
   migrations: [join(__dirname, '**', 'migrations/*{.ts,.js}')],
   cli: {
     migrationsDir: [join(__dirname, '**', 'migrations')],
   },
 });
-
-export const createConnectionSource = (ormConfig: PostgresConnectionOptions) =>
-  new DataSource({
-    migrationsTableName: ormConfig.migrationsTableName,
-    type: ormConfig.type,
-    host: ormConfig.host,
-    port: ormConfig.port,
-    username: ormConfig.username,
-    password: ormConfig.password,
-    database: ormConfig.database,
-    logging: ormConfig.logging,
-    synchronize: ormConfig.synchronize,
-    name: 'default',
-    entities: ['src/**/*.entity.{ts,js}'], // note, this must point to entities folder in src, so cli can discover entities for migration generation
-    migrations: ormConfig.migrations,
-  });
