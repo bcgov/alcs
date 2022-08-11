f19daa34b92ff2cf2da2f76dbd0faf76241b2266
Daniel Haselhan <daniel@bit3.ca>
2022-08-11 12:57:48 -0700

    Add initializer flag to auth service
    * Auth guard checks to make sure auth service is initialized, if not initialize it

--- alcs-frontend/src/app/services/authentication/auth.guard.ts
+++ alcs-frontend/src/app/services/authentication/auth.guard.ts
@@ -9,6 +9,9 @@ export class AuthGuard implements CanActivate {
   constructor(private authenticationService: AuthenticationService, private router: Router) {}
 
   async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
+    if (!this.authenticationService.isInitialized) {
+      await this.authenticationService.loadTokenFromStorage();
+    }
     const token = await this.authenticationService.getToken();
     if (token) {
       return true;
