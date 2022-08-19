ed173ff5bedecb532d49e43acdfdca7fe15b78fd
Mekhti <mekhti.huseinov.ssi@gmail.com>
2022-08-19 10:28:56 -0700

    UI tests fix

--- alcs-frontend/src/app/shared/mention-textarea/mention-textarea.component.spec.ts
+++ alcs-frontend/src/app/shared/mention-textarea/mention-textarea.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { MentionTextareaComponent } from './mention-textarea.component';
 
@@ -10,7 +11,7 @@ describe('MentionTextareaComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MentionTextareaComponent],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, MatSnackBarModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MentionTextareaComponent);
