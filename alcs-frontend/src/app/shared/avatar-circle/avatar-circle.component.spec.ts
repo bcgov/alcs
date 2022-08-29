4c3db1930dc775220613b00978ceebf59e494050
Daniel Haselhan <daniel@bit3.ca>
2022-08-29 11:12:38 -0700

    Add Avatar Menu
    * Add avatar dropdown menu to header
    * Move avatar circle to its own component
    * Change typography to only bold flat and stroke buttons

---
+++ alcs-frontend/src/app/shared/avatar-circle/avatar-circle.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AvatarCircleComponent } from './avatar-circle.component';
+
+describe('AvatarCircleComponent', () => {
+  let component: AvatarCircleComponent;
+  let fixture: ComponentFixture<AvatarCircleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AvatarCircleComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AvatarCircleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
