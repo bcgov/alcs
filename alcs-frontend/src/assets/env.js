2b220342946d6ad0ef323aaa324868d3331bd926
Paul Dittaro <pdittaro@gmail.com>
2022-08-29 09:23:12 -0700

    Non-injectable service version of env load

--- alcs-frontend/src/assets/env.js
+++ alcs-frontend/src/assets/env.js
@@ -2,6 +2,6 @@
     window["env"] = window["env"] || {};
   
     // Environment variables
-    window["env"]["apiRoot"] = "http://localhost:8080";
+    window["env"]["apiUrl"] = "http://localhost:8080";
     window["env"]["homeUrl"] = "/home";
   })(this);
--- alcs-frontend/src/assets/env.template.js
+++ alcs-frontend/src/assets/env.template.js
@@ -2,6 +2,6 @@
     window["env"] = window["env"] || {};
   
     // Environment variables
-    window["env"]["apiRoot"] = "${API_ROOT}";
+    window["env"]["apiUrl"] = "${API_URL}";
     window["env"]["homeUrl"] = "/home";
   })(this);
