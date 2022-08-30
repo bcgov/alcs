08aaa6e1aad0493193666592bec0df887b1b0a87
Mekhti <mekhti.huseinov.ssi@gmail.com>
2022-08-30 16:38:09 -0700

    UI unit tests

--- alcs-frontend/src/app/features/application/application-meeting/application-meeting-dialog/application-meeting-dialog.component.spec.ts
+++ alcs-frontend/src/app/features/application/application-meeting/application-meeting-dialog/application-meeting-dialog.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ApplicationDecisionMeetingService } from '../../../../services/application/application-decision-meeting/application-decision-meeting.service';
 
 import { ApplicationMeetingDialogComponent } from './application-meeting-dialog.component';
 
@@ -9,6 +14,15 @@ describe('ApplicationMeetingDialogComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ApplicationMeetingDialogComponent],
+      providers: [
+        {
+          provide: ApplicationDecisionMeetingService,
+          useValue: {},
+        },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} },
+      ],
+      imports: [HttpClientTestingModule, MatDialogModule, MatSnackBarModule, FormsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ApplicationMeetingDialogComponent);
--- alcs-frontend/src/app/features/application/application-meeting/application-meeting.component.spec.ts
+++ alcs-frontend/src/app/features/application/application-meeting/application-meeting.component.spec.ts
@@ -1,14 +1,38 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BehaviorSubject } from 'rxjs';
+import { ApplicationDetailService } from '../../../services/application/application-detail.service';
+import { ApplicationDetailedDto } from '../../../services/application/application.dto';
+import { ConfirmationDialogService } from '../../../shared/confirmation-dialog/confirmation-dialog.service';
 
 import { ApplicationMeetingComponent } from './application-meeting.component';
 
 describe('ApplicationMeetingComponent', () => {
   let component: ApplicationMeetingComponent;
   let fixture: ComponentFixture<ApplicationMeetingComponent>;
+  const mockAppDetailService = jasmine.createSpyObj<ApplicationDetailService>('ApplicationDetailService', [
+    'loadApplication',
+  ]);
+  mockAppDetailService.$application = new BehaviorSubject<ApplicationDetailedDto | undefined>(undefined);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ApplicationMeetingComponent],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {},
+        },
+        {
+          provide: MatDialog,
+          useValue: {},
+        },
+        { provide: ApplicationDetailService, useValue: mockAppDetailService },
+        { provide: ConfirmationDialogService, useValue: {} },
+      ],
+      imports: [HttpClientTestingModule, MatSnackBarModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ApplicationMeetingComponent);
