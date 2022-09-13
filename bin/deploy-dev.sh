79f6b1dbab47b2d8c8cdd4574183538b4d1bd58b
Paul Dittaro <pdittaro@gmail.com>
2022-09-13 13:43:03 -0700

    Change deploy script

--- bin/deploy-dev.sh
+++ bin/deploy-dev.sh
@@ -11,8 +11,12 @@ docker build -t alcs-migrate:latest -f alcs-api/Dockerfile.migrate alcs-api
 docker tag alcs-migrate:latest image-registry.apps.silver.devops.gov.bc.ca/a5cf88-dev/alcs-migrate:latest
 docker push image-registry.apps.silver.devops.gov.bc.ca/a5cf88-dev/alcs-migrate:latest
 
+oc patch deployment/alcs-api -n a5cf88-dev --patch \
+  "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"last-restart\":\"`date +'%s'`\"}}}}}"
+
 docker build -t alcs-web:latest alcs-frontend 
 docker tag alcs-web:latest image-registry.apps.silver.devops.gov.bc.ca/a5cf88-dev/alcs-web:latest
 docker push image-registry.apps.silver.devops.gov.bc.ca/a5cf88-dev/alcs-web:latest
 oc patch deployment/alcs-web -n a5cf88-dev --patch \
   "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"last-restart\":\"`date +'%s'`\"}}}}}"
+
