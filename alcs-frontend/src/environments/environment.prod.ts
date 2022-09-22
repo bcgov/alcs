18bd787a702a372291065ef00879897b8dcc8da6
Daniel Haselhan <daniel@bit3.ca>
2022-09-22 13:41:12 -0700

    Bug Fixes Pt2
    * Remove line from timeline if only a single entry
    * Add Other: to radio input
    * Move info request to 3rd menu position
    * Fix date format in prod env

--- alcs-frontend/src/environments/environment.prod.ts
+++ alcs-frontend/src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   production: true,
   apiUrl: window.env?.['apiUrl'] || 'http://localhost:8080',
   homeUrl: window.env?.['homeUrl'] || '/home',
-  dateFormat: 'YYYY-MMM-dd',
+  dateFormat: 'YYYY-MMM-DD',
   shortTimeFormat: 'MMM D, h:mm a',
   longTimeFormat: 'YYYY-MMM-DD hh:mm:ss a',
   maxFileSize: 104857600, //should match setting in backend
