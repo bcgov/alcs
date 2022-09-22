55542f4227f32e9463e0a9863f8f7711fd4d646d
Daniel Haselhan <daniel@bit3.ca>
2022-09-22 16:49:26 -0700

    Add missing date formats
    * This resulted in full timestamp on the datepicker

--- alcs-frontend/src/app/shared/utils/date-format.ts
+++ alcs-frontend/src/app/shared/utils/date-format.ts
@@ -2,12 +2,14 @@ import { environment } from '../../../environments/environment';
 
 export const DATE_FORMATS = {
   parse: {
-    dateInput: 'LL',
+    dateInput: 'DD.MM.YYYY',
   },
   display: {
     dateInput: environment.dateFormat,
     monthYearLabel: 'YYYY',
     dateA11yLabel: 'LL',
     monthYearA11yLabel: 'YYYY',
+    monthLabel: 'MMM',
+    monthDayLabel: 'MMM-DD',
   },
 };
