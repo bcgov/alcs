0b2a6ef789c81c7e5008137d166a4ef75e5c4aed
Mekhti <mekhti.huseinov.ssi@gmail.com>
2022-11-03 15:57:46 -0700

    UI test update after merge

--- alcs-frontend/src/app/features/home/audit/audit-subtasks.component.spec.ts
+++ alcs-frontend/src/app/features/home/audit/audit-subtasks.component.spec.ts
@@ -1,8 +1,10 @@
 import { EventEmitter } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BehaviorSubject } from 'rxjs';
 import { CardSubtaskService } from '../../../services/card/card-subtask/card-subtask.service';
 import { HomeService } from '../../../services/home/home.service';
+import { AssigneeDto } from '../../../services/user/user.dto';
 import { UserService } from '../../../services/user/user.service';
 
 import { AuditSubtasksComponent } from './audit-subtasks.component';
@@ -10,8 +12,12 @@ import { AuditSubtasksComponent } from './audit-subtasks.component';
 describe('AuditComponent', () => {
   let component: AuditSubtasksComponent;
   let fixture: ComponentFixture<AuditSubtasksComponent>;
+  let mockUserService;
 
   beforeEach(async () => {
+    mockUserService = jasmine.createSpyObj<UserService>('UserService', ['fetchAssignableUsers']);
+    mockUserService.$assignableUsers = new BehaviorSubject<AssigneeDto[]>([]);
+
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       providers: [
@@ -27,10 +33,7 @@ describe('AuditComponent', () => {
         },
         {
           provide: UserService,
-          useValue: {
-            $users: new EventEmitter(),
-            fetchUsers: jasmine.createSpy(),
-          },
+          useValue: mockUserService,
         },
         { provide: HomeService, useValue: {} },
       ],
