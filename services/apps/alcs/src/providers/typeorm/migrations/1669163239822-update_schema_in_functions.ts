74470e4d100a4aec5f66d7bf0db28f92475d3d3f
Mekhti <mekhti.huseinov.ssi@gmail.com>
2022-11-23 10:29:57 -0800

    update migrations

--- services/apps/alcs/src/providers/typeorm/migrations/1669163239822-update_schema_in_functions.ts
+++ services/apps/alcs/src/providers/typeorm/migrations/1669163239822-update_schema_in_functions.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class updateShcemaInFunctions1669163239822
+export class updateSchemaInFunctions1669163239822
   implements MigrationInterface
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
@@ -147,5 +147,7 @@ export class updateShcemaInFunctions1669163239822
     `);
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // nope
+  }
 }
