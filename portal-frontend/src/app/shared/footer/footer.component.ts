977626db4794804bd9986d2088f9ad06d45742a4
Daniel Haselhan <daniel@bit3.ca>
2022-11-24 09:05:35 -0800

    Code Review Feedback

--- portal-frontend/src/app/shared/footer/footer.component.ts
+++ portal-frontend/src/app/shared/footer/footer.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  year = 0;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.year = new Date().getFullYear();
   }
-
 }
