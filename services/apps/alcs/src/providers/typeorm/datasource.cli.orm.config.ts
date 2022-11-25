20ce447cb503a86b569fc43c2775281a32979785
Mekhti <mekhti.huseinov.ssi@gmail.com>
2022-11-25 12:03:28 -0800

    alcs schema in typeorm cli

--- services/apps/alcs/src/providers/typeorm/datasource.cli.orm.config.ts
+++ services/apps/alcs/src/providers/typeorm/datasource.cli.orm.config.ts
@@ -2,7 +2,7 @@ import * as config from 'config';
 import { join } from 'path';
 import { DataSource } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
-// import { ALCS_DATABASE_SCHEMA } from './orm.config';
+import { ALCS_DATABASE_SCHEMA } from './orm.config';
 
 // this is a typeorm cli specific configuration
 export const connectionSource = new DataSource({
@@ -13,9 +13,7 @@ export const connectionSource = new DataSource({
   username: config.get<string>('DATABASE.USER'),
   password: config.get<string>('DATABASE.PASSWORD'),
   database: config.get<string>('DATABASE.NAME'),
-  // TODO: before the first deployment schema must be set to 'public' once first deployment completes it needs to change to line bellow
-  // schema: ALCS_DATABASE_SCHEMA
-  schema: 'public',
+  schema: ALCS_DATABASE_SCHEMA,
   synchronize: false,
   name: 'default',
   entities: ['apps/alcs/src/**/*.entity.{ts,js}'], // note, this must point to entities folder in src, so cli can discover entities for migration generation
