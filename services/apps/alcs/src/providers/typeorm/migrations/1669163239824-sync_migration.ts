3c6f78e7c9a3e49eb0bb2e2d94c1776df7d80f60
Mekhti <mekhti.huseinov.ssi@gmail.com>
2022-11-25 15:23:24 -0800

    update sync migration

--- services/apps/alcs/src/providers/typeorm/migrations/1669163239824-sync_migration.ts
+++ services/apps/alcs/src/providers/typeorm/migrations/1669163239824-sync_migration.ts
@@ -4,7 +4,7 @@ export class syncMigration1669154474301 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `
-      INSERT INTO public.migrations ("timestamp","name") VALUES (1669151433055,'renameSchema1669151433055');
+      INSERT INTO "alcs".migrations ("timestamp","name") VALUES (1669151433055,'renameSchema1669151433055');
       INSERT INTO "alcs"."migrations" ("timestamp","name") VALUES (1669163239822,'updateSchemaInFunctions1669163239822');
       INSERT INTO "alcs".migrations ("timestamp","name") VALUES (1669154474301,'syncMigration1669154474301');
       `,
