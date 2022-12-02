47a46bf6bb34716878efa6754b9cae7ebb13a90a
Paul Dittaro <pdittaro@gmail.com>
2022-12-02 09:02:00 -0800

    Fix migrate image

--- services/migrate.sh
+++ services/migrate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 env
 echo "Beginning migration..."
-node --require ts-node/register ./node_modules/typeorm/cli.js migration:run -d ./src/providers/typeorm/datasource.cli.orm.config.ts
+node --require ts-node/register ./node_modules/typeorm/cli.js migration:run -d ./apps/${NEST_APP}/src/providers/typeorm/datasource.cli.orm.config.ts
