160f4ff8009adec95a97ae822f5a68e6f717d0a0
Mekhti <mekhti.huseinov.ssi@gmail.com>
2022-12-06 13:29:58 -0800

    added certificates

---
+++ services/apps/alcs/src/providers/grpc/grpc.options.config.ts
@@ -0,0 +1,37 @@
+import { ServerCredentials } from '@grpc/grpc-js';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
+import * as config from 'config';
+import * as fs from 'fs';
+import { addReflectionToGrpcConfig } from 'nestjs-grpc-reflection';
+import { join } from 'path';
+
+const key = fs.readFileSync(
+  join(config.get('GRPC.CERT_FOLDER'), config.get('GRPC.KEY')),
+);
+
+const cert = fs.readFileSync(
+  join(config.get('GRPC.CERT_FOLDER'), config.get('GRPC.CERT')),
+);
+
+const root = fs.readFileSync(
+  join(config.get('GRPC.CERT_FOLDER'), config.get('GRPC.ROOT_CERT')),
+);
+
+export const grpcOptions: GrpcOptions = addReflectionToGrpcConfig({
+  transport: Transport.GRPC,
+  options: {
+    package: 'alcs_application',
+    url: config.get('GRPC.URL'),
+    credentials: ServerCredentials.createSsl(
+      root,
+      [
+        {
+          cert_chain: cert,
+          private_key: key,
+        },
+      ],
+      true,
+    ),
+    protoPath: config.get('GRPC.PROTO'),
+  },
+});
