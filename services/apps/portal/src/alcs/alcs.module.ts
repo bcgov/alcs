b5013b911870a141091a4bf8186dc328a5622b9b
Mekhti <mekhti.huseinov.ssi@gmail.com>
2022-12-07 10:42:02 -0800

    portal client grpc

--- services/apps/portal/src/alcs/alcs.module.ts
+++ services/apps/portal/src/alcs/alcs.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ApplicationGrpcModule } from './application-grpc/application-grpc.module';
 import { CodeController } from './code/code.controller';
 import { DocumentService } from './document/document.service';
 import { LocalGovernmentService } from './local-government/local-government.service';
 
 @Module({
-  imports: [],
+  imports: [ApplicationGrpcModule],
   providers: [DocumentService, LocalGovernmentService],
   controllers: [CodeController],
   exports: [DocumentService, LocalGovernmentService],
---
+++ services/apps/portal/src/providers/grpc/grpc.packages.ts
@@ -0,0 +1,3 @@
+import { ALCS_APPLICATION_PACKAGE_NAME } from '../../alcs/application-grpc/alcs-application.message.interface';
+
+export const grpcPackagesNames: string[] = [ALCS_APPLICATION_PACKAGE_NAME];
