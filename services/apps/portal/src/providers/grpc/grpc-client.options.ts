a47a424a74e7727263260b3178b94bdf90a17953
Mekhti <mekhti.huseinov.ssi@gmail.com>
2022-12-07 14:51:07 -0800

    disable grpc on portal

--- services/apps/portal/src/providers/grpc/grpc-client.options.ts
+++ services/apps/portal/src/providers/grpc/grpc-client.options.ts
@@ -1,28 +1,31 @@
-import * as grpc from '@grpc/grpc-js';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
-import * as config from 'config';
-import * as fs from 'fs';
-import { join } from 'path';
-import { grpcPackagesNames } from './grpc.packages';
 
-const key = fs.readFileSync(
-  join(config.get('GRPC.CERT_FOLDER'), config.get('GRPC.KEY')),
-);
+// const key = fs.readFileSync(
+//   join(config.get('GRPC.CERT_FOLDER'), config.get('GRPC.KEY')),
+// );
 
-const cert = fs.readFileSync(
-  join(config.get('GRPC.CERT_FOLDER'), config.get('GRPC.CERT')),
-);
+// const cert = fs.readFileSync(
+//   join(config.get('GRPC.CERT_FOLDER'), config.get('GRPC.CERT')),
+// );
 
-const root = fs.readFileSync(
-  join(config.get('GRPC.CERT_FOLDER'), config.get('GRPC.ROOT_CERT')),
-);
+// const root = fs.readFileSync(
+//   join(config.get('GRPC.CERT_FOLDER'), config.get('GRPC.ROOT_CERT')),
+// );
+
+// export const grpcClientOptions: GrpcOptions = {
+//   transport: Transport.GRPC,
+//   options: {
+//     package: grpcPackagesNames,
+//     url: config.get('GRPC.URL'),
+//     credentials: grpc.credentials.createSsl(root, key, cert),
+//     protoPath: config.get('GRPC.PROTO'),
+//   },
+// };
 
 export const grpcClientOptions: GrpcOptions = {
   transport: Transport.GRPC,
   options: {
-    package: grpcPackagesNames,
-    url: config.get('GRPC.URL'),
-    credentials: grpc.credentials.createSsl(root, key, cert),
-    protoPath: config.get('GRPC.PROTO'),
+    package: 'mock',
+    protoPath: '',
   },
 };
