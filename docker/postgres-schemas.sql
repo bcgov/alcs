fd6084dc2bebb0f4f62bf3f9dcaa4916cb29830c
Paul Dittaro <pdittaro@gmail.com>
2022-12-07 13:04:53 -0800

    Add portal health check

--- docker/postgres-schemas.sql
+++ docker/postgres-schemas.sql
@@ -1,3 +1,3 @@
 -- Seed file to create ALCS and Portal schemas on docker database startup
 --CREATE SCHEMA IF NOT EXISTS alcs;
---CREATE SCHEMA IF NOT EXISTS portal;
+CREATE SCHEMA IF NOT EXISTS portal;
