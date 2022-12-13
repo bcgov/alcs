321ff1b27d8ae5b45e8a5e0e7756a48560b2d0f7
Mekhti <mekhti.huseinov.ssi@gmail.com>
2022-12-13 13:53:44 -0800

    fix UI unit tests

--- alcs-frontend/src/app/features/application/applicant-info/applicant-info.component.spec.ts
+++ alcs-frontend/src/app/features/application/applicant-info/applicant-info.component.spec.ts
@@ -1,16 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { BehaviorSubject } from 'rxjs';
+import { ApplicationDetailService } from '../../../services/application/application-detail.service';
+import { ApplicationDto } from '../../../services/application/application.dto';
 
 import { ApplicantInfoComponent } from './applicant-info.component';
 
 describe('ApplicantInfoComponent', () => {
   let component: ApplicantInfoComponent;
   let fixture: ComponentFixture<ApplicantInfoComponent>;
+  let mockAppDetailService: DeepMocked<ApplicationDetailService>;
 
   beforeEach(async () => {
+    mockAppDetailService = createMock();
+    mockAppDetailService.$application = new BehaviorSubject<ApplicationDto | undefined>(undefined);
+
     await TestBed.configureTestingModule({
-      declarations: [ ApplicantInfoComponent ]
-    })
-    .compileComponents();
+      declarations: [ApplicantInfoComponent],
+      providers: [{ provide: ApplicationDetailService, useValue: mockAppDetailService }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ApplicantInfoComponent);
     component = fixture.componentInstance;
