214963179f2b11797488dab84c000e9728c410e6
Mekhti <mekhti.huseinov.ssi@gmail.com>
2022-12-19 16:11:32 -0800

    just random change to trigger codeclimate

--- services/apps/portal/src/portal.controller.ts
+++ services/apps/portal/src/portal.controller.ts
@@ -1,22 +1,22 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from './common/authorization/auth-guard.service';
-import { PortalService } from './portal.service';
-import { HealthCheckDto } from './healthcheck/healthcheck.dto';
 import { Public } from 'nest-keycloak-connect';
+import { AuthGuard } from './common/authorization/auth-guard.service';
+import { HealthCheckDto } from './healthcheck/healthcheck.dto';
+import { PortalService } from './portal.service';
 
 @Controller()
 export class PortalController {
   constructor(private readonly portalService: PortalService) {}
 
-  @Get()
-  @Public()
-  async getHealthStatus(): Promise<HealthCheckDto> {
-    return await this.portalService.getHealthStatus();
-  }
-
   @Get('/token')
   @UseGuards(AuthGuard)
   checkTokenValid() {
     return 'Token Valid';
+  }
+
+  @Get()
+  @Public()
+  async getHealthStatus(): Promise<HealthCheckDto> {
+    return await this.portalService.getHealthStatus();
   }
 }
