18789344a3a9fc2053105614259a04b02650198c
Mekhti <mekhti.huseinov.ssi@gmail.com>
2023-01-10 13:02:16 -0800

    UI unit tests

--- portal-frontend/src/app/features/edit-application/change-application-type-dialog/change-application-type-dialog.component.spec.ts
+++ portal-frontend/src/app/features/edit-application/change-application-type-dialog/change-application-type-dialog.component.spec.ts
@@ -1,5 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatRadioModule } from '@angular/material/radio';
+import { ApplicationService } from '../../../services/application/application.service';
+import { CodeService } from '../../../services/code/code.service';
 import { ChangeApplicationTypeDialogComponent } from './change-application-type-dialog.component';
 
 describe('ChangeApplicationTypeDialogComponent', () => {
@@ -8,9 +12,21 @@ describe('ChangeApplicationTypeDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ChangeApplicationTypeDialogComponent ]
-    })
-    .compileComponents();
+      imports: [MatDialogModule, MatRadioModule],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        {
+          provide: ApplicationService,
+          useValue: {},
+        },
+        {
+          provide: CodeService,
+          useValue: {},
+        },
+        { provide: MAT_DIALOG_DATA, useValue: { fileId: 'fake' } },
+      ],
+      declarations: [ChangeApplicationTypeDialogComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ChangeApplicationTypeDialogComponent);
     component = fixture.componentInstance;
