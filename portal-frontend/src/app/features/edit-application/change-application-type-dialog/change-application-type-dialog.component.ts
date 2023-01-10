ecf7004cbcfb74dc02863806560cd1acb6d7d013
Mekhti <mekhti.huseinov.ssi@gmail.com>
2023-01-09 16:17:26 -0800

    wip change app type

--- portal-frontend/src/app/features/edit-application/change-application-type-dialog/change-application-type-dialog.component.ts
+++ portal-frontend/src/app/features/edit-application/change-application-type-dialog/change-application-type-dialog.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatRadioChange } from '@angular/material/radio';
+import { ApplicationService } from '../../../services/application/application.service';
+import { ApplicationTypeDto } from '../../../services/code/code.dto';
+import { CodeService } from '../../../services/code/code.service';
 
 @Component({
   selector: 'app-change-application-type-dialog',
@@ -7,9 +11,25 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./change-application-type-dialog.component.scss'],
 })
 export class ChangeApplicationTypeDialogComponent implements OnInit {
-  constructor(private dialogRef: MatDialogRef<ChangeApplicationTypeDialogComponent>) {}
+  applicationTypes: ApplicationTypeDto[] = [];
+  selectedAppType: ApplicationTypeDto | undefined = undefined;
 
-  ngOnInit(): void {}
+  readMoreClicked: boolean = false;
+
+  constructor(
+    private dialogRef: MatDialogRef<ChangeApplicationTypeDialogComponent>,
+    private applicationService: ApplicationService,
+    private codeService: CodeService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadCodes();
+  }
+
+  private async loadCodes() {
+    const codes = await this.codeService.loadCodes();
+    this.applicationTypes = codes.applicationTypes.filter((type) => !!type.portalLabel);
+  }
 
   async onCancel() {
     this.dialogRef.close();
@@ -18,5 +38,23 @@ export class ChangeApplicationTypeDialogComponent implements OnInit {
   async onSubmit() {
     console.log('submit');
     this.onCancel();
+  }
+
+  async onAppTypeSelected(event: MatRadioChange) {
+    this.selectedAppType = this.applicationTypes.find((e) => e.code === event.value);
+    this.readMoreClicked = false;
+  }
+
+  isEllipsisActive(e: string): boolean {
+    const el = document.getElementById(e);
+    return el ? el.clientHeight < el.scrollHeight : false;
+  }
+
+  isReadMoreVisible(): boolean {
+    return this.readMoreClicked || this.isEllipsisActive('appTypeDescription');
+  }
+
+  onReadMoreClicked() {
+    this.readMoreClicked = !this.readMoreClicked;
   }
 }
