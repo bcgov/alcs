6c1a842e0726b36c638e0bbec431228bceb8f49d
mhuseinov <61513701+mhuseinov@users.noreply.github.com>
2023-01-13 15:05:21 -0800

    Feature/alcs 336 csp connect src (#314)
    * wip
    
    * added ENABLED_CONNECT_SRC
    
    * csp content-src
    
    * typo
    
    * missing init.sh in portal
    
    * Change to envsubst for consistency, allow write permission
    
    * Tmp typo
    
    Co-authored-by: Paul Dittaro <pdittaro@gmail.com>

--- alcs-frontend/init.sh
+++ alcs-frontend/init.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 echo 'Replace connect-src with' $ENABLED_CONNECT_SRC
-sed -ri 's@connect-src \*@connect-src '"$ENABLED_CONNECT_SRC"'@' /etc/nginx/nginx.conf
+envsubst < /etc/nginx/nginx.conf > /tmp/nginx.conf && mv /tmp/nginx.conf /etc/nginx/nginx.conf
 
 # When the container starts, replace the settings.json with values from environment variables
-envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js && exec nginx -g 'daemon off;'
+envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
+
+exec nginx -g 'daemon off;'
