8ed35ccc7a4076227ad832ca7879e9dbef16f6fe
Paul Dittaro <pdittaro@gmail.com>
2023-01-13 14:56:46 -0800

    Tmp typo

--- portal-frontend/init.sh
+++ portal-frontend/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo 'Replace connect-src with' $ENABLED_CONNECT_SRC
-envsubst < /etc/nginx/nginx.conf > /tmp/nginx.conf/tmp.conf && mv tmp/nginx.conf /etc/nginx/nginx.conf
+envsubst < /etc/nginx/nginx.conf > /tmp/nginx.conf && mv /tmp/nginx.conf /etc/nginx/nginx.conf
 
 # When the container starts, replace the settings.json with values from environment variables
 envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
