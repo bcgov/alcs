eb080491ab185a8e61111d2b6da560b8638bc40e
Mekhti <mekhti.huseinov.ssi@gmail.com>
2023-01-13 13:15:50 -0800

    added ENABLED_CONNECT_SRC

--- alcs-frontend/init.sh
+++ alcs-frontend/init.sh
@@ -2,7 +2,7 @@
 
 echo 'Replace connect-src with' $ENABLED_CONNECT_SRC
 # TODO adjust sed
-sed -ri 's/connect-src \*/'$ENABLED_CONNECT_SRC'/' /etc/nginx/nginx.conf
+sed -ri 's@connect-src \*@connect-src '"$ENABLED_CONNECT_SRC"'@' /etc/nginx/nginx.conf
 
 # When the container starts, replace the settings.json with values from environment variables
 envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js && exec nginx -g 'daemon off;'
