e14594ede1d3ecca4f6da49d3db11ab3e914418e
Daniel Haselhan <daniel@bit3.ca>
2023-01-13 15:09:26 -0800

    Move Review Application to its own module

--- portal-frontend/src/app/app.module.ts
+++ portal-frontend/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { MatStepperModule } from '@angular/material/stepper';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthorizationComponent } from './features/authorization/authorization.component';
@@ -19,25 +17,16 @@ import { ApplicationListComponent } from './features/home/application-list/appli
 import { HomeComponent } from './features/home/home.component';
 import { LandingPageComponent } from './features/landing-page/landing-page.component';
 import { LoginComponent } from './features/login/login.component';
+import { ViewApplicationComponent } from './features/view-application/view-application.component';
 import { AuthInterceptorService } from './services/authentication/auth-interceptor.service';
 import { TokenRefreshService } from './services/authentication/token-refresh.service';
 import { ConfirmationDialogComponent } from './shared/confirmation-dialog/confirmation-dialog.component';
 import { ConfirmationDialogService } from './shared/confirmation-dialog/confirmation-dialog.service';
 import { DragDropDirective } from './shared/file-drag-drop/drag-drop.directive';
-import { FileDragDropComponent } from './shared/file-drag-drop/file-drag-drop.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FileSizePipe } from './shared/pipes/fileSize.pipe';
 import { SharedModule } from './shared/shared.module';
-import { ViewApplicationComponent } from './features/view-application/view-application.component';
-import { ReviewApplicationComponent } from './features/review-application/review-application.component';
-import { ReviewContactInformationComponent } from './features/review-application/review-contact-information/review-contact-information.component';
-import { ReviewOcpComponent } from './features/review-application/review-ocp/review-ocp.component';
-import { ReviewZoningComponent } from './features/review-application/review-zoning/review-zoning.component';
-import { ReviewResolutionComponent } from './features/review-application/review-resolution/review-resolution.component';
-import { ReviewAttachmentsComponent } from './features/review-application/review-attachments/review-attachments.component';
-import { ReviewSubmitComponent } from './features/review-application/review-submit/review-submit.component';
-import { NoDataComponent } from './shared/no-data/no-data.component';
 
 @NgModule({
   declarations: [
@@ -48,37 +37,24 @@ import { NoDataComponent } from './shared/no-data/no-data.component';
     HomeComponent,
     AuthorizationComponent,
     EditApplicationComponent,
-    FileDragDropComponent,
     DragDropDirective,
     ApplicationListComponent,
     CreateApplicationDialogComponent,
     ChangeApplicationTypeDialogComponent,
     LandingPageComponent,
     ParcelEntryComponent,
-    FileSizePipe,
     ConfirmationDialogComponent,
     ViewApplicationComponent,
-    ReviewApplicationComponent,
-    ReviewContactInformationComponent,
-    ReviewOcpComponent,
-    ReviewZoningComponent,
-    ReviewResolutionComponent,
-    ReviewAttachmentsComponent,
-    ReviewSubmitComponent,
-    NoDataComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     SharedModule,
-    MatStepperModule,
     MatAutocompleteModule,
     MatSortModule,
     MatPaginatorModule,
-    SharedModule,
     MatToolbarModule,
-    MatButtonToggleModule,
-    MatExpansionModule,
   ],
   providers: [
     ConfirmationDialogService,
--- portal-frontend/src/app/shared/shared.module.ts
+++ portal-frontend/src/app/shared/shared.module.ts
@@ -3,8 +3,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MAT_DATE_FORMATS } from '@angular/material/core';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -12,14 +14,17 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FileDragDropComponent } from './file-drag-drop/file-drag-drop.component';
+import { FileSizePipe } from './pipes/fileSize.pipe';
 import { DATE_FORMATS } from './utils/date-format';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   exports: [
     CommonModule,
     FormsModule,
@@ -35,10 +40,15 @@ import { DATE_FORMATS } from './utils/date-format';
     MatTableModule,
     MatProgressSpinnerModule,
     MatTooltipModule,
-    BrowserAnimationsModule,
     MatRadioModule,
     MatTabsModule,
+    MatButtonToggleModule,
+    MatExpansionModule,
+    MatStepperModule,
+    FileDragDropComponent,
+    FileSizePipe,
   ],
+  declarations: [FileDragDropComponent, FileSizePipe],
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders<SharedModule> {
