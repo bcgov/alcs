9aaf966ff5470b1353b0bb01a00969f1cf50cb7f
Paul Dittaro <pdittaro@gmail.com>
2023-01-13 16:18:38 -0800

    Allow nginx variables

--- alcs-frontend/init.sh
+++ alcs-frontend/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo 'Replace connect-src with' $ENABLED_CONNECT_SRC
-envsubst < /etc/nginx/nginx.conf > /tmp/nginx.conf && cp /tmp/nginx.conf /etc/nginx/nginx.conf
+echo 'Replace nginx environment variables'
+envsubst "`printf '${%s} ' $(awk 'BEGIN{for(v in ENVIRON) print v}')`" < /etc/nginx/nginx.conf > /tmp/nginx.conf && cp /tmp/nginx.conf /etc/nginx/nginx.conf
 
 # When the container starts, replace the settings.json with values from environment variables
 envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
