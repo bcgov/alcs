fb816814ce0a55c802010a457e8faa402177292d
Mekhti <mekhti.huseinov.ssi@gmail.com>
2023-02-10 15:57:27 -0800

    remove pad

--- services/apps/portal/src/parcel/parcel.controller.ts
+++ services/apps/portal/src/parcel/parcel.controller.ts
@@ -17,7 +17,7 @@ export class ParcelController {
     if (lookupResult) {
       return {
         pin: lookupResult.pin,
-        pid: lookupResult.pid?.padStart(9, '0'),
+        pid: lookupResult.pid,
         legalDescription: lookupResult.legalDescription,
         mapArea: Number.parseFloat(lookupResult.gisAreaHa).toFixed(2),
       };
