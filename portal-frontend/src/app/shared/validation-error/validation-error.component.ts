834bf52d428eaf2d93c20f2a1f5877ffd2895c99
Mekhti <mekhti.huseinov.ssi@gmail.com>
2023-03-03 12:44:00 -0800

    validation error on certificate of title upload

--- portal-frontend/src/app/shared/validation-error/validation-error.component.ts
+++ portal-frontend/src/app/shared/validation-error/validation-error.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-validation-error',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./validation-error.component.scss'],
 })
 export class ValidationErrorComponent {
-  constructor() {}
+  @Input() errorClass = 'subtext error';
 }
