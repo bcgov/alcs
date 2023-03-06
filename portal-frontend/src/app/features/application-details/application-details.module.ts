9e614210586d492ddc663f5c2f3a8b1e89af01fe
Daniel Haselhan <daniel@bit3.ca>
2023-03-06 09:21:42 -0800

    Show validation error and format PID
    * Show error if pid is not correct length
    * Add ngxMask for formatting

--- portal-frontend/src/app/features/application-details/application-details.module.ts
+++ portal-frontend/src/app/features/application-details/application-details.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NgxMaskPipe } from 'ngx-mask';
 import { SharedModule } from '../../shared/shared.module';
 import { ApplicationDetailsComponent } from './application-details.component';
 import { NfuDetailsComponent } from './nfu-details/nfu-details.component';
@@ -7,7 +8,7 @@ import { ParcelComponent } from './parcel/parcel.component';
 
 @NgModule({
   declarations: [ParcelComponent, ApplicationDetailsComponent, NfuDetailsComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, NgxMaskPipe],
   exports: [ApplicationDetailsComponent],
 })
 export class ApplicationDetailsModule {}
