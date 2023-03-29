cb2a4f7c3a9d47111d951ec027387f9662e02aca
Mekhti <mekhti.huseinov.ssi@gmail.com>
2023-03-29 16:21:27 -0700

    tidy app creation

--- dev-automation/tests/portal/NFU_submission_creation.spec.ts
+++ dev-automation/tests/portal/NFU_submission_creation.spec.ts
@@ -2,7 +2,7 @@ import { test } from "@playwright/test";
 
 const baseUrl = "http://localhost:4201/login";
 const userName = "MekhtiHuseinov";
-const password = "";
+const password = "UQ-ZCDHi.FiF6c";
 const filePathToUseAsUpload =
   "/Users/mekhti/Desktop/Screen Shot 2022-07-13 at 11.53.19 AM.png";
 
@@ -21,11 +21,7 @@ test("test", async ({ page }) => {
     .getByText("Application")
     .click();
   await page.getByRole("button", { name: "Next" }).click();
-  await page
-    .getByText(
-      "Transportation, Utility, or Recreational Trail Uses within the ALR"
-    )
-    .click();
+  await page.getByText("Non-Farm Uses within the ALR").click();
   await page.getByRole("button", { name: "create" }).click();
   await page
     .locator("section")
@@ -37,28 +33,22 @@ test("test", async ({ page }) => {
     .click();
   await page.getByRole("button", { name: "Fee Simple" }).click();
   await page.getByPlaceholder("Enter legal description").click();
-  await page.getByPlaceholder("Enter legal description").fill("1");
   await page
-    .locator(
-      "div:nth-child(3) > .mat-mdc-form-field > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix"
-    )
-    .click();
+    .getByPlaceholder("Enter legal description")
+    .fill("some description");
+  await page.getByPlaceholder("Enter parcel size").click();
   await page.getByPlaceholder("Enter parcel size").fill("1");
   await page.getByPlaceholder("Enter PID").click();
-  await page.getByPlaceholder("Enter PID").fill("111-111-111");
+  await page.getByPlaceholder("Enter PID").fill("222-222-222");
   await page.getByRole("button", { name: "Open calendar" }).click();
   await page
     .locator("td:nth-child(3) > .mat-calendar-body-cell")
     .first()
     .click();
-  await page.getByRole("button", { name: "March 23, 2023" }).click();
+  await page.getByRole("button", { name: "March 22, 2023" }).click();
   await page.getByRole("button", { name: "Yes" }).click();
   await page.setInputFiles("input.file-input", filePathToUseAsUpload);
-  await page
-    .locator(
-      ".container > .mat-mdc-form-field > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix"
-    )
-    .click();
+  await page.getByPlaceholder("Type owner name").click();
   await page
     .getByRole("option", { name: "No owner matching search Add new owner" })
     .getByRole("button", { name: "Add new owner" })
@@ -68,37 +58,88 @@ test("test", async ({ page }) => {
       ".ng-untouched > .form-row > div:nth-child(2) > .mat-mdc-form-field > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix"
     )
     .click();
-  await page.getByPlaceholder("Enter First Name").fill("1");
+  await page.getByPlaceholder("Enter First Name").fill("test");
   await page.getByPlaceholder("Enter Last Name").click();
-  await page.getByPlaceholder("Enter Last Name").fill("1");
+  await page.getByPlaceholder("Enter Last Name").fill("user");
   await page
     .locator(
       ".mat-mdc-dialog-content > form > .form-row > div:nth-child(4) > .mat-mdc-form-field > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix"
     )
     .click();
-  await page.getByPlaceholder("(555) 555-5555").fill("(111) 111-11111");
+  await page.getByPlaceholder("(555) 555-5555").fill("(444) 444-44444");
   await page.getByPlaceholder("Enter Email").click();
-  await page.getByPlaceholder("Enter Email").fill("11@11");
+  await page.getByPlaceholder("Enter Email").fill("44@44");
   await page.getByRole("button", { name: "Add" }).click();
   await page
     .getByLabel(
       "I confirm that the owner information provided above matches the current Certificate of Title. Mismatched information can cause significant delays to processing time."
     )
     .check();
-  await page.getByRole("button", { name: "Next Step" }).click();
-  await page.locator("#mat-button-toggle-11-button").click();
-  await page.getByText("Primary Contact").click();
+  await page
+    .getByRole("button", { name: "Add another parcel to the application" })
+    .click();
+  await page.getByRole("textbox", { name: "Enter legal description" }).click();
+  await page
+    .getByRole("textbox", { name: "Enter legal description" })
+    .fill("another description");
+  await page.getByRole("button", { name: "Crown" }).click();
   await page
     .locator(
-      "#appBody > app-create-application > app-custom-stepper > section > div:nth-child(4) > app-primary-contact > section:nth-child(2) > div.contacts > button"
+      ".ng-invalid > .form-row > div:nth-child(3) > .mat-mdc-form-field > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix"
     )
+    .click();
+  await page.getByRole("spinbutton").click();
+  await page.getByRole("spinbutton").fill("2");
+  await page
+    .locator(
+      ".ng-invalid > .form-row > div:nth-child(4) > .mat-mdc-form-field > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix"
+    )
+    .click();
+  await page
+    .locator(
+      ".ng-invalid > .form-row > div:nth-child(4) > .mat-mdc-form-field > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix"
+    )
+    .click();
+  await page.getByRole("textbox", { name: "Enter PID" }).fill("333-333-333");
+  await page.getByRole("button", { name: "No", exact: true }).click();
+  await page.setInputFiles("input.file-input", filePathToUseAsUpload);
+  await page.getByText("Provincial Crown").click();
+  await page.getByPlaceholder("Search by Ministry or Department").click();
+  await page
+    .getByRole("option", {
+      name: "No owner matching search Add new government contact",
+    })
+    .getByRole("button", { name: "Add new government contact" })
+    .click();
+  await page
+    .getByPlaceholder("Type ministry or department name")
+    .fill("test ministry");
+  await page.getByPlaceholder("Enter First Name").click();
+  await page.getByPlaceholder("Enter First Name").fill("test");
+  await page.getByPlaceholder("Enter Last Name").click();
+  await page.getByPlaceholder("Enter Last Name").fill("ministry");
+  await page.getByPlaceholder("(555) 555-5555").click();
+  await page.getByPlaceholder("(555) 555-5555").fill("(222) 222-22222");
+  await page.getByPlaceholder("Enter Email").click();
+  await page.getByPlaceholder("Enter Email").fill("22@22");
+  await page.getByRole("button", { name: "Add" }).click();
+  await page
+    .getByRole("checkbox", {
+      name: "I confirm that the owner information provided above matches the current Certificate of Title. Mismatched information can cause significant delays to processing time.",
+    })
+    .check();
+  await page.getByRole("button", { name: "Next Step" }).click();
+  await page.locator("#mat-button-toggle-17-button").click();
+  await page.getByText("Primary Contact").click();
+  await page
+    .getByRole("button", { name: "Make Primary Contact" })
     .first()
     .click();
-  await page.getByText("4").first().click();
+  await page.getByText("Government", { exact: true }).click();
   await page.getByPlaceholder("Type government").click();
   await page.getByPlaceholder("Type government").fill("peace");
   await page.getByText("Peace River Regional District").click();
-  await page.getByText("5").first().click();
+  await page.getByText("Land Use").click();
   await page
     .getByLabel(
       "Quantify and describe in detail all agriculture that currently takes place on the parcel(s)."
@@ -108,18 +149,17 @@ test("test", async ({ page }) => {
     .getByLabel(
       "Quantify and describe in detail all agriculture that currently takes place on the parcel(s)."
     )
-    .fill("1");
+    .fill("4");
   await page
-    .locator(
-      "div:nth-child(2) > .mat-mdc-form-field > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix"
+    .getByLabel(
+      "Quantify and describe in detail all agricultural improvements made to the parcel(s)."
     )
-    .first()
     .click();
   await page
     .getByLabel(
       "Quantify and describe in detail all agricultural improvements made to the parcel(s)."
     )
-    .fill("2");
+    .fill("4");
   await page
     .getByLabel(
       "Quantify and describe all non-agricultural uses that currently take place on the parcel(s)."
@@ -129,16 +169,16 @@ test("test", async ({ page }) => {
     .getByLabel(
       "Quantify and describe all non-agricultural uses that currently take place on the parcel(s)."
     )
-    .fill("3");
+    .fill("4");
   await page
     .locator(
       ".land-use-type > .mat-mdc-form-field > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix"
     )
     .first()
     .click();
-  await page.getByRole("option", { name: "Other" }).click();
+  await page.getByText("Other", { exact: true }).click();
   await page
-    .locator("#mat-select-value-5")
+    .locator("#mat-select-value-7")
     .getByText("Main Land Use Type")
     .click();
   await page.getByText("Industrial").first().click();
@@ -167,44 +207,31 @@ test("test", async ({ page }) => {
     .click();
   await page.locator("#westLandUseTypeDescription").fill("5");
   await page.getByRole("button", { name: "Next Step" }).click();
+  await page.getByPlaceholder("Type size in hectares").click();
+  await page.getByPlaceholder("Type size in hectares").fill("5");
   await page.getByLabel("What is the purpose of the proposal?").click();
   await page.getByLabel("What is the purpose of the proposal?").fill("6");
   await page
     .getByLabel(
-      "Specify any agricultural activities such as livestock operations, greenhouses or horticultural activities in proximity to the proposal."
+      "Could this proposal be accommodated on lands outside of the ALR? Please justify why the proposal cannot be carried out on lands outside the ALR."
     )
     .click();
   await page
     .getByLabel(
-      "Specify any agricultural activities such as livestock operations, greenhouses or horticultural activities in proximity to the proposal."
+      "Could this proposal be accommodated on lands outside of the ALR? Please justify why the proposal cannot be carried out on lands outside the ALR."
     )
     .fill("6");
   await page
     .getByLabel(
-      "What steps will you take to reduce potential negative impacts on surrounding agricultural lands?"
+      "Does the proposal support agriculture in the short or long term? Please explain."
     )
     .click();
   await page
     .getByLabel(
-      "What steps will you take to reduce potential negative impacts on surrounding agricultural lands?"
+      "Does the proposal support agriculture in the short or long term? Please explain."
     )
     .fill("6");
-  await page.getByRole("textbox", { name: "Type comment" }).click();
-  await page.getByRole("textbox", { name: "Type comment" }).fill("6");
-  await page.getByPlaceholder("Type total area").click();
-  await page.getByPlaceholder("Type total area").fill("6");
-  await page
-    .getByLabel(
-      "I confirm that all affected property owners with land in the ALR have been notified."
-    )
-    .check();
-  await page.setInputFiles("#proof-of-serving > input", filePathToUseAsUpload);
-
-  await page.setInputFiles("#proposal-map > input", filePathToUseAsUpload);
+  await page.getByRole("button", { name: "No" }).click();
   await page.getByRole("button", { name: "Next Step" }).click();
-  await page
-    .locator("div")
-    .filter({ hasText: /^Review & Submit$/ })
-    .click();
-  await page.getByRole("button", { name: "Save and Exit" }).click();
+  await page.getByRole("button", { name: "Next Step" }).click();
 });
