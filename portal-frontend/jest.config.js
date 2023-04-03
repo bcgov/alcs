7b44aa260a0ed19d230bb380e4720412edd245a9
mhuseinov <61513701+mhuseinov@users.noreply.github.com>
2023-04-03 09:00:47 -0700

    Alcs/playwright demo (#499)
    TUR application submission creation in portal
    Partial NFU submission creation in portal

--- portal-frontend/jest.config.js
+++ portal-frontend/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'jest-preset-angular',
   moduleDirectories: ['node_modules', 'src'],
+  modulePathIgnorePatterns:['/test'],
   globalSetup: 'jest-preset-angular/global-setup',
   coverageReporters: [
     [
