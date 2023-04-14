5b9bc4df2f6dab7b978be349780821a16034ab91
Mekhti <mekhti.huseinov.ssi@gmail.com>
2023-04-14 09:48:08 -0700

    tidy; update api readme and package.json

--- services/apps/alcs/src/portal/application-submission/generate-submission-document/generate-submission-document.service.ts
+++ services/apps/alcs/src/portal/application-submission/generate-submission-document/generate-submission-document.service.ts
@@ -32,20 +32,13 @@ const NO_DATA = 'No Data';
 @Injectable()
 export class GenerateSubmissionDocumentService {
   constructor(
-    // TODO check and clean up this forward refs
-    @Inject(forwardRef(() => CdogsService))
     private documentGenerationService: CdogsService,
     @Inject(forwardRef(() => ApplicationSubmissionService))
     private applicationSubmissionService: ApplicationSubmissionService,
-    @Inject(forwardRef(() => ApplicationLocalGovernmentService))
     private localGovernmentService: ApplicationLocalGovernmentService,
-    @Inject(forwardRef(() => ApplicationService))
     private applicationService: ApplicationService,
-    @Inject(forwardRef(() => ApplicationParcelService))
     private parcelService: ApplicationParcelService,
-    @Inject(forwardRef(() => ApplicationOwnerService))
     private ownerService: ApplicationOwnerService,
-    @Inject(forwardRef(() => ApplicationDocumentService))
     private applicationDocumentService: ApplicationDocumentService,
   ) {}
 
