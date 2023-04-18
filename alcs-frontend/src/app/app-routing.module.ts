7d961db08ee91546335c06159b68c48dac4d7b43
Daniel Haselhan <daniel@bit3.ca>
2023-04-18 15:43:13 -0700

    Enable PreLoad Modules Flag
    Fixes issues when navigating to lazy loaded routes that may not exist yet, may have performance impacts

--- alcs-frontend/src/app/app-routing.module.ts
+++ alcs-frontend/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthorizationComponent } from './features/authorization/authorization.component';
 import { NotFoundComponent } from './features/errors/not-found/not-found.component';
 import { LoginComponent } from './features/login/login.component';
@@ -78,7 +78,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
