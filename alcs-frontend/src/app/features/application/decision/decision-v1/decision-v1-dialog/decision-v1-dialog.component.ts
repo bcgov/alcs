713358291a4eb3ed992bbfb834181bd2bc003458
Daniel Haselhan <daniel@bit3.ca>
2023-05-10 15:06:08 -0700

    Only allow linking Decisions to Approved Modifications and Proceed Recons

--- alcs-frontend/src/app/features/application/decision/decision-v1/decision-v1-dialog/decision-v1-dialog.component.ts
+++ alcs-frontend/src/app/features/application/decision/decision-v1/decision-v1-dialog/decision-v1-dialog.component.ts
@@ -242,7 +242,7 @@ export class DecisionV1DialogComponent implements OnInit {
       .filter(
         (modification) =>
           (existingDecision && existingDecision.modifies?.uuid === modification.uuid) ||
-          (modification.reviewOutcome.code !== 'REF' && modification.resultingDecision === null)
+          (modification.reviewOutcome.code === 'APPR' && modification.resultingDecision === null)
       )
       .map((modification, index) => ({
         label: `Modification Request #${modifications.length - index} - ${modification.modifiesDecisions
@@ -256,7 +256,7 @@ export class DecisionV1DialogComponent implements OnInit {
       .filter(
         (reconsideration) =>
           (existingDecision && existingDecision.reconsiders?.uuid === reconsideration.uuid) ||
-          (reconsideration.reviewOutcome?.code !== 'REF' && reconsideration.resultingDecision === null)
+          (reconsideration.reviewOutcome?.code === 'PRC' && reconsideration.resultingDecision === null)
       )
       .map((reconsideration, index) => ({
         label: `Reconsideration Request #${reconsiderations.length - index} - ${reconsideration.reconsideredDecisions
