a3fe7ab7f78749759f45d5f7c96829dd1fecfa98
Liam Stoddard <liam@Liams-MacBook-Air.local>
2023-05-26 16:08:16 -0700

    changed comment steps

--- bin/migrate-oats-data/sql/insert-batch-application.sql
+++ bin/migrate-oats-data/sql/insert-batch-application.sql
@@ -1,26 +1,4 @@
--- Step 1: Create helper table to lookup uids and deal with duplicates
--- DROP TABLE IF EXISTS application_etl;
-
--- CREATE TEMPORARY TABLE application_etl (
---     id SERIAL PRIMARY KEY,
---     application_id int,
---     card_uuid UUID NOT NULL DEFAULT gen_random_uuid(),
---     duplicated bool DEFAULT false
--- );
-
--- INSERT INTO
---     application_etl (application_id, duplicated)
--- SELECT
---     DISTINCT oa.alr_application_id AS application_id,
---     CASE
---         WHEN a.uuid IS NOT NULL THEN TRUE
---         ELSE false
---     END AS duplicated
--- FROM
---     oats.oats_alr_applications AS oa
---     LEFT JOIN alcs.application AS a ON oa.alr_application_id :: text = a.file_number;
-
--- Step 2: Create associated card
+-- Step 1: Create associated card
 -- INSERT INTO
 --     alcs.card (uuid, audit_created_by)
 -- SELECT
@@ -31,7 +9,7 @@
 -- WHERE
 --     ae.duplicated IS false;
 
--- Step 3: Perform a lookup to retrieve the applicant's name or organization for each application ID
+-- Step 2: Perform a lookup to retrieve the applicant's name or organization for each application ID
 WITH applicant_lookup AS (
     SELECT
         DISTINCT oaap.alr_application_id AS application_id,
@@ -57,7 +35,7 @@ WITH applicant_lookup AS (
     GROUP BY
         oaap.alr_application_id
 ),
--- Step 4: Perform a lookup to retrieve the region code for each application ID
+-- Step 3: Perform a lookup to retrieve the region code for each application ID
 panel_lookup AS (
     SELECT
         DISTINCT oaap.alr_application_id AS application_id,
@@ -69,17 +47,8 @@ panel_lookup AS (
         JOIN oats.oats_organizations oo2 ON oo.parent_organization_id = oo2.organization_id
     WHERE
         oo2.organization_type_cd = 'PANEL'
-) -- Step 5: Insert new records into the alcs_applications table
--- INSERT INTO
---     alcs.application (
---         file_number,
---         -- card_uuid,
---         type_code,
---         applicant,
---         region_code,
---         local_government_uuid,
---         audit_created_by
---     )
+) 
+-- Step 4: Insert new records into the alcs_applications table
 SELECT
     oa.alr_application_id :: text AS file_number,
     -- ae.card_uuid AS card_uuid,
