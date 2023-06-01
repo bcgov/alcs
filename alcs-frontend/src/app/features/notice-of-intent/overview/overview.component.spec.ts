85d5d8f699334d6c12e5ad63b5b4336393f47f5a
Mekhti <mekhti.huseinov.ssi@gmail.com>
2023-06-01 11:10:04 -0700

    unit tests

--- alcs-frontend/src/app/features/notice-of-intent/overview/overview.component.spec.ts
+++ alcs-frontend/src/app/features/notice-of-intent/overview/overview.component.spec.ts
@@ -2,6 +2,8 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { BehaviorSubject } from 'rxjs';
+import { NoticeOfIntentMeetingDto } from '../../../services/notice-of-intent/meeting/notice-of-intent-meeting.dto';
+import { NoticeOfIntentMeetingService } from '../../../services/notice-of-intent/meeting/notice-of-intent-meeting.service';
 import { NoticeOfIntentDetailService } from '../../../services/notice-of-intent/notice-of-intent-detail.service';
 import { NoticeOfIntentDto } from '../../../services/notice-of-intent/notice-of-intent.dto';
 
@@ -11,10 +13,14 @@ describe('OverviewComponent', () => {
   let component: OverviewComponent;
   let fixture: ComponentFixture<OverviewComponent>;
   let mockNOIDetailService: DeepMocked<NoticeOfIntentDetailService>;
+  let mockNoticeOfIntentMeetingService: DeepMocked<NoticeOfIntentMeetingService>;
 
   beforeEach(async () => {
     mockNOIDetailService = createMock();
     mockNOIDetailService.$noticeOfIntent = new BehaviorSubject<NoticeOfIntentDto | undefined>(undefined);
+
+    mockNoticeOfIntentMeetingService = createMock();
+    mockNoticeOfIntentMeetingService.$meetings = new BehaviorSubject<NoticeOfIntentMeetingDto[]>([]);
 
     await TestBed.configureTestingModule({
       providers: [
@@ -22,6 +28,7 @@ describe('OverviewComponent', () => {
           provide: NoticeOfIntentDetailService,
           useValue: mockNOIDetailService,
         },
+        { provide: NoticeOfIntentMeetingService, useValue: mockNoticeOfIntentMeetingService },
       ],
       declarations: [OverviewComponent],
       schemas: [NO_ERRORS_SCHEMA],
