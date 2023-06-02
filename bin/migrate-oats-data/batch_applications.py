d256ae42c08b2572333610ed43ba26c910a52b64
lstod <lstodd@protonmail.com>
2023-06-02 13:09:04 -0700

    Feature/alcs 559 (#638)
    * Batching of applications
    
    * Removed cards, single batch variable, updated failed_inserts
    
    * panel region codes
    
    ---------
    
    Co-authored-by: Liam Stoddard <liam@Liams-MacBook-Air.local>

---
+++ bin/migrate-oats-data/batch_applications.py
@@ -0,0 +1,194 @@
+from db import inject_conn_pool
+def application_etl_temp_table():
+    """
+    function creates a temporary table to be used by .sql files called later in script
+    """
+    return f"""
+        DROP TABLE IF EXISTS oats.application_etl;
+
+        CREATE TABLE oats.application_etl (
+            id SERIAL PRIMARY KEY,
+            application_id int,
+            card_uuid UUID NOT NULL DEFAULT gen_random_uuid(),
+            duplicated bool DEFAULT false
+        )
+    """
+
+def application_etl_insert():
+    """
+    function inserts data into prevoisly created table and decipers duplication of uuids
+    application_id is copied from oats.oats_alr_applications.alr_application_id
+    """
+    return f"""
+        INSERT INTO
+            oats.application_etl (application_id, duplicated)
+        SELECT
+            DISTINCT oa.alr_application_id AS application_id,
+            CASE
+                WHEN a.uuid IS NOT NULL THEN TRUE
+                ELSE false
+            END AS duplicated
+        FROM
+            oats.oats_alr_applications AS oa
+            LEFT JOIN alcs.application AS a ON oa.alr_application_id :: text = a.file_number
+    """
+
+def appl_code_lut():
+    """
+    Create temporary table to match codes from OATS to ALCS
+    """
+    return f"""
+        DROP TABLE IF EXISTS oats.appl_code_lut;
+
+        CREATE TABLE oats.appl_code_lut (
+            oats_code VARCHAR,
+            alcs_code VARCHAR
+        )
+    """
+def insert_appl_code_lut():
+    """
+    Insert LUT values
+    """
+    return f"""
+
+        INSERT INTO 
+            oats.appl_code_lut (oats_code, alcs_code)
+
+        VALUES ('TUR','TURP'),
+            ('INC','INCL'),
+            ('EXC','EXCL'),
+            ('SDV','SUBD'),
+            ('NFU','NFUP'),
+            ('SCH','PFRS'),
+            ('EXT','ROSO'),
+            ('FILL','POFO'),
+            ('SRW','NARU'),
+            ('CSC','NARU'),
+            ('NAR','NARU')
+
+    """
+
+def compile_application_insert_query(number_of_rows_to_insert):
+    """
+    function takes the number of rows to insert and generates an SQL insert statement with upserts using the ON CONFLICT clause
+    """
+
+    applications_to_insert = ",".join(["%s"] * number_of_rows_to_insert)
+    return f"""
+        INSERT INTO alcs.application (file_number, type_code, 
+                                      applicant, region_code, local_government_uuid, audit_created_by)
+
+        VALUES{applications_to_insert}
+        ON CONFLICT (file_number) DO UPDATE SET
+            file_number = EXCLUDED.file_number,
+            type_code = EXCLUDED.type_code,
+            applicant = EXCLUDED.applicant,
+            region_code = EXCLUDED.region_code,
+            local_government_uuid = EXCLUDED.local_government_uuid,
+            audit_created_by = EXCLUDED.audit_created_by
+    """
+# ON CONFLICT needs confirmation of what value is kept
+def drop_etl_temp_table():
+    """
+    remove the table
+    """
+    return f"""
+    DROP TABLE oats.application_etl;
+    """
+
+
+def drop_appl_code_temp_table():
+    """
+    remove the table
+    """
+    return f"""
+    DROP TABLE oats.appl_code_lut;
+    """
+
+@inject_conn_pool
+def process_applications(conn=None, batch_size=10000):
+    """
+    function uses a decorator pattern @inject_conn_pool to inject a database connection pool to the function. It fetches the total count of non duplicate applications and prints it to the console. Then, it fetches the applications to insert in batches using application IDs / file_number, constructs an insert query, and processes them.
+    """
+    with conn.cursor() as cursor:
+        cursor.execute(application_etl_temp_table())
+        cursor.execute(application_etl_insert())
+        cursor.execute(appl_code_lut())
+        print("inserting LUT")
+        cursor.execute(insert_appl_code_lut())
+        conn.commit()
+        with open(
+            "sql/insert_batch_application_count.sql", "r", encoding="utf-8"
+        ) as sql_file:
+            count_query = sql_file.read()
+            cursor.execute(count_query)
+            count_total = cursor.fetchone()[0]
+        print("- Applications to insert: ", count_total)
+
+        failed_inserts = 0
+        successful_inserts_count = 0
+        last_application_id = 0
+        
+
+        with open("sql/insert-batch-application.sql", "r", encoding="utf-8") as sql_file:
+            application_sql = sql_file.read()
+            while True:
+                cursor.execute(
+                    f"{application_sql} WHERE ae.application_id > {last_application_id} ORDER by ae.application_id"
+                )
+                
+                rows = cursor.fetchmany(batch_size)
+                if not rows:
+                    break
+                try: 
+                    applications_to_be_inserted_count = len(rows)
+
+                    insert_query = compile_application_insert_query(
+                        applications_to_be_inserted_count
+                    ) 
+                    cursor.execute(insert_query, rows)
+                    conn.commit()
+ 
+                    last_application_id = rows[-1][0]
+                    successful_inserts_count = (
+                        successful_inserts_count + applications_to_be_inserted_count
+                    )
+
+                    print(
+                        f"retrieved/inserted items count: {applications_to_be_inserted_count}; total successfully inserted/updated applications so far {successful_inserts_count}; last inserted applidation_id: {last_application_id}"
+
+                    )
+                except Exception as e:
+                    conn.rollback()
+                    print("Error", e)
+                    failed_inserts  = count_total - successful_inserts_count
+                    last_application_id = last_application_id +1
+                    break
+
+    print("Total amount of successful inserts:", successful_inserts_count)
+    print("Total failed inserts:", failed_inserts)
+    if failed_inserts == 0:
+        with conn.cursor() as cursor:
+            cursor.execute(drop_etl_temp_table())
+            print("etl temp table removed")
+            cursor.execute(drop_appl_code_temp_table())
+            print("code lut removed")
+
+    else:
+        print("Table not deleted, inserts failed")
+        #keep only the failed rows
+
+
+@inject_conn_pool
+def clean_applications(conn=None):
+    print("Start applications cleaning")
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "DELETE FROM alcs.application a WHERE a.audit_created_by = 'oats_etl'"
+        )
+        print(f"Deleted items count = {cursor.rowcount}")
+
+
+        
+
+        
---
+++ bin/migrate-oats-data/sql/insert-batch-application.sql
@@ -0,0 +1,81 @@
+
+-- Step 1: Perform a lookup to retrieve the applicant's name or organization for each application ID
+WITH applicant_lookup AS (
+    SELECT
+        DISTINCT oaap.alr_application_id AS application_id,
+        string_agg(DISTINCT oo.organization_name, ', ') FILTER (
+            WHERE
+                oo.organization_name IS NOT NULL
+        ) AS orgs,
+        string_agg(
+            DISTINCT concat(op.first_name || ' ' || op.last_name),
+            ', '
+        ) FILTER (
+            WHERE
+                op.last_name IS NOT NULL
+                OR op.first_name IS NOT NULL
+        ) AS persons
+    FROM
+        oats.oats_alr_application_parties oaap
+        LEFT JOIN oats.oats_person_organizations opo ON oaap.person_organization_id = opo.person_organization_id
+        LEFT JOIN oats.oats_persons op ON op.person_id = opo.person_id
+        LEFT JOIN oats.oats_organizations oo ON opo.organization_id = oo.organization_id
+    WHERE
+        oaap.alr_appl_role_code = 'APPL'
+    GROUP BY
+        oaap.alr_application_id
+),
+-- Step 2: Perform a lookup to retrieve the region code for each application ID
+panel_lookup AS (
+    SELECT
+        DISTINCT oaap.alr_application_id AS application_id,
+        oo2.organization_name AS panel_region
+    FROM
+        oats.oats_alr_application_parties oaap
+        JOIN oats.oats_person_organizations opo ON oaap.person_organization_id = opo.person_organization_id
+        JOIN oats.oats_organizations oo ON opo.organization_id = oo.organization_id
+        JOIN oats.oats_organizations oo2 ON oo.parent_organization_id = oo2.organization_id
+    WHERE
+        oo2.organization_type_cd = 'PANEL'
+),
+-- Step X: Perform lookup to retrieve type code
+application_type_lookup AS (
+    SELECT
+        oaac.alr_application_id AS application_id,
+        oacc."description" AS "description",
+        oaac.alr_change_code AS code,
+        acl.alcs_code AS alcs_code
+
+    FROM
+        oats.oats_alr_appl_components AS oaac
+        JOIN oats.oats_alr_change_codes oacc ON oaac.alr_change_code = oacc.alr_change_code
+        JOIN oats.appl_code_lut AS acl ON oaac.alr_change_code = acl.oats_code
+
+        
+)
+
+-- Step 3: Insert new records into the alcs_applications table
+SELECT
+    oa.alr_application_id :: text AS file_number,
+    -- TODO: type code lookup
+    'NARU' as type_code,
+    -- COALESCE(atl.alcs_code, 'NARU') AS type_code,
+    CASE
+        WHEN applicant_lookup.orgs IS NOT NULL THEN applicant_lookup.orgs
+        WHEN applicant_lookup.persons IS NOT NULL THEN applicant_lookup.persons
+        ELSE 'Unknown'
+    END AS applicant,
+    -- TODO: panel region lookup
+    -- 'INTR',
+	ar.code as region_code,
+    --Peace river TODO: local government lookup
+    '001cfdad-bc6e-4d25-9294-1550603da980',
+    'oats_etl'
+FROM
+    oats.oats_alr_applications AS oa
+    JOIN oats.application_etl AS ae ON oa.alr_application_id = ae.application_id AND ae.duplicated IS false
+    LEFT JOIN applicant_lookup ON oa.alr_application_id = applicant_lookup.application_id
+    LEFT JOIN panel_lookup ON oa.alr_application_id = panel_lookup.application_id
+    -- LEFT JOIN application_type_lookup AS atl ON oa.alr_application_id = atl.application_id
+	LEFT JOIN alcs.application_region ar ON panel_lookup.panel_region = ar."label"
+    
