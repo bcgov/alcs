4015bf039a80b164621d3207e73bfbb585d932be
Mekhti <mekhti.huseinov.ssi@gmail.com>
2023-06-16 09:28:38 -0700

    unit tests

--- alcs-frontend/src/app/features/application/decision/decision-v2/decision-component/pofo/pofo.component.spec.ts
+++ alcs-frontend/src/app/features/application/decision/decision-v2/decision-component/pofo/pofo.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DecisionComponentDto } from '../../../../../../services/application/decision/application-decision-v2/application-decision-v2.dto';
 
 import { PofoComponent } from './pofo.component';
 
@@ -8,12 +9,12 @@ describe('PofoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PofoComponent ]
-    })
-    .compileComponents();
+      declarations: [PofoComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PofoComponent);
     component = fixture.componentInstance;
+    component.component = {} as DecisionComponentDto;
     fixture.detectChanges();
   });
 
