88eec2a2b741740eac3429ea55e6dd231891c9ac
Mekhti <mekhti.huseinov.ssi@gmail.com>
2023-06-20 10:44:02 -0700

    UI unit tests

--- alcs-frontend/src/app/services/search/search.service.spec.ts
+++ alcs-frontend/src/app/services/search/search.service.spec.ts
@@ -1,16 +1,85 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { of, throwError } from 'rxjs';
+import { ToastService } from '../toast/toast.service';
+import { SearchResultDto } from './search.dto';
 import { SearchService } from './search.service';
 
 describe('SearchService', () => {
   let service: SearchService;
+  let mockHttpClient: DeepMocked<HttpClient>;
+  let mockToastService: DeepMocked<ToastService>;
+
+  const mockSearchResults: SearchResultDto[] = [
+    {
+      fileNumber: '123456',
+      type: 'Type A',
+      referenceId: 'REF123',
+      localGovernmentName: 'Local Gov 1',
+    },
+    {
+      fileNumber: '789012',
+      type: 'Type B',
+      referenceId: 'REF456',
+      applicant: 'John Doe',
+      localGovernmentName: 'Local Gov 2',
+      boardCode: 'BC123',
+      label: {
+        code: 'example_code',
+        shortLabel: 'Short label',
+        backgroundColor: '#ffffff',
+        textColor: '#000000',
+        label: 'Application Type 1',
+        description: 'Application Type 1',
+      },
+    },
+  ];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockHttpClient = createMock();
+    mockToastService = createMock();
+
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: mockHttpClient,
+        },
+        {
+          provide: ToastService,
+          useValue: mockToastService,
+        },
+      ],
+    });
     service = TestBed.inject(SearchService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch search results by search text', async () => {
+    mockHttpClient.get.mockReturnValue(of(mockSearchResults));
+
+    const res = await service.fetch('1');
+
+    expect(mockHttpClient.get).toHaveBeenCalledTimes(1);
+    expect(res).toBeDefined();
+    expect(res!.length).toEqual(2);
+  });
+
+  it('should show an error toast message if search fails', async () => {
+    mockHttpClient.get.mockReturnValue(
+      throwError(() => {
+        new Error('');
+      })
+    );
+
+    const res = await service.fetch('1');
+
+    expect(mockHttpClient.get).toHaveBeenCalledTimes(1);
+    expect(res).toBeUndefined();
+    expect(mockToastService.showErrorToast).toHaveBeenCalledTimes(1);
   });
 });
--- alcs-frontend/src/app/shared/header/search/search.component.spec.ts
+++ alcs-frontend/src/app/shared/header/search/search.component.spec.ts
@@ -1,16 +1,49 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { Observable } from 'rxjs';
+import { SearchService } from '../../../services/search/search.service';
+import { ToastService } from '../../../services/toast/toast.service';
 
 import { SearchComponent } from './search.component';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let mockSearchService: DeepMocked<SearchService>;
+  let mockRouter: DeepMocked<Router>;
+  let mockToastService: DeepMocked<ToastService>;
 
   beforeEach(async () => {
+    mockSearchService = createMock();
+    mockRouter = createMock();
+    mockToastService = createMock();
+
     await TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
-    })
-    .compileComponents();
+      providers: [
+        {
+          provide: SearchService,
+          useValue: mockSearchService,
+        },
+        {
+          provide: Router,
+          useValue: mockRouter,
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParamMap: new Observable<ParamMap>
+          },
+        },
+        {
+          provide: ToastService,
+          useValue: mockToastService,
+        },
+      ],
+      declarations: [SearchComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
