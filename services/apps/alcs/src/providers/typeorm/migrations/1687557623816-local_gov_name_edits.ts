61d32bd754b3bd86c4396338531d4cd73792bada
Liam Stoddard <liam@Liams-MacBook-Air.local>
2023-06-28 13:55:19 -0700

    bugfix to use uuid instead of name go brrr

--- services/apps/alcs/src/providers/typeorm/migrations/1687557623816-local_gov_name_edits.ts
+++ services/apps/alcs/src/providers/typeorm/migrations/1687557623816-local_gov_name_edits.ts
@@ -5,7 +5,7 @@ export class localGovNameEdits1687557623816 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
           UPDATE "alcs"."application_local_government" SET "name" = 'Village of Masset' WHERE "name" = 'Village of Massett';
-          DELETE FROM alcs.application_local_government WHERE name = 'Northern Rockies Regional Municipality';
+          DELETE FROM alcs.application_local_government WHERE uuid = '33aa1f7d-3b65-4ed5-badf-11dafb0b2789';
         `);
         
     }
