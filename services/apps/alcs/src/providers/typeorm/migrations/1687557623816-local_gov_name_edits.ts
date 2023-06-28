9f78019678d9b3ed9efed201f4788e811831d778
Liam Stoddard <liam@Liams-MacBook-Air.local>
2023-06-28 15:39:16 -0700

    uuids were reversed, added support for covenants and planning review

--- services/apps/alcs/src/providers/typeorm/migrations/1687557623816-local_gov_name_edits.ts
+++ services/apps/alcs/src/providers/typeorm/migrations/1687557623816-local_gov_name_edits.ts
@@ -5,9 +5,11 @@ export class localGovNameEdits1687557623816 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
           UPDATE "alcs"."application_local_government" SET "name" = 'Village of Masset' WHERE "name" = 'Village of Massett';
-          UPDATE "alcs"."application" SET "local_government_uuid" = '92961db6-b74c-460b-bbad-e285398fa491' WHERE "local_government_uuid" = '33aa1f7d-3b65-4ed5-badf-11dafb0b2789';
-          UPDATE "alcs"."notice_of_intent" SET "local_government_uuid" = '92961db6-b74c-460b-bbad-e285398fa491' WHERE "local_government_uuid" = '33aa1f7d-3b65-4ed5-badf-11dafb0b2789';
-          DELETE FROM alcs.application_local_government WHERE uuid = '33aa1f7d-3b65-4ed5-badf-11dafb0b2789';
+          UPDATE "alcs"."application" SET "local_government_uuid" = '33aa1f7d-3b65-4ed5-badf-11dafb0b2789' WHERE "local_government_uuid" = '92961db6-b74c-460b-bbad-e285398fa491';
+          UPDATE "alcs"."notice_of_intent" SET "local_government_uuid" = '33aa1f7d-3b65-4ed5-badf-11dafb0b2789' WHERE "local_government_uuid" = '92961db6-b74c-460b-bbad-e285398fa491';
+          UPDATE "alcs"."planning_review" SET "local_government_uuid" = '33aa1f7d-3b65-4ed5-badf-11dafb0b2789' WHERE "local_government_uuid" = '92961db6-b74c-460b-bbad-e285398fa491';
+          UPDATE "alcs"."covenant" SET "local_government_uuid" = '33aa1f7d-3b65-4ed5-badf-11dafb0b2789' WHERE "local_government_uuid" = '92961db6-b74c-460b-bbad-e285398fa491';
+          DELETE FROM alcs.application_local_government WHERE uuid = '92961db6-b74c-460b-bbad-e285398fa491';
         `);
     }
 
