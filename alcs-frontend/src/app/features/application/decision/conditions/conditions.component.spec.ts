7c7ccdf35be359aa575f65ca9409584d68f1fc66
Mekhti <mekhti.huseinov.ssi@gmail.com>
2023-07-04 17:10:00 -0700

    ui tests

--- alcs-frontend/src/app/features/application/decision/conditions/conditions.component.spec.ts
+++ alcs-frontend/src/app/features/application/decision/conditions/conditions.component.spec.ts
@@ -1,8 +1,10 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { BehaviorSubject } from 'rxjs';
 import { ApplicationDetailService } from '../../../../services/application/application-detail.service';
+import { ApplicationDto } from '../../../../services/application/application.dto';
 import { ApplicationDecisionV2Service } from '../../../../services/application/decision/application-decision-v2/application-decision-v2.service';
 
 import { ConditionsComponent } from './conditions.component';
@@ -15,6 +17,8 @@ describe('ConditionsComponent', () => {
 
   beforeEach(async () => {
     mockApplicationDetailService = createMock();
+    mockApplicationDetailService.$application = new BehaviorSubject<ApplicationDto | undefined>(undefined);
+
     mockApplicationDecisionV2Service = createMock();
 
     await TestBed.configureTestingModule({
@@ -30,7 +34,11 @@ describe('ConditionsComponent', () => {
         },
         {
           provide: ActivatedRoute,
-          useValue: {},
+          useValue: {
+            snapshot: {
+              paramMap: convertToParamMap({ uuid: 'fake' }),
+            },
+          },
         },
       ],
       schemas: [NO_ERRORS_SCHEMA],
