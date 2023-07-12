b24a935fb67d13dc699ad96987b50cef17100401
to. sandra <76515860+sandratoh@users.noreply.github.com>
2023-07-12 16:21:11 -0700

    Update decision component field layouts (#772)
    * Update decision component field layouts
    - Move common basic fields to separate component
    - Style field row by different app type components
    
    * Pass frontend unit test

---
+++ alcs-frontend/src/app/features/application/decision/decision-v2/decision-component/basic/basic.component.spec.ts
@@ -0,0 +1,26 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DecisionComponentDto } from '../../../../../../services/application/decision/application-decision-v2/application-decision-v2.dto';
+
+import { BasicComponent } from './basic.component';
+
+describe('BasicComponent', () => {
+  let component: BasicComponent;
+  let fixture: ComponentFixture<BasicComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BasicComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BasicComponent);
+    component = fixture.componentInstance;
+    component.component = {} as DecisionComponentDto;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- alcs-frontend/src/app/features/application/decision/decision-v2/decision-component/basic/basic.component.ts
+++ alcs-frontend/src/app/features/application/decision/decision-v2/decision-component/basic/basic.component.ts
@@ -2,10 +2,13 @@ import { Component, Input } from '@angular/core';
 import { DecisionComponentDto } from '../../../../../../services/application/decision/application-decision-v2/application-decision-v2.dto';
 
 @Component({
-  selector: 'app-turp',
-  templateUrl: './turp.component.html',
-  styleUrls: ['./turp.component.scss'],
+  selector: 'app-basic',
+  templateUrl: './basic.component.html',
+  styleUrls: ['./basic.component.scss'],
 })
-export class TurpComponent {
+export class BasicComponent {
   @Input() component!: DecisionComponentDto;
+  @Input() fillRow = false;
+
+  constructor() {}
 }
