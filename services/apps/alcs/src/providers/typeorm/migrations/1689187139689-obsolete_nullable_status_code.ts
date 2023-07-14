8a9db8524e3bd775091a6203afe718c131917bf7
mhuseinov <61513701+mhuseinov@users.noreply.github.com>
2023-07-14 15:59:40 -0700

    remove redundant constrain deletion
    remove redundant constrain deletion

--- services/apps/alcs/src/providers/typeorm/migrations/1689187139689-obsolete_nullable_status_code.ts
+++ services/apps/alcs/src/providers/typeorm/migrations/1689187139689-obsolete_nullable_status_code.ts
@@ -6,15 +6,9 @@ export class obsoleteNullableStatusCode1689187139689
   name = 'obsoleteNullableStatusCode1689187139689';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `
-        ALTER TABLE "alcs"."application_submission"  ALTER COLUMN status_code DROP NOT NULL;
-       `,
-    );
-
-    // await queryRunner.query(
-    //   `ALTER TABLE "alcs"."application_submission" DROP COLUMN "status_code"`,
-    // );
+     await queryRunner.query(
+       `ALTER TABLE "alcs"."application_submission" DROP COLUMN "status_code"`,
+     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
