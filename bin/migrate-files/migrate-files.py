1380ebe009fdb39e51a28acca669368e2eaf0926
Liam Stoddard <lstodd@protonmail.com>
2023-07-24 16:43:00 -0700

    changed to functions

--- bin/migrate-files/migrate-files.py
+++ bin/migrate-files/migrate-files.py
@@ -5,6 +5,188 @@ import boto3
 from tqdm import tqdm
 import pickle
 
+def application_docs(starting_document_id,batch):
+    # Get total number of files
+    try:
+        cursor.execute ('SELECT COUNT(*) FROM OATS.OATS_DOCUMENTS WHERE dbms_lob.getLength(DOCUMENT_BLOB) > 0 AND ALR_APPLICATION_ID IS NOT NULL')
+    except cx_Oracle.Error as e:
+        error, = e.args
+        print(error.message)
+
+    application_count = cursor.fetchone()[0]
+    print('Count =', application_count)
+
+    # # Execute the SQL query to retrieve the BLOB data and key column
+    cursor.execute(f"""
+    SELECT DOCUMENT_ID, ALR_APPLICATION_ID, FILE_NAME, DOCUMENT_BLOB, DOCUMENT_CODE, DESCRIPTION, DOCUMENT_SOURCE_CODE, UPLOADED_DATE, WHEN_UPDATED, REVISION_COUNT, dbms_lob.getLength(DOCUMENT_BLOB) as LENGTH
+    FROM OATS.OATS_DOCUMENTS
+    WHERE dbms_lob.getLength(DOCUMENT_BLOB) > 0
+    AND DOCUMENT_ID > {starting_document_id}
+    AND ALR_APPLICATION_ID IS NOT NULL
+    ORDER BY DOCUMENT_ID ASC
+    """)
+
+    # Set the batch size
+    BATCH_SIZE = batch
+
+    # Track progress
+    documents_processed = 0
+    last_document_id = 0
+
+    try:
+        with tqdm(total=application_count, unit="file", desc="Uploading files to S3") as pbar:
+            while True:
+                # Fetch the next batch of BLOB data
+                data = cursor.fetchmany(BATCH_SIZE)
+                if not data:
+                    break
+                # Upload the batch to S3 with a progress bar
+                for document_id, application_id, filename, file, code, description, source, created, updated, revision, length in data:
+                    tqdm.write(f"{application_id}/{document_id}_{filename}")
+
+                    with tqdm(total=length, unit="B", unit_scale=True, desc=filename) as pbar2:
+                        s3.upload_fileobj(file, ecs_bucket, f"migrate/application/{application_id}/{document_id}_{filename}",
+                            Callback=lambda bytes_transferred: pbar2.update(bytes_transferred),)
+                    pbar.update(1)
+                    last_document_id = document_id
+                    documents_processed += 1
+
+    except Exception as e:
+        print("Something went wrong:",e)
+        print("Processed", documents_processed,  "files")
+        
+        # Set resume status in case of interuption
+        with open('last-file.pickle', 'wb') as file:
+            pickle.dump(last_document_id, file)
+        
+        cursor.close()
+        conn.close()
+        exit()
+
+    # Display results
+    print("Process complete: Successfully migrated", documents_processed, "files.")
+
+def planning_docs(starting_planning_document_id,batch):
+    # Get total number of files
+    try:
+        cursor.execute ('SELECT COUNT(*) FROM OATS.OATS_DOCUMENTS WHERE dbms_lob.getLength(DOCUMENT_BLOB) > 0 AND PLANNING_REVIEW_ID IS NOT NULL')
+    except cx_Oracle.Error as e:
+        error, = e.args
+        print(error.message)
+
+    planning_review_count = cursor.fetchone()[0]
+    print('Count =', planning_review_count)
+
+    # # Execute the SQL query to retrieve the BLOB data and key column
+    cursor.execute(f"""
+    SELECT DOCUMENT_ID, PLANNING_REVIEW_ID, FILE_NAME, DOCUMENT_BLOB, DOCUMENT_CODE, DESCRIPTION, DOCUMENT_SOURCE_CODE, UPLOADED_DATE, WHEN_UPDATED, REVISION_COUNT, dbms_lob.getLength(DOCUMENT_BLOB) as LENGTH
+    FROM OATS.OATS_DOCUMENTS
+    WHERE dbms_lob.getLength(DOCUMENT_BLOB) > 0
+    AND DOCUMENT_ID > {starting_planning_document_id}
+    AND PLANNING_REVIEW_ID IS NOT NULL
+    ORDER BY DOCUMENT_ID ASC
+    """)
+
+    # Set the batch size
+    BATCH_SIZE = batch
+
+    # Track progress
+    documents_processed = 0
+    last_planning_document_id = 0
+
+    try:
+        with tqdm(total=planning_review_count, unit="file", desc="Uploading files to S3") as pbar:
+            while True:
+                # Fetch the next batch of BLOB data
+                data = cursor.fetchmany(BATCH_SIZE)
+                if not data:
+                    break
+                # Upload the batch to S3 with a progress bar
+                for document_id, planning_review_id, filename, file, code, description, source, created, updated, revision, length in data:
+                    tqdm.write(f"{planning_review_id}/{document_id}_{filename}")
+
+                    with tqdm(total=length, unit="B", unit_scale=True, desc=filename) as pbar2:
+                        s3.upload_fileobj(file, ecs_bucket, f"migrate/planning_review/{planning_review_id}/{document_id}_{filename}",
+                            Callback=lambda bytes_transferred: pbar2.update(bytes_transferred),)
+                    pbar.update(1)
+                    last_planning_document_id = document_id
+                    documents_processed += 1
+
+    except Exception as e:
+        print("Something went wrong:",e)
+        print("Processed", documents_processed,  "files")
+        
+        # Set resume status in case of interuption
+        with open('last-planning-file.pickle', 'wb') as file:
+            pickle.dump(last_planning_document_id, file)
+        
+        cursor.close()
+        conn.close()
+        exit()
+
+    # Display results
+    print("Process complete: Successfully migrated", documents_processed, "files.")
+
+def issue_docs(starting_issue_document_id,batch):
+    # Get total number of files
+    try:
+        cursor.execute ('SELECT COUNT(*) FROM OATS.OATS_DOCUMENTS WHERE dbms_lob.getLength(DOCUMENT_BLOB) > 0 AND ISSUE_ID IS NOT NULL')
+    except cx_Oracle.Error as e:
+        error, = e.args
+        print(error.message)
+
+    issue_count = cursor.fetchone()[0]
+    print('Count =', issue_count)
+
+    # # Execute the SQL query to retrieve the BLOB data and key column
+    cursor.execute(f"""
+    SELECT DOCUMENT_ID, ISSUE_ID, FILE_NAME, DOCUMENT_BLOB, DOCUMENT_CODE, DESCRIPTION, DOCUMENT_SOURCE_CODE, UPLOADED_DATE, WHEN_UPDATED, REVISION_COUNT, dbms_lob.getLength(DOCUMENT_BLOB) as LENGTH
+    FROM OATS.OATS_DOCUMENTS
+    WHERE dbms_lob.getLength(DOCUMENT_BLOB) > 0
+    AND DOCUMENT_ID > {starting_issue_document_id}
+    AND ISSUE_ID IS NOT NULL
+    ORDER BY DOCUMENT_ID ASC
+    """)
+
+    # Set the batch size
+    BATCH_SIZE = batch
+
+    # Track progress
+    documents_processed = 0
+    last_issue_document_id = 0
+
+    try:
+        with tqdm(total=issue_count, unit="file", desc="Uploading files to S3") as pbar:
+            while True:
+                # Fetch the next batch of BLOB data
+                data = cursor.fetchmany(BATCH_SIZE)
+                if not data:
+                    break
+                # Upload the batch to S3 with a progress bar
+                for document_id, issue_id, filename, file, code, description, source, created, updated, revision, length in data:
+                    tqdm.write(f"{issue_id}/{document_id}_{filename}")
+
+                    with tqdm(total=length, unit="B", unit_scale=True, desc=filename) as pbar2:
+                        s3.upload_fileobj(file, ecs_bucket, f"migrate/issue/{issue_id}/{document_id}_{filename}",
+                            Callback=lambda bytes_transferred: pbar2.update(bytes_transferred),)
+                    pbar.update(1)
+                    last_issue_document_id = document_id
+                    documents_processed += 1
+
+    except Exception as e:
+        print("Something went wrong:",e)
+        print("Processed", documents_processed,  "files")
+        
+        # Set resume status in case of interuption
+        with open('last-issue-file.pickle', 'wb') as file:
+            pickle.dump(last_issue_document_id, file)
+        
+        cursor.close()
+        conn.close()
+        exit()
+
+    # Display results
+    print("Process complete: Successfully migrated", documents_processed, "files.")
 # Load environment variables from .env file
 load_dotenv()
 
@@ -41,208 +223,34 @@ if os.path.isfile('last-file.pickle'):
     with open('last-file.pickle', 'rb') as file:
         starting_document_id = pickle.load(file)
 
-print('Starting applications from:', starting_document_id)
-
 starting_planning_document_id = 0
 # Determine job resume status
 if os.path.isfile('last-planning-file.pickle'):
     with open('last-planning-file.pickle', 'rb') as file:
         starting_planning_document_id = pickle.load(file)
-    #ignore imported files before failure
-    starting_document_id = 999999
-
-print('Starting planning_review from:', starting_planning_document_id)
+    print('Starting planning_review from:', starting_planning_document_id)
 
 starting_issue_document_id = 0
 # Determine job resume status
 if os.path.isfile('last-issue-file.pickle'):
     with open('last-issue-file.pickle', 'rb') as file:
         starting_issue_document_id = pickle.load(file)
-    #ignore imported files before failure
-    starting_document_id = 999999
-    starting_planning_document_id = 999999
-print('Starting issues from:', starting_issue_document_id)
-
-# Get total number of files
+    print('Starting issues from:', starting_issue_document_id)
 
 cursor = conn.cursor()
 
-try:
-    cursor.execute ('SELECT COUNT(*) FROM OATS.OATS_DOCUMENTS WHERE dbms_lob.getLength(DOCUMENT_BLOB) > 0 AND ALR_APPLICATION_ID IS NOT NULL')
-except cx_Oracle.Error as e:
-    error, = e.args
-    print(error.message)
+batch_size = 10
 
-application_count = cursor.fetchone()[0]
-print('Count =', application_count)
-
-# # Execute the SQL query to retrieve the BLOB data and key column
-cursor.execute(f"""
-SELECT DOCUMENT_ID, ALR_APPLICATION_ID, FILE_NAME, DOCUMENT_BLOB, DOCUMENT_CODE, DESCRIPTION, DOCUMENT_SOURCE_CODE, UPLOADED_DATE, WHEN_UPDATED, REVISION_COUNT, dbms_lob.getLength(DOCUMENT_BLOB) as LENGTH
-FROM OATS.OATS_DOCUMENTS
-WHERE dbms_lob.getLength(DOCUMENT_BLOB) > 0
-AND DOCUMENT_ID > {starting_document_id}
-AND ALR_APPLICATION_ID IS NOT NULL
-ORDER BY DOCUMENT_ID ASC
-""")
-
-# Set the batch size
-BATCH_SIZE = 10
-
-# Track progress
-documents_processed = 0
-last_document_id = 0
-
-try:
-    with tqdm(total=application_count, unit="file", desc="Uploading files to S3") as pbar:
-        while True:
-            # Fetch the next batch of BLOB data
-            data = cursor.fetchmany(BATCH_SIZE)
-            if not data:
-                break
-            # Upload the batch to S3 with a progress bar
-            for document_id, application_id, filename, file, code, description, source, created, updated, revision, length in data:
-                tqdm.write(f"{application_id}/{document_id}_{filename}")
-
-                with tqdm(total=length, unit="B", unit_scale=True, desc=filename) as pbar2:
-                    s3.upload_fileobj(file, ecs_bucket, f"migrate/application/{application_id}/{document_id}_{filename}",
-                        Callback=lambda bytes_transferred: pbar2.update(bytes_transferred),)
-                pbar.update(1)
-                last_document_id = document_id
-                documents_processed += 1
-
-except Exception as e:
-    print("Something went wrong:",e)
-    print("Processed", documents_processed,  "files")
-    
-    # Set resume status in case of interuption
-    with open('last-file.pickle', 'wb') as file:
-        pickle.dump(last_document_id, file)
-    
-    cursor.close()
-    conn.close()
-    exit()
-
-# Display results
-print("Process complete: Successfully migrated", documents_processed, "files.")
-
-try:
-    cursor.execute ('SELECT COUNT(*) FROM OATS.OATS_DOCUMENTS WHERE dbms_lob.getLength(DOCUMENT_BLOB) > 0 AND PLANNING_REVIEW_ID IS NOT NULL')
-except cx_Oracle.Error as e:
-    error, = e.args
-    print(error.message)
-
-planning_review_count = cursor.fetchone()[0]
-print('Count =', planning_review_count)
-
-# # Execute the SQL query to retrieve the BLOB data and key column
-cursor.execute(f"""
-SELECT DOCUMENT_ID, PLANNING_REVIEW_ID, FILE_NAME, DOCUMENT_BLOB, DOCUMENT_CODE, DESCRIPTION, DOCUMENT_SOURCE_CODE, UPLOADED_DATE, WHEN_UPDATED, REVISION_COUNT, dbms_lob.getLength(DOCUMENT_BLOB) as LENGTH
-FROM OATS.OATS_DOCUMENTS
-WHERE dbms_lob.getLength(DOCUMENT_BLOB) > 0
-AND DOCUMENT_ID > {starting_planning_document_id}
-AND PLANNING_REVIEW_ID IS NOT NULL
-ORDER BY DOCUMENT_ID ASC
-""")
-
-# Set the batch size
-BATCH_SIZE = 10
-
-# Track progress
-documents_processed = 0
-last_planning_document_id = 0
-
-try:
-    with tqdm(total=planning_review_count, unit="file", desc="Uploading files to S3") as pbar:
-        while True:
-            # Fetch the next batch of BLOB data
-            data = cursor.fetchmany(BATCH_SIZE)
-            if not data:
-                break
-            # Upload the batch to S3 with a progress bar
-            for document_id, planning_review_id, filename, file, code, description, source, created, updated, revision, length in data:
-                tqdm.write(f"{planning_review_id}/{document_id}_{filename}")
-
-                with tqdm(total=length, unit="B", unit_scale=True, desc=filename) as pbar2:
-                    s3.upload_fileobj(file, ecs_bucket, f"migrate/planning_review/{planning_review_id}/{document_id}_{filename}",
-                        Callback=lambda bytes_transferred: pbar2.update(bytes_transferred),)
-                pbar.update(1)
-                last_planning_document_id = document_id
-                documents_processed += 1
-
-except Exception as e:
-    print("Something went wrong:",e)
-    print("Processed", documents_processed,  "files")
-    
-    # Set resume status in case of interuption
-    with open('last-planning-file.pickle', 'wb') as file:
-        pickle.dump(last_planning_document_id, file)
-    
-    cursor.close()
-    conn.close()
-    exit()
-
-# Display results
-print("Process complete: Successfully migrated", documents_processed, "files.")
-
-try:
-    cursor.execute ('SELECT COUNT(*) FROM OATS.OATS_DOCUMENTS WHERE dbms_lob.getLength(DOCUMENT_BLOB) > 0 AND ISSUE_ID IS NOT NULL')
-except cx_Oracle.Error as e:
-    error, = e.args
-    print(error.message)
-
-issue_count = cursor.fetchone()[0]
-print('Count =', issue_count)
-
-# # Execute the SQL query to retrieve the BLOB data and key column
-cursor.execute(f"""
-SELECT DOCUMENT_ID, ISSUE_ID, FILE_NAME, DOCUMENT_BLOB, DOCUMENT_CODE, DESCRIPTION, DOCUMENT_SOURCE_CODE, UPLOADED_DATE, WHEN_UPDATED, REVISION_COUNT, dbms_lob.getLength(DOCUMENT_BLOB) as LENGTH
-FROM OATS.OATS_DOCUMENTS
-WHERE dbms_lob.getLength(DOCUMENT_BLOB) > 0
-AND DOCUMENT_ID > {starting_issue_document_id}
-AND ISSUE_ID IS NOT NULL
-ORDER BY DOCUMENT_ID ASC
-""")
-
-# Set the batch size
-BATCH_SIZE = 10
-
-# Track progress
-documents_processed = 0
-last_issue_document_id = 0
-
-try:
-    with tqdm(total=issue_count, unit="file", desc="Uploading files to S3") as pbar:
-        while True:
-            # Fetch the next batch of BLOB data
-            data = cursor.fetchmany(BATCH_SIZE)
-            if not data:
-                break
-            # Upload the batch to S3 with a progress bar
-            for document_id, issue_id, filename, file, code, description, source, created, updated, revision, length in data:
-                tqdm.write(f"{issue_id}/{document_id}_{filename}")
-
-                with tqdm(total=length, unit="B", unit_scale=True, desc=filename) as pbar2:
-                    s3.upload_fileobj(file, ecs_bucket, f"migrate/issue/{issue_id}/{document_id}_{filename}",
-                        Callback=lambda bytes_transferred: pbar2.update(bytes_transferred),)
-                pbar.update(1)
-                last_issue_document_id = document_id
-                documents_processed += 1
-
-except Exception as e:
-    print("Something went wrong:",e)
-    print("Processed", documents_processed,  "files")
-    
-    # Set resume status in case of interuption
-    with open('last-issue-file.pickle', 'wb') as file:
-        pickle.dump(last_issue_document_id, file)
-    
-    cursor.close()
-    conn.close()
-    exit()
-
-# Display results
-print("Process complete: Successfully migrated", documents_processed, "files.")
+if starting_issue_document_id > 0:
+    issue_docs(starting_issue_document_id,batch_size)
+elif starting_planning_document_id > 0:
+    planning_docs(starting_planning_document_id,batch_size)
+    issue_docs(0,batch_size)
+else:
+    print('Starting applications from:', starting_document_id)
+    application_docs(starting_document_id,batch_size)
+    planning_docs(0,batch_size)
+    issue_docs(0,batch_size)
 
 # Close the database cursor and connection
 cursor.close()
