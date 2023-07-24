8665bf476dc00b9b6f2a23dd3aea7d53055db824
Sandra To <sandra.to.e@gmail.com>
2023-07-24 12:36:54 -0700

    Update inclusion portal frontend test

--- portal-frontend/src/app/features/edit-submission/proposal/incl-proposal/incl-proposal.component.spec.ts
+++ portal-frontend/src/app/features/edit-submission/proposal/incl-proposal/incl-proposal.component.spec.ts
@@ -1,19 +1,44 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialog } from '@angular/material/dialog';
+import { DeepMocked } from '@golevelup/ts-jest';
+import { BehaviorSubject } from 'rxjs';
 import { InclProposalComponent } from './incl-proposal.component';
+import { ApplicationSubmissionService } from '../../../../services/application-submission/application-submission.service';
+import { ApplicationDocumentService } from '../../../../services/application-document/application-document.service';
+import { ApplicationSubmissionDetailedDto } from '../../../../services/application-submission/application-submission.dto';
+import { ApplicationDocumentDto } from '../../../../services/application-document/application-document.dto';
 
 describe('InclProposalComponent', () => {
   let component: InclProposalComponent;
   let fixture: ComponentFixture<InclProposalComponent>;
+  let mockApplicationService: DeepMocked<ApplicationSubmissionService>;
+  let mockAppDocumentService: DeepMocked<ApplicationDocumentService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InclProposalComponent ]
-    })
-    .compileComponents();
+      providers: [
+        {
+          provide: ApplicationSubmissionService,
+          useValue: mockApplicationService,
+        },
+        {
+          provide: ApplicationDocumentService,
+          useValue: mockAppDocumentService,
+        },
+        {
+          provide: MatDialog,
+          useValue: {},
+        },
+      ],
+      declarations: [InclProposalComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(InclProposalComponent);
     component = fixture.componentInstance;
+    component.$applicationSubmission = new BehaviorSubject<ApplicationSubmissionDetailedDto | undefined>(undefined);
+    component.$applicationDocuments = new BehaviorSubject<ApplicationDocumentDto[]>([]);
     fixture.detectChanges();
   });
 
