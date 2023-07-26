4dbdacce3ae65fd7014687514f4fede2efc8b283
Daniel Haselhan <daniel@bit3.ca>
2023-07-26 15:27:22 -0700

    Add inline ALR Area editing

--- alcs-frontend/src/app/features/application/decision/decision-v2/decision-component/subd/subd.component.ts
+++ alcs-frontend/src/app/features/application/decision/decision-v2/decision-component/subd/subd.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ApplicationDecisionComponentService } from '../../../../../../services/application/decision/application-decision-v2/application-decision-component/application-decision-component.service';
 import { DecisionComponentDto } from '../../../../../../services/application/decision/application-decision-v2/application-decision-v2.dto';
 
 @Component({
@@ -7,5 +8,20 @@ import { DecisionComponentDto } from '../../../../../../services/application/dec
   styleUrls: ['./subd.component.scss'],
 })
 export class SubdComponent {
+  constructor(private componentService: ApplicationDecisionComponentService) {}
+
   @Input() component!: DecisionComponentDto;
+
+  async onSaveAlrArea(i: number, alrArea: string | null) {
+    const lots = this.component.subdApprovedLots;
+    if (lots && this.component.uuid) {
+      lots[i].alrArea = alrArea ? parseFloat(alrArea) : null;
+      debugger;
+      await this.componentService.update(this.component.uuid, {
+        uuid: this.component.uuid,
+        subdApprovedLots: lots,
+        applicationDecisionComponentTypeCode: this.component.applicationDecisionComponentTypeCode,
+      });
+    }
+  }
 }
