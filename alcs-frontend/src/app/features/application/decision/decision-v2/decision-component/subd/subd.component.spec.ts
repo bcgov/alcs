3bf5812a77ad147fac736a1b76403f4f7162c6ba
Daniel Haselhan <daniel@bit3.ca>
2023-07-26 15:27:22 -0700

    Add inline ALR Area editing

--- alcs-frontend/src/app/features/application/decision/decision-v2/decision-component/subd/subd.component.spec.ts
+++ alcs-frontend/src/app/features/application/decision/decision-v2/decision-component/subd/subd.component.spec.ts
@@ -1,4 +1,7 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { ApplicationDecisionComponentService } from '../../../../../../services/application/decision/application-decision-v2/application-decision-component/application-decision-component.service';
 import { DecisionComponentDto } from '../../../../../../services/application/decision/application-decision-v2/application-decision-v2.dto';
 
 import { SubdComponent } from './subd.component';
@@ -6,10 +9,20 @@ import { SubdComponent } from './subd.component';
 describe('PfrsComponent', () => {
   let component: SubdComponent;
   let fixture: ComponentFixture<SubdComponent>;
+  let mockAppDecService: DeepMocked<ApplicationDecisionComponentService>;
 
   beforeEach(async () => {
+    mockAppDecService = createMock();
+
     await TestBed.configureTestingModule({
       declarations: [SubdComponent],
+      providers: [
+        {
+          provide: ApplicationDecisionComponentService,
+          useValue: mockAppDecService,
+        },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SubdComponent);
