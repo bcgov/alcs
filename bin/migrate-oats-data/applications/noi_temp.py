26ba3c9ab7ecbbb5ca84ab2cb28223c2a96ac231
Liam Stoddard <lstodd@protonmail.com>
2023-07-28 09:21:53 -0700

    commit python changes

---
+++ bin/migrate-oats-data/applications/noi_temp.py
@@ -0,0 +1,4 @@
+
+LG_DICT = [ {"key" : 'Islands Trust Gabriola Island', "value" :  'Islands Trust Gabriola Island (Historical)'},
+ {"key" : 'Islands Trust Galiano Island', "value" :  'Islands Trust Galiano Island (Historical)'},
+ {"key" : 'Islands Trust Gambier Island', "value" :  'Islands Trust Gambier Island (Historical)'}]
--- bin/migrate-oats-data/noi/sql/insert_noi.sql
+++ bin/migrate-oats-data/noi/sql/insert_noi.sql
@@ -1,26 +1,21 @@
 WITH
-    applicant_lookup AS (
-        SELECT DISTINCT
-            oaap.alr_application_id AS application_id,
-            string_agg (DISTINCT oo.organization_name, ', ') FILTER (
-                WHERE
-                    oo.organization_name IS NOT NULL
-            ) AS orgs,
-            string_agg (
-                DISTINCT concat (op.first_name || ' ' || op.last_name),
-                ', '
-            ) FILTER (
-                WHERE
-                    op.last_name IS NOT NULL
-                    OR op.first_name IS NOT NULL
-            ) AS persons
+    noi_grouped AS (
+        SELECT
+            oaac.alr_application_id
         FROM
-            oats.oats_alr_application_parties oaap
-            LEFT JOIN oats.oats_person_organizations opo ON oaap.person_organization_id = opo.person_organization_id
-            LEFT JOIN oats.oats_persons op ON op.person_id = opo.person_id
-            LEFT JOIN oats.oats_organizations oo ON opo.organization_id = oo.organization_id
+            oats.oats_alr_appl_components oaac
+            JOIN oats.oats_alr_applications oaa ON oaa.alr_application_id = oaac.alr_application_id
         WHERE
-            oaap.alr_appl_role_code = 'APPL'
+            oaa.application_class_code IN ('NOI')
         GROUP BY
-            oaap.alr_application_id
-    ),
+            oaac.alr_application_id
+        HAVING
+            count(oaac.alr_application_id) < 2 -- ignore all applications wit multiple components
+    )
+SELECT
+    oaa.alr_application_id,
+    oaac.alr_change_code
+FROM
+    noi_grouped noi
+    JOIN oats.oats_alr_appl_components oaac ON oaac.alr_application_id = noi.alr_application_id
+    JOIN oats.oats_alr_applications oaa ON oaa.alr_application_id = noi.alr_application_id
