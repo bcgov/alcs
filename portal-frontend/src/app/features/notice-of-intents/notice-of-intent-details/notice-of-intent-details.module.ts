dd5610bc1ba9b06a24bf409bca07dfb90ad1b7fe
mhuseinov <61513701+mhuseinov@users.noreply.github.com>
2023-08-15 13:02:27 -0700

    Feature/alcs 980 step 6 on step 8 (#879)
    * step 6 on step 8 for roso and some fixes
    
    * unit test

--- portal-frontend/src/app/features/notice-of-intents/notice-of-intent-details/notice-of-intent-details.module.ts
+++ portal-frontend/src/app/features/notice-of-intents/notice-of-intent-details/notice-of-intent-details.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from '../../../shared/shared.module';
 import { NoticeOfIntentDetailsComponent } from './notice-of-intent-details.component';
 import { ParcelComponent } from './parcel/parcel.component';
 import { RosoDetailsComponent } from './roso-details/roso-details.component';
+import { RosoAdditionalInformationComponent } from './roso-details/roso-additional-information/roso-additional-information.component';
 
 @NgModule({
-  declarations: [ParcelComponent, RosoDetailsComponent, NoticeOfIntentDetailsComponent],
+  declarations: [ParcelComponent, RosoDetailsComponent, NoticeOfIntentDetailsComponent, RosoAdditionalInformationComponent],
   imports: [CommonModule, SharedModule, NgxMaskPipe],
   exports: [NoticeOfIntentDetailsComponent],
 })
