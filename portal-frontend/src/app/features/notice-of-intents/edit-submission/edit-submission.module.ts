a5ade17f896ff10f69f9b48820f49cc72a531583
Daniel Haselhan <daniel@bit3.ca>
2023-09-07 10:39:52 -0700

    Trigger CanDeactivate  when URL has no Step Index

--- portal-frontend/src/app/features/notice-of-intents/edit-submission/edit-submission.module.ts
+++ portal-frontend/src/app/features/notice-of-intents/edit-submission/edit-submission.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   {
     path: '',
     component: EditSubmissionComponent,
+    canDeactivate: [CanDeactivateGuard],
   },
   {
     path: ':stepInd',
--- portal-frontend/src/app/features/notifications/edit-submission/edit-submission.module.ts
+++ portal-frontend/src/app/features/notifications/edit-submission/edit-submission.module.ts
@@ -23,6 +23,7 @@ const routes: Routes = [
   {
     path: '',
     component: EditSubmissionComponent,
+    canDeactivate: [CanDeactivateGuard],
   },
   {
     path: ':stepInd',
