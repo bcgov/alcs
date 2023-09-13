90bc57495fd046b5823e26221be973eaee42e18f
Mekhti <mekhti.huseinov.ssi@gmail.com>
2023-09-13 15:47:32 -0700

    allow running file import with different parameters

--- bin/migrate-files/migrate-files.py
+++ bin/migrate-files/migrate-files.py
@@ -1,7 +1,5 @@
-import os
 import cx_Oracle
 import boto3
-import pickle
 from config import (
     db_host,
     db_port,
@@ -15,6 +13,8 @@ from config import (
 from application_docs import import_application_docs
 from planning_docs import import_planning_review_docs
 from issue_docs import import_issue_docs
+import argparse
+import sys
 
 # Connect to the Oracle database
 # db_path = os.getenv("DB_PATH") # only necessary if running on M1
@@ -33,41 +33,34 @@ s3 = boto3.client(
     use_ssl=True,
     endpoint_url=ecs_host,
 )
-
-starting_document_id = 0
-# Determine job resume status
-if os.path.isfile("last-file.pickle"):
-    with open("last-file.pickle", "rb") as file:
-        starting_document_id = pickle.load(file)
-    print("Starting applications from:", starting_document_id)
-
-starting_planning_document_id = 0
-# Determine job resume status
-if os.path.isfile("last-planning-file.pickle"):
-    with open("last-planning-file.pickle", "rb") as file:
-        starting_planning_document_id = pickle.load(file)
-    print("Starting planning_reviews from:", starting_planning_document_id)
-
-starting_issue_document_id = 0
-# Determine job resume status
-if os.path.isfile("last-issue-file.pickle"):
-    with open("last-issue-file.pickle", "rb") as file:
-        starting_issue_document_id = pickle.load(file)
-    print("Starting issues from:", starting_issue_document_id)
-
 cursor = conn.cursor()
 
 # Set batch size
 batch_size = 1000
 
-import_application_docs(batch_size, cursor, conn, s3)
 
-import_planning_review_docs(batch_size, cursor, conn, s3)
+def main(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "document_type",
+        choices=["application", "planning","issue"],
+        help="Document type to be processed",
+    )
+    args = parser.parse_args(args)
 
-import_issue_docs(batch_size, cursor, conn, s3)
+    if args.document_type == "application":
+        import_application_docs(batch_size, cursor, conn, s3)
+    elif args.document_type == "planning":
+        import_planning_review_docs(batch_size, cursor, conn, s3)
+    elif args.document_type == "issue":
+        import_issue_docs(batch_size, cursor, conn, s3)
+        
+    print("File upload complete, closing connection")
 
-print("File upload complete, closing connection")
+    # Close the database cursor and connection
+    cursor.close()
+    conn.close()
 
-# Close the database cursor and connection
-cursor.close()
-conn.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
