b5eddf82ed731df724576ea096f01b169e87bfdc
Mekhti <mekhti.huseinov.ssi@gmail.com>
2023-09-18 13:49:46 -0700

    set email_sent date on statuses for submission imported by etl

---
+++ bin/migrate-oats-data/applications/submission_status_email/submission_status_email.py
@@ -0,0 +1,40 @@
+from db import inject_conn_pool
+import traceback
+from common import OATS_ETL_USER
+
+
+@inject_conn_pool
+def process_application_submission_status_emails(conn=None):
+    update_query = f"""
+                        UPDATE application_submission_to_submission_status status
+                        SET email_sent_date = '0001-01-01 01:00:00.000 -0700'
+                        FROM application_submission app_sub
+                        WHERE status.submission_uuid = app_sub.uuid 
+                            AND app_sub.audit_created_by = {OATS_ETL_USER}
+                    """
+
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(update_query)
+    except Exception as error:
+        print("".join(traceback.format_exception(None, error, error.__traceback__)))
+        cursor.close()
+        conn.close()
+
+
+@inject_conn_pool
+def clean_application_submission_status_emails(conn=None):
+    update_query = f"""
+                        UPDATE application_submission_to_submission_status status
+                        SET email_sent_date = NULL
+                        FROM application_submission app_sub
+                        WHERE status.submission_uuid = app_sub.uuid 
+                            AND app_sub.audit_created_by = {OATS_ETL_USER}
+                    """
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(update_query)
+    except Exception as error:
+        print("".join(traceback.format_exception(None, error, error.__traceback__)))
+        cursor.close()
+        conn.close()
--- bin/migrate-oats-data/common/__init__.py
+++ bin/migrate-oats-data/common/__init__.py
@@ -1,3 +1,4 @@
 from .oats_application_code_values import *
 from .alcs_application_enum import *
 from .etl_logger import log_start, log_end
+from .constants import *
