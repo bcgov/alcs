bc965918558c8393e6ca4658dc61a9242da43934
Mekhti <mekhti.huseinov.ssi@gmail.com>
2023-11-03 15:53:33 -0700

    certificate of title import for noi

--- bin/migrate-oats-data/noi/notice_of_intent_submissions/parcels/__init__.py
+++ bin/migrate-oats-data/noi/notice_of_intent_submissions/parcels/__init__.py
@@ -2,3 +2,6 @@ from .notice_of_intent_parcel_insert import (
     init_notice_of_intent_parcels,
     clean_parcels,
 )
+from .notice_of_intent_certificate_of_title import (
+    process_notice_of_intent_certificate_of_title,
+)
