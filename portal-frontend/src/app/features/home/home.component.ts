9e860b9e34b03c8049412148eba5cb79b2f495d9
Tristan Slater <hello@trslater.ca>
2023-11-28 09:25:17 -0800

    Set current tab name for home component from URL

--- portal-frontend/src/app/features/home/home.component.ts
+++ portal-frontend/src/app/features/home/home.component.ts
@@ -5,6 +5,7 @@ import { UserDto } from '../../services/authentication/authentication.dto';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
 import { MOBILE_BREAKPOINT } from '../../shared/utils/breakpoints';
 import { CreateSubmissionDialogComponent } from '../create-submission-dialog/create-submission-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -17,13 +18,23 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.isMobile = window.innerWidth <= MOBILE_BREAKPOINT;
   }
 
+  currentTabName: string = '';
+
   $destroy = new Subject<void>();
   isMobile = false;
   profile: UserDto | undefined;
 
-  constructor(private authenticationService: AuthenticationService, private dialog: MatDialog) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private dialog: MatDialog,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.currentTabName = params['submissionType'];
+    });
+
     this.authenticationService.$currentProfile.pipe(takeUntil(this.$destroy)).subscribe((profile) => {
       this.profile = profile;
     });
