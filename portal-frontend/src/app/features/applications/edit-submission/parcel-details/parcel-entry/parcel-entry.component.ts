46ac34f01edd1b8cf7f7d9dd57ee6b646ef0d776
Urmi Kataria <urmi.kataria@gov.bc.ca>
2023-11-27 16:19:14 -0800

    parcel owner ux changes

--- portal-frontend/src/app/features/applications/edit-submission/parcel-details/parcel-entry/parcel-entry.component.ts
+++ portal-frontend/src/app/features/applications/edit-submission/parcel-details/parcel-entry/parcel-entry.component.ts
@@ -342,7 +342,7 @@ export class ParcelEntryComponent implements OnInit {
         this.updateParcelOwners(selectedOwners);
       }
     } else {
-      event.preventDefault()
+      this.onOwnerRemoved(owner.uuid);
     }
   }
 
