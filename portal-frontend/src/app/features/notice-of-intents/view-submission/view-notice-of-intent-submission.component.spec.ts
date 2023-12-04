488f377f8b40506e3a5efba57e4f959c9f074dc3
Tristan Slater <hello@trslater.ca>
2023-12-04 11:48:23 -0800

    Fix NOI view component failing 'should create'
    Was NOI view mock was missing needed dialog
    provider.

--- portal-frontend/src/app/features/notice-of-intents/view-submission/view-notice-of-intent-submission.component.spec.ts
+++ portal-frontend/src/app/features/notice-of-intents/view-submission/view-notice-of-intent-submission.component.spec.ts
@@ -2,6 +2,7 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { ConfirmationDialogService } from '../../../shared/confirmation-dialog/confirmation-dialog.service';
 import { BehaviorSubject } from 'rxjs';
 import { NoticeOfIntentDocumentService } from '../../../services/notice-of-intent-document/notice-of-intent-document.service';
 import { NoticeOfIntentSubmissionService } from '../../../services/notice-of-intent-submission/notice-of-intent-submission.service';
@@ -15,6 +16,7 @@ describe('ViewNoticeOfIntentSubmissionComponent', () => {
   let mockNoiSubmissionService: DeepMocked<NoticeOfIntentSubmissionService>;
   let mockNoiDocumentService: DeepMocked<NoticeOfIntentDocumentService>;
   let mockActivatedRoute: DeepMocked<any>;
+  let mockDialogService: DeepMocked<ConfirmationDialogService>;
 
   beforeEach(async () => {
     mockNoiSubmissionService = createMock();
@@ -37,6 +39,10 @@ describe('ViewNoticeOfIntentSubmissionComponent', () => {
         {
           provide: ActivatedRoute,
           useValue: mockActivatedRoute,
+        },
+        {
+          provide: ConfirmationDialogService,
+          useValue: mockDialogService,
         },
       ],
       schemas: [NO_ERRORS_SCHEMA],
