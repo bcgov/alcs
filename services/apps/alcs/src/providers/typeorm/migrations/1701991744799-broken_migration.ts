31d345f1832e54bca6b37155c31a90fceeee73d9
Daniel Haselhan <daniel@bit3.ca>
2023-12-07 17:31:16 -0800

    Revert "Add a broken migration to test alerting"
    This reverts commit c6d0a53cfe7aaec41ceb4f863d999d38296937ca.

--- services/apps/alcs/src/providers/typeorm/migrations/1701991744799-broken_migration.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class BrokenMigration1701991744799 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`SELECT * FROM nonexistingtable`);
-  }
-
-  public async down(): Promise<void> {}
-}
