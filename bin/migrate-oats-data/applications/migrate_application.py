6ee0a917825ff1823a3bf746943489b4ecfbe9a7
Mekhti <mekhti.huseinov.ssi@gmail.com>
2024-01-04 13:04:19 -0800

    revert commented code

--- bin/migrate-oats-data/applications/migrate_application.py
+++ bin/migrate-oats-data/applications/migrate_application.py
@@ -31,26 +31,24 @@ from .decisions.app_modifications import (
 
 
 def process_application_etl(batch_size):
-    # TODO uncomment this
-    # process_alcs_application_prep_fields(batch_size)
-    # update_application_date_rx_all_items(batch_size)
-    # process_alcs_app_submissions(batch_size)
-    # process_application_statuses(batch_size)
-    # process_application_parcels(batch_size)
-    # process_application_owners(batch_size)
-    # process_app_staff_journal(batch_size)
+    process_alcs_application_prep_fields(batch_size)
+    update_application_date_rx_all_items(batch_size)
+    process_alcs_app_submissions(batch_size)
+    process_application_statuses(batch_size)
+    process_application_parcels(batch_size)
+    process_application_owners(batch_size)
+    process_app_staff_journal(batch_size)
     process_application_decisions(batch_size)
-    # process_application_submission_status_emails()
+    process_application_submission_status_emails()
 
 
 def clean_alcs_applications():
-    # TODO uncomment this
-    # clean_application_submission_status_emails()
-    # clean_application_staff_journal()
-    # clean_application_parcels()
+    clean_application_submission_status_emails()
+    clean_application_staff_journal()
+    clean_application_parcels()
     clean_application_decisions_etl()
-    # clean_app_submissions()
-    # clean_applications()
+    clean_app_submissions()
+    clean_applications()
 
 
 def init_applications(batch_size):
@@ -58,7 +56,7 @@ def init_applications(batch_size):
 
 
 def process_application_decisions(batch_size):
-    # init_application_decisions(batch_size)
+    init_application_decisions(batch_size)
     init_application_modifications(batch_size)
     update_application_modifications(batch_size)
     link_application_modifications(batch_size)
@@ -67,7 +65,7 @@ def process_application_decisions(batch_size):
 def clean_application_decisions_etl():
     unlink_application_modifications()
     clean_application_modifications()
-    # clean_application_decisions()
+    clean_application_decisions()
 
 
 def clean_application_parcels():
