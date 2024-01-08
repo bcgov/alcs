6d517b45a2a669f5e31e9173ee84d08395108534
to. sandra <76515860+sandratoh@users.noreply.github.com>
2024-01-08 10:26:12 -0800

    Update application proposals, reviews, and dialog (#1263)
    * Create shared purposes panel and add to soil applications
    
    * Update app confirmation dialog text and styling
    
    * Add units to application proposal review questions
    
    * Replace repeated code with shared panel component
    
    * Update condition for 0 lot size

---
+++ portal-frontend/src/app/shared/commission-purposes-panel/commission-purposes-panel.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commission-purposes-panel',
+  templateUrl: './commission-purposes-panel.component.html',
+  styleUrls: ['./commission-purposes-panel.component.scss'],
+})
+export class CommissionPurposesPanelComponent {}
