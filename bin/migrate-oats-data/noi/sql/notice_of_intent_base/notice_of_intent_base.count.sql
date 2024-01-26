8b063fdccb355f226aa2c2a74b2f74a99e8ee67f
mhuseinov <61513701+mhuseinov@users.noreply.github.com>
2024-01-25 17:09:09 -0800

    Bugfix/alcs 1550 duplicated parcels (#1342)
    Instead of using application_id (file_number) use application_component_id (submission_id)

--- bin/migrate-oats-data/noi/sql/notice_of_intent_base/notice_of_intent_base.count.sql
+++ bin/migrate-oats-data/noi/sql/notice_of_intent_base/notice_of_intent_base.count.sql
@@ -1,5 +1,5 @@
 WITH nois_with_one_or_zero_component_only AS (
-    SELECT oaa.alr_application_id
+    SELECT *
     FROM oats.alcs_etl_applications_nois oaa
     WHERE oaa.application_class_code = 'NOI'
         and oaa.alr_change_code <> 'SRW'
@@ -9,4 +9,4 @@ FROM alcs.notice_of_intent noi
     JOIN nois_with_one_or_zero_component_only oats_noi ON oats_noi.alr_application_id::TEXT = noi.file_number
     JOIN oats.oats_alr_applications oaa ON oaa.alr_application_id = oats_noi.alr_application_id
     AND oaa.application_class_code = 'NOI'
-    JOIN oats.oats_alr_appl_components oaac ON oaac.alr_application_id = oats_noi.alr_application_id;
+    JOIN oats.oats_alr_appl_components oaac ON oaac.alr_appl_component_id = oats_noi.alr_appl_component_id;
