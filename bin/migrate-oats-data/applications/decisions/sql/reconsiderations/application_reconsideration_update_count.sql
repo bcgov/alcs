1516e46aaab61abc4949d932880124f96068a7af
Mekhti <mekhti.huseinov.ssi@gmail.com>
2024-02-02 15:46:54 -0800

    update count queries for application/noi reconsideration and modifications

--- bin/migrate-oats-data/applications/decisions/sql/reconsiderations/application_reconsideration_update_count.sql
+++ bin/migrate-oats-data/applications/decisions/sql/reconsiderations/application_reconsideration_update_count.sql
@@ -1,19 +1,48 @@
 WITH alcs_reconsiderations_resulting_decisions AS (
-    SELECT appr."uuid" AS reconsideration_uuid,
+    SELECT appr."uuid" as reconsideration_uuid,
         appr.submitted_date::DATE AS submitted_date,
         appd.resolution_number,
         appd.resolution_year,
         appd.oats_alr_appl_decision_id,
-        appr.audit_created_by
+        appr.audit_created_by,
+        app.file_number
     FROM alcs.application_reconsideration appr
-        JOIN alcs.application_decision appd ON appd.reconsiders_uuid = appr."uuid"
+        LEFT JOIN alcs.application_decision appd ON appd.reconsiders_uuid = appr."uuid"
         JOIN alcs.application app ON app."uuid" = appr.application_uuid
+),
+alcs_modifications AS (
+    SELECT appm."uuid",
+        appm.submitted_date::DATE AS submitted_date,
+        appd.resolution_number,
+        appd.resolution_year,
+        appd.oats_alr_appl_decision_id
+    FROM alcs.application_modification appm
+        LEFT JOIN alcs.application_decision appd ON appd.reconsiders_uuid = appm."uuid"
+        JOIN alcs.application app ON app."uuid" = appm.application_uuid
 )
 SELECT count(*)
 FROM oats.oats_reconsideration_requests orr
-    JOIN oats.oats_alr_appl_decisions oaad ON oaad.reconsideration_request_id = orr.reconsideration_request_id
-    JOIN alcs.application app ON app.file_number = oaad.alr_application_id::TEXT
-    OR app.file_number = orr.alr_application_id::TEXT -- this is required to get oats_recons only for apps
-    JOIN alcs_reconsiderations_resulting_decisions AS arrd ON arrd.oats_alr_appl_decision_id = oaad.alr_appl_decision_id
-    AND arrd.submitted_date = orr.received_date::DATE -- this will make sure that only reconsiderations that exist in oats will be selected
+    LEFT JOIN oats.oats_alr_appl_decisions oaad ON oaad.reconsideration_request_id = orr.reconsideration_request_id
+    JOIN alcs.application app ON app.file_number = orr.alr_application_id::TEXT -- this IS required to get oats_recons only for apps
+    LEFT JOIN alcs_reconsiderations_resulting_decisions AS arrd ON (
+        (
+            arrd.oats_alr_appl_decision_id = oaad.alr_appl_decision_id
+            AND arrd.submitted_date = orr.received_date::DATE
+        )
+        OR (
+            arrd.submitted_date = orr.received_date::DATE
+            AND arrd.file_number = orr.alr_application_id::TEXT
+        )
+    )
+    LEFT JOIN alcs_modifications AS appm ON (
+        (
+            appm.oats_alr_appl_decision_id = oaad.alr_appl_decision_id
+            AND appm.submitted_date = orr.received_date::DATE
+        )
+        OR (
+            appm.submitted_date = orr.received_date::DATE
+            AND arrd.file_number = orr.alr_application_id::TEXT
+        )
+    )
 WHERE arrd.audit_created_by <> 'oats_etl'
+    AND appm."uuid" IS NULL
