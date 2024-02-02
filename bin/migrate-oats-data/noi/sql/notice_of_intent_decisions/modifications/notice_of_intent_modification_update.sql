fbc6e007c3d66c28f4ffff3e8e5b405f588ecf67
Mekhti <mekhti.huseinov.ssi@gmail.com>
2024-02-02 13:44:56 -0800

    noi modifications

--- bin/migrate-oats-data/noi/sql/notice_of_intent_decisions/modifications/notice_of_intent_modification_update.sql
+++ bin/migrate-oats-data/noi/sql/notice_of_intent_decisions/modifications/notice_of_intent_modification_update.sql
@@ -4,6 +4,7 @@ WITH alcs_modifications_resulting_decisions AS (
         noid.resolution_number,
         noid.resolution_year,
         noid.oats_alr_appl_decision_id,
+        noi.file_number,
         noim.audit_created_by
     FROM alcs.notice_of_intent_modification noim
         LEFT JOIN alcs.notice_of_intent_decision noid ON noid.modifies_uuid = noim."uuid" -- TODO will remove this comment after PROD QA with Ansley
@@ -15,11 +16,19 @@ SELECT orr.reconsideration_request_id,
     noi."uuid" AS noi_uuid,
     orr.description,
     orr.alr_application_id,
-    amrd.modification_uuid
+    amrd.uuid AS modification_uuid
 FROM oats.oats_reconsideration_requests orr
     LEFT JOIN oats.oats_alr_appl_decisions oaad ON oaad.reconsideration_request_id = orr.reconsideration_request_id
-    JOIN alcs.notice_of_intent noi ON noi.file_number = oaad.alr_application_id::TEXT
-    OR noi.file_number = orr.alr_application_id::TEXT -- this IS required to get oats_recons only for nois
-    LEFT JOIN alcs_modifications_resulting_decisions AS amrd ON amrd.oats_alr_appl_decision_id = oaad.alr_appl_decision_id
-    AND amrd.submitted_date = orr.received_date::DATE
-WHERE amrd.audit_created_by <> 'oats_etl'
+    JOIN alcs.notice_of_intent noi ON noi.file_number = orr.alr_application_id::TEXT -- this IS required to get oats_recons only for nois
+    LEFT JOIN alcs_modifications_resulting_decisions AS amrd ON (
+        (
+            amrd.oats_alr_appl_decision_id = oaad.alr_appl_decision_id
+            AND amrd.submitted_date = orr.received_date::DATE
+        )
+        OR (
+            amrd.submitted_date = orr.received_date::DATE
+            AND amrd.file_number = orr.alr_application_id::TEXT
+        )
+    )
+WHERE amrd."uuid" IS NOT NULL
+    AND amrd.audit_created_by <> 'oats_etl'
