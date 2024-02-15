8642e52024b90bbf6ece96afb8a22ede31b1d64b
mhuseinov <61513701+mhuseinov@users.noreply.github.com>
2024-02-15 15:30:46 -0800

    execute oats related migrations conditionally (#1434)
    Run oats migrations conditionally. This will ensure that developers who work on local env do not need to introduce oats schema and related infrastructure. All hosted envs have "oats" as part of DB

--- services/apps/alcs/src/providers/typeorm/migrations/1707940567877-add_advanced_parcel_mapping_true.ts
+++ services/apps/alcs/src/providers/typeorm/migrations/1707940567877-add_advanced_parcel_mapping_true.ts
@@ -5,30 +5,34 @@ export class AddAdvancedParcelMappingTrue1707940567877
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        WITH parcels_to_insert AS (
-            SELECT apps."uuid" as submission_uuid
-            FROM alcs.application_submission apps
-                JOIN oats.oats_subject_properties osp ON osp.alr_application_id = apps.file_number::bigint
-            WHERE osp.alr_application_land_ind <> 'Y' -- ensure that only other parcels are selected
-            ),
-            grouped_parcels AS (
-            SELECT *
-            FROM parcels_to_insert pti
-            Group BY pti.submission_uuid)
-        UPDATE 
-            alcs.application_submission
-        SET
-            has_other_parcels_in_community = 'True',
-            other_parcels_description = 'Not migrated from OATS'
-        FROM 
-            grouped_parcels
-        WHERE
-            "uuid" = grouped_parcels.submission_uuid 
-
-        `);
+      DO $$
+          BEGIN
+              IF EXISTS (SELECT schema_name FROM information_schema.schemata WHERE schema_name = 'oats') THEN
+              WITH parcels_to_insert AS (
+                  SELECT apps."uuid" as submission_uuid
+                  FROM alcs.application_submission apps
+                      JOIN oats.oats_subject_properties osp ON osp.alr_application_id = apps.file_number::bigint
+                  WHERE osp.alr_application_land_ind <> 'Y' -- ensure that only other parcels are selected
+                  ),
+                  grouped_parcels AS (
+                  SELECT *
+                  FROM parcels_to_insert pti
+                  Group BY pti.submission_uuid)
+              UPDATE 
+                  alcs.application_submission
+              SET
+                  has_other_parcels_in_community = 'True',
+                  other_parcels_description = 'Not migrated from OATS'
+              FROM 
+                  grouped_parcels
+              WHERE
+                  "uuid" = grouped_parcels.submission_uuid 
+            END IF;
+      END $$;
+    `);
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
+  public async down(): Promise<void> {
     // Do Nothing
   }
 }
