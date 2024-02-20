0bb084cd2c60fc14a2f9f2ca6b7e58418ca524c9
Urmi Kataria <urmi.kataria@gov.bc.ca>
2024-02-20 15:26:07 -0800

    UI enhancements

--- alcs-frontend/src/app/features/home/assigned/assigned-table/assigned-table.component.ts
+++ alcs-frontend/src/app/features/home/assigned/assigned-table/assigned-table.component.ts
@@ -22,11 +22,29 @@ export interface AssignedToMeFile {
 export class AssignedTableComponent {
   @Input() assignedFiles: AssignedToMeFile[] = [];
 
-  displayedColumns = ['highPriority', 'title', 'type', 'activeDays', 'stage'];
+  // displayedColumns = ['highPriority', 'title', 'type', 'activeDays', 'stage'];
 
   constructor(private router: Router) {}
 
   async onSelectCard(card: CardDto) {
     await this.router.navigateByUrl(`/board/${card.boardCode}?card=${card.uuid}&type=${card.type}`);
+  }
+
+  get displayedColumns(): string[] {
+    // Include all default columns
+    const columns = ['highPriority', 'title', 'type', 'activeDays', 'stage'];
+
+    // Check if any file has type 'NOTI'
+    const hasNoti = this.assignedFiles.some(file => file.type === 'NOTI');
+
+    // If 'NOTI' type exists, remove 'activeDays' column
+    if (hasNoti) {
+      const index = columns.indexOf('activeDays');
+      if (index !== -1) {
+        columns.splice(index, 1);
+      }
+    }
+
+    return columns;
   }
 }
