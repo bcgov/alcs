3571c1d347a1fe803f09ddd96720b4186659865e
Liam Stoddard <lstodd@protonmail.com>
2024-02-21 09:49:32 -0800

    update README

--- bin/migrate-oats-data/migrate.py
+++ bin/migrate-oats-data/migrate.py
@@ -8,16 +8,10 @@ from menu import setup_menu_args_parser
 from menu.post_launch_commands import (
     import_all,
     clean_all,
-    document_import,
-    app_document_import,
     application_import,
     application_clean,
-    noi_document_import,
     notice_of_intent_import,
     notice_of_intent_clean,
-    start_obfuscation,
-    import_users,
-    users_clean,
 )
 from db import connection_pool
 from common import BATCH_UPLOAD_SIZE, setup_and_get_logger
@@ -39,10 +33,6 @@ if __name__ == "__main__":
                 import_all(console, args)
             case "clean":
                 clean_all(console, args)
-            case "document-import":
-                document_import(console, args)
-            case "app-document-import":
-                app_document_import(console, args)
             case "noi-import":
                 notice_of_intent_import(console, args)
             case "noi-clean":
@@ -51,14 +41,6 @@ if __name__ == "__main__":
                 application_import(console, args)
             case "application-clean":
                 application_clean(console)
-            case "noi-document-import":
-                noi_document_import(console, args)
-            case "obfuscate":
-                start_obfuscation(console)
-            case "user-import":
-                import_users(console, args)
-            case "user-clean":
-                users_clean(console)
 
     finally:
         if connection_pool:
