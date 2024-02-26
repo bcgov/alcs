3221de57fcaaf4150bd6158ac73edb3fd73a07b6
mhuseinov <61513701+mhuseinov@users.noreply.github.com>
2024-02-26 15:35:47 -0800

    oats srw -> alcs.notification (#1451)
    SRW initial migration
    fully populate alcs.notification except applicant field
    srw import commands

---
+++ bin/migrate-oats-data/srw/post_launch/srw_migration.py
@@ -0,0 +1,15 @@
+from ..srw_base import init_srw_base, clean_initial_srw
+from ..srw_base_update import update_srw_base_fields
+
+
+def process_srw(batch_size):
+    init_srw(batch_size)
+
+
+def init_srw(batch_size):
+    init_srw_base(batch_size)
+    update_srw_base_fields(batch_size)
+
+
+def clean_srw():
+    clean_initial_srw()
