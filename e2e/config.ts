60b7b595ac08b7905e3fc41b16af104a0b45c4ca
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-03-01 13:14:41 -0800

    Clean up env data

--- e2e/config.ts
+++
@@ -1,4 +0,0 @@
-export const baseUrl = 'http://localhost:4201/login';
-export const userName = 'MekhtiHuseinov';
-export const password = 'UQ-ZCDHi.FiF6c';
-export const filePathToUseAsUpload = '/Users/mekhti/Desktop/test_upload_1.png';
