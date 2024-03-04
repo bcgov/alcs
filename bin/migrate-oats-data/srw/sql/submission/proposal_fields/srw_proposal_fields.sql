23e37c194cdb11cf15a79cae5bfd2511e4c0fb79
Mekhti <mekhti.huseinov.ssi@gmail.com>
2024-03-04 09:51:03 -0800

    add type filter

--- bin/migrate-oats-data/srw/sql/submission/proposal_fields/srw_proposal_fields.sql
+++ bin/migrate-oats-data/srw/sql/submission/proposal_fields/srw_proposal_fields.sql
@@ -9,7 +9,7 @@ SELECT oaa.alr_application_id,
     au."uuid",
     oaac.component_area
 FROM oats.oats_alr_applications oaa
-    JOIN alcs.notification_submission nos ON nos.file_number = oaa.alr_application_id::TEXT
+    JOIN alcs.notification_submission nos ON nos.file_number = oaa.alr_application_id::TEXT AND type_code = 'SRW'
     JOIN alcs.notification n ON n.file_number = nos.file_number
     JOIN oats.alcs_etl_srw aes ON aes.alr_application_id = oaa.alr_application_id
     JOIN oats.oats_alr_appl_components oaac ON oaac.alr_appl_component_id = aes.alr_appl_component_id
