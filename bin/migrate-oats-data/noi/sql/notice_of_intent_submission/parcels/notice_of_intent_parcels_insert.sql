460cb9579772cc9d36da525f18789bbe5cb1aefe
Mekhti <mekhti.huseinov.ssi@gmail.com>
2024-03-04 12:59:19 -0800

    revert changes to noi file

--- bin/migrate-oats-data/noi/sql/notice_of_intent_submission/parcels/notice_of_intent_parcels_insert.sql
+++ bin/migrate-oats-data/noi/sql/notice_of_intent_submission/parcels/notice_of_intent_parcels_insert.sql
@@ -1,22 +1,27 @@
 WITH parcels_to_insert AS (
-    SELECT nos.uuid,
+    SELECT nois.uuid,
         osp.subject_property_id
-    FROM alcs.notification_submission nos
-        JOIN oats.oats_subject_properties osp ON osp.alr_application_id = nos.file_number::bigint
-    WHERE osp.alr_application_land_ind = 'Y'
-        AND nos.type_code = 'SRW'
+    FROM alcs.notice_of_intent_submission nois
+        JOIN oats.oats_subject_properties osp ON osp.alr_application_id = nois.file_number::bigint
+    WHERE osp.alr_application_land_ind = 'Y' -- ensure that only parcels related to application are selected
 ),
 grouped_oats_property_interests_ids AS (
-    SELECT subject_property_id
+    SELECT MIN(property_owner_type_code) AS property_owner_type_code,
+        -- min will not affect anything since all property_owner_type_code are the same in scope of subject_property
+        subject_property_id
     FROM oats.oats_property_interests opi
     GROUP BY opi.subject_property_id
 )
-SELECT uuid AS notification_submission_uuid,
+SELECT uuid AS notice_of_intent_submission_uuid,
+    osp.alr_area,
     op.civic_address,
+    osp.farm_land_ind,
     op.legal_description,
     op.area_size,
     op.pid,
     op.pin,
+    osp.purchase_date,
+    gopi.property_owner_type_code,
     osp.subject_property_id,
     op.property_id
 FROM parcels_to_insert pti
