d21672267e4d7d33b4f3c2971eb9b8ab86ac5f0c
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-03-05 13:54:06 -0800

    Replace user/pass env vars with correct name

--- e2e/tests/portal/login.spec.ts
+++ e2e/tests/portal/login.spec.ts
@@ -3,8 +3,10 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('http://localhost:4201/');
   await page.getByRole('button', { name: 'Portal Login' }).click();
-  await page.locator('#user').fill(process.env.FAKE_USERNAME ?? '');
-  await page.getByLabel('Password').fill(process.env.FAKE_PASSWORD ?? '');
+  await page.locator('#user').fill(process.env.BCEID_BASIC_USERNAME ?? '');
+  await page
+    .getByLabel('Password')
+    .fill(process.env.BCEID_BASIC_PASSWORD ?? '');
   await page.getByLabel('Password').press('Enter');
   await page.getByRole('heading', { name: 'Portal Inbox' }).click();
 });
