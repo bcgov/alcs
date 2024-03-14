9befe337113af7efb32222a1cb5205180ed1a184
Daniel Haselhan <daniel@bit3.ca>
2024-03-14 13:14:38 -0700

    Code Review Feedback

--- services/apps/alcs/src/providers/typeorm/migrations/1683048988258-generate_next_resolution_number.ts
+++ services/apps/alcs/src/providers/typeorm/migrations/1683048988258-generate_next_resolution_number.ts
@@ -8,42 +8,33 @@ export class generateNextResolutionNumber1683048988258
       `CREATE OR REPLACE FUNCTION alcs.generate_next_resolution_number(p_resolution_year integer)
           RETURNS integer
           LANGUAGE plpgsql
-      AS $function$
-          declare next_resolution_number integer;
-      BEGIN	
-          select
-            row_num into next_resolution_number
-          from
-            (
+        AS $function$
+          
+            declare next_resolution_number integer;
+          BEGIN	
             select
-              coalesce(resolution_number, 0) as resolution_number,
-              row_number() over (
-              order by resolution_number) row_num
+              row_num into next_resolution_number
             from
               (
-                select resolution_number, audit_deleted_date_at
-                from alcs.application_decision
-                where resolution_year = p_resolution_year
-                UNION
-                select resolution_number, audit_deleted_date_at
-                from alcs.noi_decision
-                where resolution_year = p_resolution_year
-                UNION
-                select resolution_number, audit_deleted_date_at
-                from alcs.planning_review_decision
-                where resolution_year = p_resolution_year
-              ) as combined
+              select
+                coalesce(resolution_number, 0) as resolution_number,
+                row_number() over (
+                order by resolution_number) row_num
+              from
+                alcs.application_decision
+              where
+                resolution_year = p_resolution_year
+                and audit_deleted_date_at is null
+              ) z
             where
-              audit_deleted_date_at is null
-            ) z
-          where
-            row_num != resolution_number
-          order by
-            row_num offset 0 row fetch next 1 row only;
-      
-          return coalesce(next_resolution_number, 1);
-      END;
-      $function$   
+              row_num != resolution_number
+            order by
+              row_num offset 0 row fetch next 1 row only;
+        
+            return coalesce(next_resolution_number, 1);
+          END;
+        $function$
+        ;     
        `,
     );
   }
