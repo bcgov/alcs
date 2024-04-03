999b21bd9ee03675bece67666880e64d5d505df8
Mekhti <mekhti.huseinov.ssi@gmail.com>
2024-04-03 08:41:49 -0700

    typo with quotes

--- services/apps/alcs/src/alcs/application-decision/application-decision-outcome-type/application-decision-outcome-type.entity.ts
+++ services/apps/alcs/src/alcs/application-decision/application-decision-outcome-type/application-decision-outcome-type.entity.ts
@@ -3,6 +3,6 @@ import { BaseCodeEntity } from '../../../common/entities/base.code.entity';
 
 @Entity({
   comment:
-    "Code table for the possible outcomes of the chair''s application decision review",
+    "Code table for the possible outcomes of the chair's application decision review",
 })
 export class ApplicationDecisionChairReviewOutcomeType extends BaseCodeEntity {}
