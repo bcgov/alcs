f39211b34f70df7b212443b147ac8037120561c8
Daniel Haselhan <daniel@bit3.ca>
2024-04-04 15:04:12 -0700

    Bug Fixes
    * Add inquiries to unarchive card service
    * Add error message to PIDs on Parcels page

--- services/apps/alcs/src/alcs/admin/admin.module.ts
+++ services/apps/alcs/src/alcs/admin/admin.module.ts
@@ -1,13 +1,14 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Configuration } from '../../common/entities/configuration.entity';
+import { ApplicationCeoCriterionCode } from '../application-decision/application-ceo-criterion/application-ceo-criterion.entity';
 import { ApplicationDecisionConditionType } from '../application-decision/application-decision-condition/application-decision-condition-code.entity';
 import { ApplicationDecisionMakerCode } from '../application-decision/application-decision-maker/application-decision-maker.entity';
+import { ApplicationDecisionModule } from '../application-decision/application-decision.module';
 import { ApplicationModule } from '../application/application.module';
 import { BoardModule } from '../board/board.module';
 import { CardModule } from '../card/card.module';
-import { ApplicationCeoCriterionCode } from '../application-decision/application-ceo-criterion/application-ceo-criterion.entity';
-import { ApplicationDecisionModule } from '../application-decision/application-decision.module';
+import { InquiryModule } from '../inquiry/inquiry.module';
 import { NoticeOfIntentDecisionModule } from '../notice-of-intent-decision/notice-of-intent-decision.module';
 import { NoticeOfIntentSubtype } from '../notice-of-intent/notice-of-intent-subtype.entity';
 import { NoticeOfIntentModule } from '../notice-of-intent/notice-of-intent.module';
@@ -52,6 +53,7 @@ import { UnarchiveCardService } from './unarchive-card/unarchive-card.service';
     CardModule,
     BoardModule,
     NotificationModule,
+    InquiryModule,
   ],
   controllers: [
     HolidayController,
