b3b7204a3d1b1b8b94ef3b806a6f4f1c16c91cd5
Daniel Haselhan <daniel@bit3.ca>
2024-04-25 15:11:21 -0700

    Move logger to Winston, add s3 stream
    * Move from Pino -> Winston for easier s3 stream transport setup
    * Add s3 stream to store logs in s3 stream bucket
    * Configure stream settings to rotate logs daily

--- services/apps/alcs/src/commands/import.ts
+++ services/apps/alcs/src/commands/import.ts
@@ -1,14 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { Logger } from 'nestjs-pino';
+import winston from 'winston';
 import { ApplicationImportService } from '../alcs/import/application-import.service';
 import { NoticeOfIntentImportService } from '../alcs/import/noi-import.service';
 import { MainModule } from '../main.module';
 
-export async function importApplications() {
+export async function importApplications(logger: winston.Logger) {
   const app = await NestFactory.createApplicationContext(MainModule, {
     bufferLogs: false,
+    logger,
   });
-  app.useLogger(app.get(Logger));
 
   await app.init();
 
