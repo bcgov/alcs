102b4bf6e3e01673d17588b9814664509474aef3
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-04-26 13:59:11 -0700

    Align terminology with Playwright role terminology

--- e2e/tests/pages/portal-login-page.ts
+++ e2e/tests/pages/portal-login-page.ts
@@ -3,16 +3,16 @@ import { type Locator, type Page } from '@playwright/test';
 export class PortalLoginPage {
   readonly page: Page;
   readonly loginButton: Locator;
-  readonly userIdTextField: Locator;
-  readonly passwordTextField: Locator;
+  readonly userIdTextbox: Locator;
+  readonly passwordTextbox: Locator;
   readonly continueButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.loginButton = page.getByRole('button', { name: 'Portal Login' });
     // There is an error with the username label on BCeID page
-    this.userIdTextField = page.getByRole('textbox').nth(0);
-    this.passwordTextField = page.getByLabel('Password');
+    this.userIdTextbox = page.getByRole('textbox').nth(0);
+    this.passwordTextbox = page.getByLabel('Password');
     this.continueButton = page.getByRole('button', { name: 'Continue' });
   }
 
@@ -22,8 +22,8 @@ export class PortalLoginPage {
 
   async logIn(username: string, password: string) {
     await this.loginButton.click();
-    await this.userIdTextField.fill(username);
-    await this.passwordTextField.fill(password);
+    await this.userIdTextbox.fill(username);
+    await this.passwordTextbox.fill(password);
     await this.continueButton.click();
   }
 }
