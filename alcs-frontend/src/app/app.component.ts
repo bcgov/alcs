59b01cf40dd0c16c3feeb1cd4d097d837093dece
Daniel Haselhan <daniel@bit3.ca>
2024-05-01 10:40:35 -0700

    More Snowplow
    * Add script hash to enable it to load inline
    * Remove analytics service from ALCS, we are only doing tracking on portal

--- alcs-frontend/src/app/app.component.ts
+++ alcs-frontend/src/app/app.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { AnalyticsService } from './services/analytics/analytics.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'alcs-frontend';
 
-  constructor(private analyticsService: AnalyticsService) {}
-
-  ngOnInit(): void {
-    this.analyticsService.init();
-  }
+  constructor() {}
 }
