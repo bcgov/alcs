2706bfb9dfd41581267b17f1c8a064cd047ae085
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-05-13 12:32:53 -0700

    Prevent close for dialogs using existing service
    A lot of dialogs already use confirmation dialog
    service. We can easily set these to not close when
    clicking outside.

--- portal-frontend/src/app/shared/confirmation-dialog/confirmation-dialog.service.ts
+++ portal-frontend/src/app/shared/confirmation-dialog/confirmation-dialog.service.ts
@@ -13,6 +13,7 @@ export class ConfirmationDialogService {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
       data,
       autoFocus: false,
+      disableClose: true,
     });
 
     dialogRef.afterClosed().subscribe((response) => {
