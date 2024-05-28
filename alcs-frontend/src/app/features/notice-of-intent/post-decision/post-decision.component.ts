a22aefdb52b52a28fc9b72c09cef548ca39176d5
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-05-28 12:39:47 -0700

    Allow add NOI modi from post-decision page

--- alcs-frontend/src/app/features/notice-of-intent/post-decision/post-decision.component.ts
+++ alcs-frontend/src/app/features/notice-of-intent/post-decision/post-decision.component.ts
@@ -4,10 +4,11 @@ import { combineLatestWith, Subject, takeUntil } from 'rxjs';
 import { NoticeOfIntentDetailService } from '../../../services/notice-of-intent/notice-of-intent-detail.service';
 import { NoticeOfIntentModificationDto } from '../../../services/notice-of-intent/notice-of-intent-modification/notice-of-intent-modification.dto';
 import { NoticeOfIntentModificationService } from '../../../services/notice-of-intent/notice-of-intent-modification/notice-of-intent-modification.service';
-import { ToastService } from '../../../services/toast/toast.service';
 import { ConfirmationDialogService } from '../../../shared/confirmation-dialog/confirmation-dialog.service';
-import { formatDateForApi } from '../../../shared/utils/api-date-formatter';
 import { EditModificationDialogComponent } from './edit-modification-dialog/edit-modification-dialog.component';
+import { CreateNoiModificationDialogComponent } from '../../board/dialogs/noi-modification/create/create-noi-modification-dialog.component';
+import { ApplicationLocalGovernmentDto } from 'src/app/services/application/application-local-government/application-local-government.dto';
+import { ApplicationRegionDto } from 'src/app/services/application/application-code.dto';
 
 type LoadingModifications = NoticeOfIntentModificationDto & {
   modifiesDecisionsNumbers: string[];
@@ -20,14 +21,16 @@ type LoadingModifications = NoticeOfIntentModificationDto & {
 export class PostDecisionComponent implements OnInit, OnDestroy {
   $destroy = new Subject<void>();
   fileNumber: string = '';
+  applicant: string = '';
+  localGovernment: ApplicationLocalGovernmentDto | undefined = undefined;
+  region: ApplicationRegionDto | undefined = undefined;
   modifications: LoadingModifications[] = [];
 
   constructor(
     public dialog: MatDialog,
     private noticeOfIntentDetailService: NoticeOfIntentDetailService,
     private modificationService: NoticeOfIntentModificationService,
-    private toastService: ToastService,
-    private confirmationDialogService: ConfirmationDialogService
+    private confirmationDialogService: ConfirmationDialogService,
   ) {}
 
   ngOnInit(): void {
@@ -37,15 +40,33 @@ export class PostDecisionComponent implements OnInit, OnDestroy {
       .subscribe(([application, modifications]) => {
         if (application) {
           this.fileNumber = application.fileNumber;
+          this.applicant = application.applicant;
+          this.localGovernment = application.localGovernment;
+          this.region = application.region;
           this.modifications =
             modifications?.map((m) => ({
               ...m,
               modifiesDecisionsNumbers: m.modifiesDecisions.flatMap(
-                (d) => `#${d.resolutionNumber}/${d.resolutionYear}`
+                (d) => `#${d.resolutionNumber}/${d.resolutionYear}`,
               ),
             })) ?? [];
         }
       });
+  }
+
+  onCreateModification() {
+    this.dialog.open(CreateNoiModificationDialogComponent, {
+      minWidth: '600px',
+      maxWidth: '1100px',
+      maxHeight: '80vh',
+      width: '90%',
+      data: {
+        fileNumber: this.fileNumber,
+        applicant: this.applicant,
+        localGovernment: this.localGovernment,
+        region: this.region,
+      },
+    });
   }
 
   onEditModification(modification: NoticeOfIntentModificationDto) {
