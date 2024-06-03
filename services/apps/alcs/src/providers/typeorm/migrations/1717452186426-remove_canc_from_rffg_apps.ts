9a9a19a1c13005c19588c207a4b1cb2349fd9f80
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-06-03 16:20:50 -0700

    Align capitalization with other migrations

--- services/apps/alcs/src/providers/typeorm/migrations/1717452186426-remove_canc_from_rffg_apps.ts
+++ services/apps/alcs/src/providers/typeorm/migrations/1717452186426-remove_canc_from_rffg_apps.ts
@@ -3,14 +3,14 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class RemoveCancFromRffgApps1717452186426 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(`
-      update alcs.application_submission_to_submission_status astss1
-      set effective_date = null, email_sent_date = null
-      from alcs.application_submission_to_submission_status astss2
-      where astss1.submission_uuid = astss2.submission_uuid  
-      and astss1.status_type_code = 'CANC'
-      and astss2.status_type_code = 'RFFG'
-      and astss1.effective_date is not null
-      and astss2.effective_date is not null
+      UPDATE alcs.application_submission_to_submission_status astss1
+      SET effective_date = NULL, email_sent_date = NULL
+      FROM alcs.application_submission_to_submission_status astss2
+      WHERE astss1.submission_uuid = astss2.submission_uuid  
+      AND astss1.status_type_code = 'CANC'
+      AND astss2.status_type_code = 'RFFG'
+      AND astss1.effective_date IS NOT NULL
+      AND astss2.effective_date IS NOT NULL;
     `);
   }
 
