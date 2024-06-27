efe994ca46bf86513a743182fcec0829669aec02
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-06-26 18:33:00 -0700

    Create custom stricter email validator

---
+++ portal-frontend/src/app/shared/validators/email-validator.ts
@@ -0,0 +1,14 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+import validator from 'validator';
+
+export const strictEmailValidator = (control: AbstractControl): ValidationErrors | null => {
+  return isString(control.value) &&
+    !validator.isEmpty(control.value, { ignore_whitespace: true }) &&
+    validator.isEmail(control.value, { allow_display_name: true })
+    ? null
+    : { email: true };
+};
+
+function isString(s: string) {
+  return Object.prototype.toString.call(s) === '[object String]';
+}
