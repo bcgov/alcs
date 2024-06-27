d53aac20ed89ef398d3127081d5899ff5a7dd510
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-06-27 11:31:30 -0700

    Add `isEmail` option to `app-inline-text`
    - Add `isEmail` flag
    - Conditionally add new strict email validator
    - Disable save button if invalid
    - Ignore attempts to save when invalid

--- alcs-frontend/src/app/shared/inline-editors/inline-text/inline-text.component.ts
+++ alcs-frontend/src/app/shared/inline-editors/inline-text/inline-text.component.ts
@@ -8,6 +8,8 @@ import {
   Output,
   ViewChild,
 } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { strictEmailValidator } from '../../validators/email-validator';
 
 @Component({
   selector: 'app-inline-text[value]',
@@ -19,9 +21,12 @@ export class InlineTextComponent implements AfterContentChecked {
   @Input() value?: string | undefined;
   @Input() placeholder: string = 'Enter a value';
   @Input() required = false;
+  @Input() isEmail = false;
   @Output() save = new EventEmitter<string | null>();
   @Input() mask?: string | undefined;
   @Input() maxLength: number | null = null;
+
+  textInputControl = new FormControl<string | null>(null, []);
 
   @ViewChild('editInput') textInput!: ElementRef;
 
@@ -29,6 +34,12 @@ export class InlineTextComponent implements AfterContentChecked {
   pendingValue: undefined | string;
 
   constructor() {}
+
+  ngOnInit() {
+    if (this.isEmail) {
+      this.textInputControl.setValidators([strictEmailValidator]);
+    }
+  }
 
   startEdit() {
     this.isEditing = true;
@@ -42,6 +53,10 @@ export class InlineTextComponent implements AfterContentChecked {
   }
 
   confirmEdit() {
+    if (this.textInputControl.invalid) {
+      return;
+    }
+
     if (this.pendingValue !== this.value) {
       this.save.emit(this.pendingValue?.toString() ?? null);
 
