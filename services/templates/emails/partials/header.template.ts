1e33aadf5b7b78acdcbaaa44f9cd09c0a7f9d7ca
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-07-10 13:55:28 -0700

    Rename applicant to 'Name'
    - Replace 'Owner Name' with 'Name' in UI
    - Replace 'Applicant' with 'Name' in PDF templates

--- services/templates/emails/partials/header.template.ts
+++ services/templates/emails/partials/header.template.ts
@@ -4,7 +4,7 @@ export const header = `
   <mj-section background-color="white" padding="40px 0px 0px 0px">
     <mj-column width="400px">
       <mj-text font-size="21px"><b>{{ parentTypeLabel }} ID #{{fileNumber}}</b></mj-text>
-      <mj-text font-size="15px">Owner Name: <b>{{applicantName}}</b></mj-text>
+      <mj-text font-size="15px">Name: <b>{{applicantName}}</b></mj-text>
       <mj-text font-size="15px">Status: <b>{{status}}</b></mj-text>
     </mj-column>
     <mj-column width="200px">
