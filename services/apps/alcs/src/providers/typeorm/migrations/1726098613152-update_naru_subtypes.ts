7fb6bb74cd1bac3dde87d4b1faa68531295dcfac
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-09-12 08:10:37 -0700

    Update NARU description

---
+++ services/apps/alcs/src/providers/typeorm/migrations/1726098613152-update_naru_subtypes.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class UpdateNaruSubtypes1726098613152 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+        update  alcs.application_type at
+        set     html_description = 'Choose this option if you are proposing to conduct a non-adhering residential use within the Agricultural Land Reserve under
+        <a target="_blank" href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/02036_01#section20.1">Section 20.1(2) of the Agricultural Land Commission Act.</a>
+        <br /><br />
+        Non-adhering residential uses include:
+        <ul>
+            <li>An additional residence(s)</li>
+            <li>A residential structure larger than 500 m²</li>
+            <li>Retain an existing residence while building a new residence</li>
+            <li>Temporary foreign worker housing</li>
+        </ul>
+        '
+        where   at.code = 'NARU';
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+        update  alcs.application_type at
+        set     html_description = 'Choose this option if you are proposing to conduct a non-adhering residential use within the Agricultural Land Reserve under
+        <a target="_blank" href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/02036_01#section20.1">Section 20.1(2) of the Agricultural Land Commission Act.</a>
+        <br /><br />
+        Non-adhering residential uses include:
+        <ul>
+            <li>An additional residence for farm use;</li>
+            <li>A residential structure larger than 500 m²; or,</li>
+            <li>A non-adhering tourism accommodation.</li>
+        </ul>
+        '
+        where   at.code = 'NARU';
+    `);
+  }
+}
