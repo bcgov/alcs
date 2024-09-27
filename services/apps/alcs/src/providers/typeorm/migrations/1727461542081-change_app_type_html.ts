cfd2c3278c8b348044b41da6ec606a9bc40d2a63
Felipe Barreta <fbarreta@gmail.com>
2024-09-27 12:09:09 -0700

    Add migration to fix html text

---
+++ services/apps/alcs/src/providers/typeorm/migrations/1727461542081-change_app_type_html.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class changeAppTypeHtml1727461542081 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `UPDATE "alcs"."application_type" SET 
+      "html_description"=CONCAT(html_description, '<p>If you need help selecting an application type, please <a target="_blank" href="https://www.alc.gov.bc.ca/contact/#lup">Contact Us</a>.</p>') 
+       WHERE "html_description" is not null`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // N/A
+  }
+}
