4456b89c5eb1790fc093be2ee83aa60d163219a8
Felipe Barreta <fbarreta@gmail.com>
2024-10-01 09:43:44 -0700

    New line break added

--- services/apps/alcs/src/providers/typeorm/migrations/1727461542081-change_app_type_html.ts
+++ services/apps/alcs/src/providers/typeorm/migrations/1727461542081-change_app_type_html.ts
@@ -4,7 +4,7 @@ export class ChangeAppTypeHtml1727461542081 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `UPDATE "alcs"."application_type" SET 
-      "html_description"=CONCAT(html_description, '<p>If you need help selecting an application type, please <a target="_blank" href="https://www.alc.gov.bc.ca/contact/#lup">Contact Us</a>.</p>') 
+      "html_description"=CONCAT(html_description, '<br /><p>If you need help selecting an application type, please <a target="_blank" href="https://www.alc.gov.bc.ca/contact/#lup">Contact Us</a>.</p>') 
        WHERE "html_description" is not null`,
     );
   }
