9c87239a3d1e6a96ea515f4fdfdab29b4216bd9a
abradat <shn.khorsandi@gmail.com>
2024-10-10 12:57:05 -0700

    Fix NARU migration

--- services/apps/alcs/src/providers/typeorm/migrations/1728581017573-convert_naru_floor_area_and_existing_structuers.ts
+++ services/apps/alcs/src/providers/typeorm/migrations/1728581017573-convert_naru_floor_area_and_existing_structuers.ts
@@ -6,21 +6,24 @@ export class ConvertNaruFloorAreaAndExistingStructuers1728581017573
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
             UPDATE alcs.application_submission as2
-            SET naru_existing_residences = jsonb_build_object(
-                'floorArea', null
-                'description', as2.naru_existing_structures
-            )
+            SET naru_existing_residences = jsonb_build_array(
+                jsonb_build_object(
+                    'floorArea', 0,
+                    'description', as2.naru_existing_structures
+                )
+            ) 
             WHERE as2.naru_existing_structures IS NOT NULL;
         `);
 
     await queryRunner.query(`
             UPDATE alcs.application_submission as2
-            SET naru_proposed_residences = jsonb_build_object(
-                'floorArea', (as2.naru_floor_area::float) ,
-                'description', null
-            )
-            WHERE as2.naru_floor_area IS NOT NULL;
-            )    
+            SET naru_proposed_residences = jsonb_build_array(
+                jsonb_build_object(
+                    'floorArea', (as2.naru_floor_area::float) ,
+                    'description', ''
+                )
+            ) 
+            WHERE as2.naru_floor_area IS NOT NULL;   
         `);
   }
 
