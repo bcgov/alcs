37c843fa1bb5614345635eaa769dbf4ed90bc887
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-11-08 13:28:44 -0800

    Answer 'No' to existing soil apps

---
+++ services/apps/alcs/src/providers/typeorm/migrations/1731100639156-answer_no_to_structures_for_old_soil_apps.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AnswerNoToStructuresForOldSoilApps1731100639156 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+      update
+        alcs.application_submission as2
+      set
+        as2.soil_is_new_structure = false
+      where
+        as2.type_code in ('PFRS', 'POFO', 'ROSO')
+        and as2.soil_is_new_structure is null;
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
