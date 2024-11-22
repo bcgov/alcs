16c9663131ed6b6d08e04a72947fdca2e365bd18
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2024-11-21 16:19:31 -0800

    add admin decision condition types to NOIs

--- services/apps/alcs/src/alcs/notice-of-intent-decision/notice-of-intent-decision-condition/notice-of-intent-decision-condition-code.entity.ts
+++ services/apps/alcs/src/alcs/notice-of-intent-decision/notice-of-intent-decision-condition/notice-of-intent-decision-condition-code.entity.ts
@@ -1,7 +1,50 @@
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { BaseCodeEntity } from '../../../common/entities/base.code.entity';
+import { AutoMap } from 'automapper-classes';
+import { ColumnNumericTransformer } from '../../../utils/column-numeric-transform';
+import { DateLabel } from '../../application-decision/application-decision-condition/application-decision-condition-code.entity';
 
 @Entity({
   comment: 'Decision Condition Types Code Table for Notice of Intents',
 })
-export class NoticeOfIntentDecisionConditionType extends BaseCodeEntity {}
+export class NoticeOfIntentDecisionConditionType extends BaseCodeEntity {
+  @AutoMap()
+  @Column({ default: true, type: 'boolean' })
+  isComponentToConditionChecked: boolean;
+
+  @AutoMap()
+  @Column({ default: true, type: 'boolean' })
+  isDescriptionChecked: boolean;
+
+  @AutoMap()
+  @Column({ default: false, type: 'boolean' })
+  isAdministrativeFeeAmountChecked: boolean;
+
+  @AutoMap()
+  @Column({ nullable: true, type: 'boolean' })
+  isAdministrativeFeeAmountRequired: boolean | null;
+
+  @AutoMap()
+  @Column({ nullable: true, type: 'decimal', precision: 8, scale: 2, transformer: new ColumnNumericTransformer() })
+  administrativeFeeAmount: number | null;
+
+  @AutoMap()
+  @Column({ default: false, type: 'boolean' })
+  isSingleDateChecked: boolean;
+
+  @AutoMap()
+  @Column({ nullable: true, type: 'boolean' })
+  isSingleDateRequired: boolean | null;
+
+  @AutoMap()
+  @Column({ type: 'enum', enum: DateLabel, nullable: true })
+  singleDateLabel: DateLabel | null;
+
+  @AutoMap()
+  @Column({ default: false, type: 'boolean' })
+  isSecurityAmountChecked: boolean;
+
+  @AutoMap()
+  @Column({ nullable: true, type: 'boolean' })
+  isSecurityAmountRequired: boolean | null;
+}
