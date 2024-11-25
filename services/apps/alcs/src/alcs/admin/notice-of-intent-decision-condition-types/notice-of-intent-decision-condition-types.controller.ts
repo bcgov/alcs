957378370b4cc8e9f6580010483e0706a719422d
Tristan Slater <1631008+trslater@users.noreply.github.com>
2024-11-25 15:50:56 -0800

    Use proper NOI DTO instead of apps DTO

--- services/apps/alcs/src/alcs/admin/notice-of-intent-decision-condition-types/notice-of-intent-decision-condition-types.controller.ts
+++ services/apps/alcs/src/alcs/admin/notice-of-intent-decision-condition-types/notice-of-intent-decision-condition-types.controller.ts
@@ -4,8 +4,8 @@ import * as config from 'config';
 import { AUTH_ROLE } from '../../../common/authorization/roles';
 import { RolesGuard } from '../../../common/authorization/roles-guard.service';
 import { UserRoles } from '../../../common/authorization/roles.decorator';
-import { ApplicationDecisionConditionTypeDto } from '../../application-decision/application-decision-condition/application-decision-condition.dto';
 import { NoticeofIntentDecisionConditionTypesService } from './notice-of-intent-decision-condition-types.service';
+import { NoticeOfIntentDecisionConditionTypeDto } from '../../notice-of-intent-decision/notice-of-intent-decision-condition/notice-of-intent-decision-condition.dto';
 
 @Controller('noi-decision-condition-types')
 @ApiOAuth2(config.get<string[]>('KEYCLOAK.SCOPES'))
@@ -21,7 +21,7 @@ export class NoticeofIntentDecisionConditionTypesController {
 
   @Patch('/:code')
   @UserRoles(AUTH_ROLE.ADMIN)
-  async update(@Param('code') code: string, @Body() updateDto: ApplicationDecisionConditionTypeDto) {
+  async update(@Param('code') code: string, @Body() updateDto: NoticeOfIntentDecisionConditionTypeDto) {
     return await this.noiDecisionConditionTypesService.update(code, updateDto);
   }
 
@@ -33,7 +33,7 @@ export class NoticeofIntentDecisionConditionTypesController {
 
   @Post('')
   @UserRoles(AUTH_ROLE.ADMIN)
-  async create(@Body() createDto: ApplicationDecisionConditionTypeDto) {
+  async create(@Body() createDto: NoticeOfIntentDecisionConditionTypeDto) {
     return await this.noiDecisionConditionTypesService.create(createDto);
   }
 }
