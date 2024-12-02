ee8d850f0f73ba2afdfa66b83d2fef9493c8c7a4
Felipe Barreta <fbarreta@gmail.com>
2024-12-02 12:50:47 -0800

    ALCS-2092 Change date type field

--- services/apps/alcs/src/providers/typeorm/migrations/1733163698882-add_date_and_admin_fee.ts
+++ services/apps/alcs/src/providers/typeorm/migrations/1733163698882-add_date_and_admin_fee.ts
@@ -3,8 +3,8 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class AddDateAndAdminFee1733163698882 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`UPDATE "alcs"."application_decision_condition_type" SET "is_administrative_fee_amount_checked" = true, "application_decision_condition_type_date_type_enum" = 'Single', "single_date_label" = 'Due Date' WHERE "code" NOT IN ('SRPT', 'UEND')`);
-        await queryRunner.query(`UPDATE "alcs"."notice_of_intent_decision_condition_type" SET "is_administrative_fee_amount_checked" = true, "application_decision_condition_type_date_type_enum" = 'Single', "single_date_label" = 'Due Date' WHERE "code" NOT IN ('SRPT', 'UEND')`);
+        await queryRunner.query(`UPDATE "alcs"."application_decision_condition_type" SET "is_administrative_fee_amount_checked" = true, "date_type" = 'Single', "single_date_label" = 'Due Date' WHERE "code" NOT IN ('SRPT', 'UEND')`);
+        await queryRunner.query(`UPDATE "alcs"."notice_of_intent_decision_condition_type" SET "is_administrative_fee_amount_checked" = true, "date_type" = 'Single', "single_date_label" = 'Due Date' WHERE "code" NOT IN ('SRPT', 'UEND')`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
