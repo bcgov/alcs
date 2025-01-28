faaa3bb50b87d2840fc0313f3dc925dc07a6a01f
Tristan Slater <1631008+trslater@users.noreply.github.com>
2025-01-28 12:00:55 -0800

    Fix public doc sorting

--- portal-frontend/src/app/features/public/application/alc-review/submission-documents/submission-documents.component.ts
+++ portal-frontend/src/app/features/public/application/alc-review/submission-documents/submission-documents.component.ts
@@ -7,6 +7,8 @@ import { PublicDocumentDto } from '../../../../../services/public/public.dto';
 import { PublicService } from '../../../../../services/public/public.service';
 import { openFileInline } from '../../../../../shared/utils/file';
 
+type PublicDocumentWithoutTypeDto = Omit<PublicDocumentDto, 'type'>;
+
 @Component({
   selector: 'app-submission-documents[applicationSubmission]',
   templateUrl: './submission-documents.component.html',
@@ -21,13 +23,27 @@ export class PublicSubmissionDocumentsComponent implements OnInit, OnDestroy {
   @Input() applicationDocuments!: PublicDocumentDto[];
   @Input() applicationSubmission!: PublicApplicationSubmissionDto;
 
-  @ViewChild(MatSort) sort!: MatSort;
   dataSource: MatTableDataSource<PublicDocumentDto> = new MatTableDataSource<PublicDocumentDto>();
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private publicService: PublicService) {}
 
   ngOnInit(): void {
-    this.dataSource = new MatTableDataSource(this.applicationDocuments);
+    this.dataSource.data = this.applicationDocuments;
+    this.dataSource.sortingDataAccessor = (
+      { type, ...rest }: PublicDocumentDto,
+      sortHeaderId: string,
+    ): string | number => {
+      if (sortHeaderId === 'type') {
+        return type?.label ?? '';
+      }
+
+      return (rest as PublicDocumentWithoutTypeDto)[sortHeaderId as keyof PublicDocumentWithoutTypeDto] ?? '';
+    };
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 
   async openFile(file: PublicDocumentDto) {
