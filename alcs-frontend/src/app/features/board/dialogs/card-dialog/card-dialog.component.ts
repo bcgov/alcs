1d43b763b89f57d683dc9d0560c5dff2d102c7dc
Tristan Slater <1631008+trslater@users.noreply.github.com>
2025-03-06 09:04:04 -0800

    Improve subtask filtering logic

--- alcs-frontend/src/app/features/board/dialogs/card-dialog/card-dialog.component.ts
+++ alcs-frontend/src/app/features/board/dialogs/card-dialog/card-dialog.component.ts
@@ -79,9 +79,10 @@ export class CardDialogComponent implements OnInit, OnDestroy {
 
   filterAssigneeList(term: string, item: AssigneeDto) {
     const termLower = term.toLocaleLowerCase();
-    return item.email
-      ? item.email.toLocaleLowerCase().indexOf(termLower) > -1
-      : false || item.prettyName.toLocaleLowerCase().indexOf(termLower) > -1;
+    return (
+      (item.email && item.email.toLocaleLowerCase().indexOf(termLower) > -1) ||
+      item.prettyName.toLocaleLowerCase().indexOf(termLower) > -1
+    );
   }
 
   onArchiveCard() {
