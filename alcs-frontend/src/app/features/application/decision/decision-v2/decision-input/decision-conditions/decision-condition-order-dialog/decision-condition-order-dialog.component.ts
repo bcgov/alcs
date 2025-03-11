39a23258a933d7660a7f2cafa0d2344fbbdb8e08
Tristan Slater <1631008+trslater@users.noreply.github.com>
2025-03-11 15:58:45 -0700

    Replace condition that kept orders sane

--- alcs-frontend/src/app/features/application/decision/decision-v2/decision-input/decision-conditions/decision-condition-order-dialog/decision-condition-order-dialog.component.ts
+++ alcs-frontend/src/app/features/application/decision/decision-v2/decision-input/decision-conditions/decision-condition-order-dialog/decision-condition-order-dialog.component.ts
@@ -48,63 +48,61 @@ export class DecisionConditionOrderDialogComponent implements OnInit {
   }
 
   async openMenu($event: MouseEvent, record: ApplicationDecisionConditionDto) {
-      this.overlayRef?.detach();
-      $event.preventDefault();
-      this.selectedRecord = record.uuid;
-      const positionStrategy = this.overlay
-        .position()
-        .flexibleConnectedTo({ x: $event.x, y: $event.y })
-        .withPositions([
-          {
-            originX: 'end',
-            originY: 'bottom',
-            overlayX: 'start',
-            overlayY: 'top',
-          },
-        ]);
-  
-      this.overlayRef = this.overlay.create({
-        positionStrategy,
-        scrollStrategy: this.overlay.scrollStrategies.close(),
-      });
-  
-      this.overlayRef.attach(
-        new TemplatePortal(this.orderMenu, this.viewContainerRef, {
-          $implicit: record,
-        }),
-      );
-    }
-  
-    sendToBottom(record: ApplicationDecisionConditionDto) {
-      const currentIndex = this.conditionsToOrder.findIndex((item) => item.uuid === record.uuid);
-      this.conditionsToOrder.sort((a,b) => a.order - b.order).forEach((item) => {
-        if (item.order > currentIndex) {
-          item.order--;
-        }
-      });
-      this.conditionsToOrder[currentIndex].order = this.conditionsToOrder.length;
-      this.dataSource.data = this.conditionsToOrder.sort((a,b) => a.order - b.order);
-      this.overlayRef?.detach();
-      this.selectedRecord = undefined;
-    }
-  
-    sendToTop(record: ApplicationDecisionConditionDto) {
-      const currentIndex = this.conditionsToOrder.findIndex((item) => item.uuid === record.uuid);
-      this.conditionsToOrder.sort((a,b) => a.order - b.order).forEach((item) => {
-        if (item.order < currentIndex) {
-          item.order++;
-        }
-      });
-      this.conditionsToOrder[currentIndex].order = 0;
-      this.dataSource.data = this.conditionsToOrder.sort((a,b) => a.order - b.order);
-      this.overlayRef?.detach();
-      this.selectedRecord = undefined;
-    }
-  
-    clearMenu() {
-      this.overlayRef?.detach();
-      this.selectedRecord = undefined;
-    }
+    this.overlayRef?.detach();
+    $event.preventDefault();
+    this.selectedRecord = record.uuid;
+    const positionStrategy = this.overlay
+      .position()
+      .flexibleConnectedTo({ x: $event.x, y: $event.y })
+      .withPositions([
+        {
+          originX: 'end',
+          originY: 'bottom',
+          overlayX: 'start',
+          overlayY: 'top',
+        },
+      ]);
+
+    this.overlayRef = this.overlay.create({
+      positionStrategy,
+      scrollStrategy: this.overlay.scrollStrategies.close(),
+    });
+
+    this.overlayRef.attach(
+      new TemplatePortal(this.orderMenu, this.viewContainerRef, {
+        $implicit: record,
+      }),
+    );
+  }
+
+  sendToBottom(record: ApplicationDecisionConditionDto) {
+    this.conditionsToOrder.forEach((item) => {
+      if (item.order > record.order) {
+        item.order--;
+      }
+    });
+    record.order = this.conditionsToOrder.length - 1;
+    this.dataSource.data = this.conditionsToOrder.sort((a, b) => a.order - b.order);
+    this.overlayRef?.detach();
+    this.selectedRecord = undefined;
+  }
+
+  sendToTop(record: ApplicationDecisionConditionDto) {
+    this.conditionsToOrder.forEach((item) => {
+      if (item.order < record.order) {
+        item.order++;
+      }
+    });
+    record.order = 0;
+    this.dataSource.data = this.conditionsToOrder.sort((a, b) => a.order - b.order);
+    this.overlayRef?.detach();
+    this.selectedRecord = undefined;
+  }
+
+  clearMenu() {
+    this.overlayRef?.detach();
+    this.selectedRecord = undefined;
+  }
 
   private moveItem(currentIndex: number, targetIndex: number) {
     this.conditionsToOrder.sort((a,b) => a.order - b.order).forEach((item) => {
