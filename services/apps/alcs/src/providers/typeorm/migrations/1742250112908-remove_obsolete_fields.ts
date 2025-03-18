c6a3f6344da1891fb71751db6ee9d07f6fec3e9b
Felipe Barreta <fbarreta@gmail.com>
2025-03-18 12:07:17 -0700

    ALCS-2073 Backup tables

--- services/apps/alcs/src/providers/typeorm/migrations/1742250112908-remove_obsolete_fields.ts
+++ services/apps/alcs/src/providers/typeorm/migrations/1742250112908-remove_obsolete_fields.ts
@@ -4,6 +4,32 @@ export class RemoveObsoleteFields1742250112908 implements MigrationInterface {
     name = 'RemoveObsoleteFields1742250112908'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        // backup tables to public schema
+        await queryRunner.query(`CREATE TABLE "public"."bck-2073-application_decision_component" (LIKE "alcs"."application_decision_component" INCLUDING ALL);`);
+        await queryRunner.query(`INSERT INTO "public"."bck-2073-application_decision_component"
+                                    SELECT * 
+                                    FROM "alcs"."application_decision_component";`);
+        await queryRunner.query(`CREATE TABLE "public"."bck-2073-application_decision_condition" (LIKE "alcs"."application_decision_condition" INCLUDING ALL);`);
+        await queryRunner.query(`INSERT INTO "public"."bck-2073-application_decision_condition"
+                                    SELECT * 
+                                    FROM "alcs"."application_decision_condition";`);
+        await queryRunner.query(`CREATE TABLE "public"."bck-2073-application" (LIKE "alcs"."application" INCLUDING ALL);`);
+        await queryRunner.query(`INSERT INTO "public"."bck-2073-application"
+                                    SELECT * 
+                                    FROM "alcs"."application";`);
+        await queryRunner.query(`CREATE TABLE "public"."bck-2073-notice_of_intent_decision_component" (LIKE "alcs"."notice_of_intent_decision_component" INCLUDING ALL);`);
+        await queryRunner.query(`INSERT INTO "public"."bck-2073-notice_of_intent_decision_component"
+                                    SELECT * 
+                                    FROM "alcs"."notice_of_intent_decision_component";`);
+        await queryRunner.query(`CREATE TABLE "public"."bck-2073-notice_of_intent_decision_condition" (LIKE "alcs"."notice_of_intent_decision_condition" INCLUDING ALL);`);
+        await queryRunner.query(`INSERT INTO "public"."bck-2073-notice_of_intent_decision_condition"
+                                    SELECT * 
+                                    FROM "alcs"."notice_of_intent_decision_condition";`);
+        await queryRunner.query(`CREATE TABLE "public"."bck-2073-notice_of_intent" (LIKE "alcs"."notice_of_intent" INCLUDING ALL);`);
+        await queryRunner.query(`INSERT INTO "public"."bck-2073-notice_of_intent"
+                                    SELECT * 
+                                    FROM "alcs"."notice_of_intent";`);
+        //end backup
         await queryRunner.query(`ALTER TABLE "alcs"."application_decision_component" DROP COLUMN "end_date"`);
         await queryRunner.query(`ALTER TABLE "alcs"."application_decision_component" DROP COLUMN "expiry_date"`);
         await queryRunner.query(`ALTER TABLE "alcs"."application_decision_component" DROP COLUMN "end_date2"`);
