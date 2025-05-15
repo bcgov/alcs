8ba1efd4420f1810c93d567353eb96d27fa62e16
Tristan Slater <1631008+trslater@users.noreply.github.com>
2025-05-14 17:13:01 -0700

    Provide test dependencies for new downloadFile

--- portal-frontend/src/app/features/notifications/view-submission/alc-review/submission-documents/submission-documents.component.spec.ts
+++ portal-frontend/src/app/features/notifications/view-submission/alc-review/submission-documents/submission-documents.component.spec.ts
@@ -5,14 +5,20 @@ import { NoticeOfIntentDocumentService } from '../../../../../services/notice-of
 import { NotificationDocumentService } from '../../../../../services/notification-document/notification-document.service';
 
 import { SubmissionDocumentsComponent } from './submission-documents.component';
+import { HttpClient } from '@angular/common/http';
+import { DocumentService } from '../../../../../services/document/document.service';
 
 describe('SubmissionDocumentsComponent', () => {
   let component: SubmissionDocumentsComponent;
   let fixture: ComponentFixture<SubmissionDocumentsComponent>;
   let mockNotificationDocumentService: DeepMocked<NotificationDocumentService>;
+  let mockHttpClient: DeepMocked<HttpClient>;
+  let mockDocumentService: DeepMocked<DocumentService>;
 
   beforeEach(async () => {
     mockNotificationDocumentService = createMock();
+    mockHttpClient = createMock();
+    mockDocumentService = createMock();
 
     await TestBed.configureTestingModule({
       declarations: [SubmissionDocumentsComponent],
@@ -20,6 +26,14 @@ describe('SubmissionDocumentsComponent', () => {
         {
           provide: NotificationDocumentService,
           useValue: mockNotificationDocumentService,
+        },
+        {
+          provide: HttpClient,
+          useValue: mockHttpClient,
+        },
+        {
+          provide: DocumentService,
+          useValue: mockDocumentService,
         },
       ],
       schemas: [NO_ERRORS_SCHEMA],
