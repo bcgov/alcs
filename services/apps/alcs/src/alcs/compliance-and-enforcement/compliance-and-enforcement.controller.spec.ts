3d8d4cbdcc594d41d56923579f371c37fb2e11a6
Tristan Slater <1631008+trslater@users.noreply.github.com>
2025-05-22 13:13:32 -0700

    Write tests for compliance backend

---
+++ services/apps/alcs/src/alcs/compliance-and-enforcement/compliance-and-enforcement.controller.spec.ts
@@ -0,0 +1,154 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ComplianceAndEnforcementController } from './compliance-and-enforcement.controller';
+import { ComplianceAndEnforcementService } from './compliance-and-enforcement.service';
+import { createMock, DeepMocked } from '@golevelup/nestjs-testing';
+import { mockKeyCloakProviders } from '../../../test/mocks/mockTypes';
+import { ClsService } from 'nestjs-cls';
+import { ComplianceAndEnforcementDto } from './compliance-and-enforcement.dto';
+import { AutomapperModule } from 'automapper-nestjs';
+import { classes } from 'automapper-classes';
+
+describe('ComplianceAndEnforcementController', () => {
+  let controller: ComplianceAndEnforcementController;
+  let mockComplianceAndEnforcementService: DeepMocked<ComplianceAndEnforcementService>;
+
+  beforeEach(async () => {
+    mockComplianceAndEnforcementService = createMock();
+
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        AutomapperModule.forRoot({
+          strategyInitializer: classes(),
+        }),
+      ],
+      controllers: [ComplianceAndEnforcementController],
+      providers: [
+        {
+          provide: ComplianceAndEnforcementService,
+          useValue: mockComplianceAndEnforcementService,
+        },
+        {
+          provide: ClsService,
+          useValue: {},
+        },
+        ...mockKeyCloakProviders,
+      ],
+    }).compile();
+
+    controller = module.get<ComplianceAndEnforcementController>(ComplianceAndEnforcementController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('fetch', () => {
+    it('should return all items', async () => {
+      const result: ComplianceAndEnforcementDto[] = [
+        {
+          uuid: '1',
+          fileNumber: '1',
+          dateSubmitted: 0,
+          dateOpened: 0,
+          dateClosed: 0,
+          initialSubmissionType: null,
+          allegedContraventionNarrative: '',
+          allegedActivity: [],
+          intakeNotes: '',
+        },
+        {
+          uuid: '2',
+          fileNumber: '2',
+          dateSubmitted: 0,
+          dateOpened: 0,
+          dateClosed: 0,
+          initialSubmissionType: null,
+          allegedContraventionNarrative: '',
+          allegedActivity: [],
+          intakeNotes: '',
+        },
+      ];
+      mockComplianceAndEnforcementService.fetchAll.mockResolvedValue(result);
+      expect(await controller.fetchAll()).toEqual(result);
+      expect(mockComplianceAndEnforcementService.fetchAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('fetchByFileNumber', () => {
+    it('should return a single item by ID', async () => {
+      const result: ComplianceAndEnforcementDto = {
+        uuid: '1',
+        fileNumber: '1',
+        dateSubmitted: 0,
+        dateOpened: 0,
+        dateClosed: 0,
+        initialSubmissionType: null,
+        allegedContraventionNarrative: '',
+        allegedActivity: [],
+        intakeNotes: '',
+      };
+      mockComplianceAndEnforcementService.fetchByFileNumber.mockResolvedValue(result);
+      expect(await controller.fetchByFileNumber('1')).toEqual(result);
+      expect(mockComplianceAndEnforcementService.fetchByFileNumber).toHaveBeenCalledWith('1');
+    });
+  });
+
+  describe('create', () => {
+    it('should create a new item', async () => {
+      const createDto = {};
+      const resultDto: ComplianceAndEnforcementDto = {
+        uuid: '1',
+        fileNumber: '1',
+        dateSubmitted: 0,
+        dateOpened: 0,
+        dateClosed: 0,
+        initialSubmissionType: null,
+        allegedContraventionNarrative: '',
+        allegedActivity: [],
+        intakeNotes: '',
+      };
+      mockComplianceAndEnforcementService.create.mockResolvedValue(resultDto);
+      expect(await controller.create(createDto)).toEqual(resultDto);
+      expect(mockComplianceAndEnforcementService.create).toHaveBeenCalledWith(createDto);
+    });
+  });
+
+  describe('update', () => {
+    it('should update an existing item', async () => {
+      const updateDto = {
+        uuid: '1',
+        fileNumber: '1',
+        dateSubmitted: 0,
+        dateOpened: 0,
+        dateClosed: 0,
+        initialSubmissionType: null,
+        allegedContraventionNarrative: '',
+        allegedActivity: [],
+        intakeNotes: '',
+      };
+      const resultDto = {
+        uuid: '1',
+        fileNumber: '1',
+        dateSubmitted: 0,
+        dateOpened: 0,
+        dateClosed: 0,
+        initialSubmissionType: null,
+        allegedContraventionNarrative: '',
+        allegedActivity: [],
+        intakeNotes: '',
+      };
+      mockComplianceAndEnforcementService.update.mockResolvedValue(resultDto);
+      expect(await controller.update('1', updateDto)).toEqual(resultDto);
+      expect(mockComplianceAndEnforcementService.update).toHaveBeenCalledWith('1', updateDto);
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete an item', async () => {
+      const result = { affected: 1, raw: '' };
+      mockComplianceAndEnforcementService.delete.mockResolvedValue(result);
+      expect(await controller.delete('1')).toEqual(result);
+      expect(mockComplianceAndEnforcementService.delete).toHaveBeenCalledWith('1');
+    });
+  });
+});
---
+++ services/apps/alcs/src/alcs/compliance-and-enforcement/compliance-and-enforcement.service.spec.ts
@@ -0,0 +1,131 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ComplianceAndEnforcementService } from './compliance-and-enforcement.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AllegedActivity, ComplianceAndEnforcement, InitialSubmissionType } from './compliance-and-enforcement.entity';
+import { AutomapperModule } from 'automapper-nestjs';
+import { classes } from 'automapper-classes';
+import { createMock, DeepMocked } from '@golevelup/nestjs-testing';
+import { CONFIG_TOKEN } from '@app/common/config/config.module';
+import * as config from 'config';
+import { ComplianceAndEnforcementDto, UpdateComplianceAndEnforcementDto } from './compliance-and-enforcement.dto';
+import { ComplianceAndEnforcementProfile } from './compliance-and-enforcement.automapper.profile';
+
+const mockComplianceAndEnforcement = new ComplianceAndEnforcement({
+  uuid: '1',
+  fileNumber: '1',
+  dateSubmitted: new Date(0),
+  dateOpened: new Date(0),
+  dateClosed: new Date(0),
+  initialSubmissionType: InitialSubmissionType.COMPLAINT,
+  allegedContraventionNarrative: 'foo',
+  allegedActivity: [AllegedActivity.BREACH_OF_CONDITION],
+  intakeNotes: 'bar',
+});
+
+describe('ComplianceAndEnforcementService', () => {
+  let service: ComplianceAndEnforcementService;
+  let mockComplianceAndEnforcementRepository: DeepMocked<Repository<ComplianceAndEnforcement>>;
+
+  beforeEach(async () => {
+    mockComplianceAndEnforcementRepository = createMock<Repository<ComplianceAndEnforcement>>();
+
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        AutomapperModule.forRoot({
+          strategyInitializer: classes(),
+        }),
+      ],
+      providers: [
+        ComplianceAndEnforcementService,
+        ComplianceAndEnforcementProfile,
+        {
+          provide: getRepositoryToken(ComplianceAndEnforcement),
+          useValue: mockComplianceAndEnforcementRepository,
+        },
+        {
+          provide: CONFIG_TOKEN,
+          useValue: config,
+        },
+      ],
+    }).compile();
+
+    service = module.get<ComplianceAndEnforcementService>(ComplianceAndEnforcementService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('fetchAll', () => {
+    const resultDto: ComplianceAndEnforcementDto = {
+      ...mockComplianceAndEnforcement,
+      dateSubmitted: 0,
+      dateOpened: 0,
+      dateClosed: 0,
+    };
+    it('should return all records', async () => {
+      mockComplianceAndEnforcementRepository.find.mockResolvedValue([mockComplianceAndEnforcement]);
+      expect(await service.fetchAll()).toEqual([resultDto]);
+    });
+  });
+
+  describe('fetchByFileNumber', () => {
+    it('should return a single record by id', async () => {
+      const resultDto: ComplianceAndEnforcementDto = {
+        ...mockComplianceAndEnforcement,
+        dateSubmitted: 0,
+        dateOpened: 0,
+        dateClosed: 0,
+      };
+      mockComplianceAndEnforcementRepository.findOne.mockResolvedValue(mockComplianceAndEnforcement);
+      expect(await service.fetchByFileNumber('1')).toEqual(resultDto);
+    });
+  });
+
+  describe('create', () => {
+    it('should create and save a new record', async () => {
+      const createDto: UpdateComplianceAndEnforcementDto = {};
+      const createEntity = new ComplianceAndEnforcement();
+      const resultDto: ComplianceAndEnforcementDto = {
+        ...mockComplianceAndEnforcement,
+        dateSubmitted: 0,
+        dateOpened: 0,
+        dateClosed: 0,
+      };
+      mockComplianceAndEnforcementRepository.save.mockResolvedValue(mockComplianceAndEnforcement);
+      expect(await service.create(createDto)).toEqual(resultDto);
+      expect(mockComplianceAndEnforcementRepository.save).toHaveBeenCalledWith(createEntity);
+    });
+  });
+
+  describe('update', () => {
+    it('should update and return the updated record', async () => {
+      const updateDto: UpdateComplianceAndEnforcementDto = {};
+      const updateEntity = new ComplianceAndEnforcement({
+        uuid: mockComplianceAndEnforcement.uuid,
+        fileNumber: mockComplianceAndEnforcement.fileNumber,
+      });
+      const resultDto: ComplianceAndEnforcementDto = {
+        ...mockComplianceAndEnforcement,
+        dateSubmitted: 0,
+        dateOpened: 0,
+        dateClosed: 0,
+      };
+      mockComplianceAndEnforcementRepository.save.mockResolvedValue(mockComplianceAndEnforcement);
+      mockComplianceAndEnforcementRepository.findOneBy.mockResolvedValue(mockComplianceAndEnforcement);
+      expect(await service.update('1', updateDto)).toEqual(resultDto);
+      expect(mockComplianceAndEnforcementRepository.save).toHaveBeenCalledWith(updateEntity);
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete a record and return confirmation', async () => {
+      const deleteResult = { affected: 1, raw: '' };
+      mockComplianceAndEnforcementRepository.delete.mockResolvedValue(deleteResult);
+      const result = await service.delete('1');
+      expect(mockComplianceAndEnforcementRepository.delete).toHaveBeenCalledWith('1');
+      expect(result).toEqual(deleteResult);
+    });
+  });
+});
