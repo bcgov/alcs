20ca47d868f1013541f4dd5be6e1d5d9ac11943c
Tristan Slater <1631008+trslater@users.noreply.github.com>
2025-07-24 14:31:41 -0700

    Add section column to C&E docs

--- services/apps/alcs/src/alcs/compliance-and-enforcement/document/document.entity.ts
+++ services/apps/alcs/src/alcs/compliance-and-enforcement/document/document.entity.ts
@@ -1,8 +1,12 @@
 import { AutoMap } from 'automapper-classes';
-import { BaseEntity, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { DocumentCode } from '../../../document/document-code.entity';
 import { Document } from '../../../document/document.entity';
 import { ComplianceAndEnforcement } from '../compliance-and-enforcement.entity';
+
+enum Section {
+  SUBMISSION = 'Submission',
+}
 
 @Entity({
   comment: "Links complaint/referral documents with the complaint/referral they're saved to and logs other attributes",
@@ -28,4 +32,8 @@ export class ComplianceAndEnforcementDocument extends BaseEntity {
   @OneToOne(() => Document, { cascade: true })
   @JoinColumn()
   document: Document;
+
+  @AutoMap()
+  @Column({ type: 'enum', enum: Section })
+  section: Section;
 }
