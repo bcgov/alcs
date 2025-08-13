83b3da3fcb0ce55b3015184fd4581c2e651f5e3e
Tristan Slater <1631008+trslater@users.noreply.github.com>
2025-08-13 15:59:33 -0700

    Make property optional

--- alcs-frontend/src/app/services/compliance-and-enforcement/compliance-and-enforcement.dto.ts
+++ alcs-frontend/src/app/services/compliance-and-enforcement/compliance-and-enforcement.dto.ts
@@ -29,7 +29,7 @@ export interface ComplianceAndEnforcementDto {
   allegedActivity: AllegedActivity[];
   intakeNotes: string;
   submitters: ComplianceAndEnforcementSubmitterDto[];
-  property: ComplianceAndEnforcementPropertyDto;
+  property?: ComplianceAndEnforcementPropertyDto;
 }
 
 export interface UpdateComplianceAndEnforcementDto {
