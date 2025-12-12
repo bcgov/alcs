cde11104ffbf46289cec121a1d8d96cc4779ff5a
Tristan Slater <1631008+trslater@users.noreply.github.com>
2025-12-12 00:12:21 -0800

    Update Angular core/CLI 19 -> 20

--- portal-frontend/src/app/features/applications/edit-submission/other-attachments/other-attachments-upload-dialog/other-attachments-upload-dialog.component.ts
+++ portal-frontend/src/app/features/applications/edit-submission/other-attachments/other-attachments-upload-dialog/other-attachments-upload-dialog.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import {
