de86dff8ab636a3a5ea2aa592fe54d0c120e376c
Tristan Slater <1631008+trslater@users.noreply.github.com>
2025-12-21 18:36:17 -0800

    Fix mock HTTP client for NOI decision service

--- portal-frontend/src/app/services/notice-of-intent-decision/notice-of-intent-decision.service.spec.ts
+++ portal-frontend/src/app/services/notice-of-intent-decision/notice-of-intent-decision.service.spec.ts
@@ -1,5 +1,4 @@
-import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { of, throwError } from 'rxjs';
@@ -16,20 +15,18 @@ describe('NoticeOfIntentDecisionService', () => {
     mockHttpClient = createMock();
 
     TestBed.configureTestingModule({
-    imports: [],
-    providers: [
+      imports: [],
+      providers: [
         {
-            provide: ToastService,
-            useValue: mockToastService,
+          provide: ToastService,
+          useValue: mockToastService,
         },
         {
-            provide: HttpClient,
-            useValue: mockHttpClient,
+          provide: HttpClient,
+          useValue: mockHttpClient,
         },
-        provideHttpClient(withInterceptorsFromDi()),
-        provideHttpClientTesting(),
-    ]
-});
+      ],
+    });
     service = TestBed.inject(NoticeOfIntentDecisionService);
   });
 
@@ -42,18 +39,16 @@ describe('NoticeOfIntentDecisionService', () => {
     mockHttpClient.get.mockReturnValue(of(mockResponse));
 
     const res = await service.getByFileId('fileNumber');
+
     expect(mockHttpClient.get).toHaveBeenCalledTimes(1);
     expect(res).toEqual(mockResponse);
   });
 
   it('should show an error toast if getByFileID fails', async () => {
-    mockHttpClient.get.mockReturnValue(
-      throwError(() => {
-        new Error('');
-      })
-    );
+    mockHttpClient.get.mockReturnValue(throwError(() => new Error('')));
 
     await service.getByFileId('fileNumber');
+
     expect(mockHttpClient.get).toHaveBeenCalledTimes(1);
     expect(mockToastService.showErrorToast).toHaveBeenCalledTimes(1);
   });
@@ -63,18 +58,16 @@ describe('NoticeOfIntentDecisionService', () => {
     mockHttpClient.get.mockReturnValue(of(mockResponse));
 
     const res = await service.openFile('fileNumber');
+
     expect(mockHttpClient.get).toHaveBeenCalledTimes(1);
     expect(res).toEqual(mockResponse);
   });
 
   it('should show an error toast if openFile fails', async () => {
-    mockHttpClient.get.mockReturnValue(
-      throwError(() => {
-        new Error('');
-      })
-    );
+    mockHttpClient.get.mockReturnValue(throwError(() => new Error('')));
 
     await service.openFile('documentId');
+
     expect(mockHttpClient.get).toHaveBeenCalledTimes(1);
     expect(mockToastService.showErrorToast).toHaveBeenCalledTimes(1);
   });
