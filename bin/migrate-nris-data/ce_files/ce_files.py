60eeec505c851070f41b4221fa73e4853ae3fd92
Tristan Slater <1631008+trslater@users.noreply.github.com>
2026-03-05 09:04:19 -0800

    Remove never used user

--- bin/migrate-nris-data/ce_files/ce_files.py
+++ bin/migrate-nris-data/ce_files/ce_files.py
@@ -1,12 +1,11 @@
 from common.etl_logger import setup_and_get_logger
 from config import ABS_PATH
-from db import batch_etl, ensure_ce_users
+from db import batch_etl
 
 
 def ce_file_etl(batch_size):
     logger = setup_and_get_logger("ce-files")
 
-    ensure_ce_users(logger)
     batch_etl(
         logger,
         batch_size,
