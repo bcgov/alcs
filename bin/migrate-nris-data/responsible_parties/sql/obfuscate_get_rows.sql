9aa9191e03ca7b2df98c1604dde75a2e437a73eb
Tristan Slater <1631008+trslater@users.noreply.github.com>
2026-03-09 17:00:22 -0700

    ETL responsible properties

---
+++ bin/migrate-nris-data/responsible_parties/sql/obfuscate_get_rows.sql
@@ -0,0 +1,4 @@
+select
+    acerp.uuid
+from
+    alcs.compliance_and_enforcement_responsible_party acerp;
---
+++ bin/migrate-nris-data/responsible_parties/sql/obfuscate_update.sql
@@ -0,0 +1,12 @@
+update
+    alcs.compliance_and_enforcement_responsible_party as acerp
+set 
+    individual_name = v.individual_name
+from
+    (values %s) as v(
+        uuid,
+        individual_name
+    )
+where
+    acerp.uuid::text = v.uuid::text
+    and acerp.individual_name is not null;
