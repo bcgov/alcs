b2d0b63d512fbbb896c6eef0dddec292531475e9
Tristan Slater <1631008+trslater@users.noreply.github.com>
2026-03-10 11:34:03 -0700

    Fix obfuscation

--- bin/migrate-nris-data/responsible_parties/sql/obfuscate_update.sql
+++ bin/migrate-nris-data/responsible_parties/sql/obfuscate_update.sql
@@ -1,12 +1,14 @@
 update
     alcs.compliance_and_enforcement_responsible_party as acerp
 set 
-    individual_name = v.individual_name
+    individual_name = case
+        when acerp.individual_name is not null or acerp.individual_name <> '' then v.individual_name
+        else acerp.individual_name
+    end
 from
     (values %s) as v(
         uuid,
         individual_name
     )
 where
-    acerp.uuid::text = v.uuid::text
-    and acerp.individual_name is not null;
+    acerp.uuid::text = v.uuid::text;
