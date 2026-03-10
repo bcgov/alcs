32b3e72895cb43fe027ccc602672711b6686d57f
Tristan Slater <1631008+trslater@users.noreply.github.com>
2026-03-10 16:00:38 -0700

    Make property ETL

---
+++ bin/migrate-nris-data/properties/properties.py
@@ -0,0 +1,40 @@
+from common.etl_logger import setup_and_get_logger
+from config import ABS_PATH
+from db import batch_read_write
+from faker import Faker
+
+
+def etl(batch_size):
+    logger = setup_and_get_logger("property-etl")
+
+    batch_read_write(
+        logger,
+        batch_size,
+        ABS_PATH / "properties/sql/count.sql",
+        ABS_PATH / "properties/sql/et.sql",
+        ABS_PATH / "properties/sql/l.sql",
+    )
+
+
+def obfuscate(batch_size):
+    logger = setup_and_get_logger("property-obfuscation")
+
+    batch_read_write(
+        logger,
+        batch_size,  # Obfuscation can be done in smaller batches to reduce transaction size.
+        ABS_PATH / "properties/sql/obfuscate_count.sql",
+        ABS_PATH / "properties/sql/obfuscate_get_rows.sql",
+        ABS_PATH / "properties/sql/obfuscate_update.sql",
+        row_processor=row_obfuscator(),
+    )
+
+
+def row_obfuscator():
+    faker = Faker("la")
+
+    def obfuscate_row(row):
+        row["alc_history"] = "\n\n".join(faker.paragraphs())
+
+        return row
+
+    return obfuscate_row
