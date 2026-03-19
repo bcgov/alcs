702072edacaff5e3aea8bfa841d2988927c0ec6c
Tristan Slater <1631008+trslater@users.noreply.github.com>
2026-03-19 13:15:42 -0700

    Fix: missed req to use max date

--- bin/migrate-nris-data/ce_files/sql/close_count.sql
+++ bin/migrate-nris-data/ce_files/sql/close_count.sql
@@ -1,7 +1,16 @@
+with max_dates as (
+    select
+        ace.uuid,
+        max(ni."date")::timestamptz as date_closed
+    from
+        alcs.compliance_and_enforcement ace 
+        join nris.inspection ni on ni.related_records::text = ace.file_number::text
+    where
+        ni.reason = 'File Closure'
+    group by
+        ace.uuid
+)
 select
     count(*)
 from
-    alcs.compliance_and_enforcement ace 
-    join nris.inspection ni on ni.related_records::text = ace.file_number::text
-where
-    ni.reason = 'File Closure';
+    max_dates;
