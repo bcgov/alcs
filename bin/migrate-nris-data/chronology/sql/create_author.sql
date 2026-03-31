1e7abd847b50de1904b347fe367ad39b7cf4faa0
Tristan Slater <1631008+trslater@users.noreply.github.com>
2026-03-31 12:37:59 -0700

    Add client roles to author

--- bin/migrate-nris-data/chronology/sql/create_author.sql
+++ bin/migrate-nris-data/chronology/sql/create_author.sql
@@ -4,12 +4,16 @@ insert into
         audit_created_by,
         display_name,
         identity_provider,
-        preferred_username
+        preferred_username,
+        name,
+        client_roles
     )
 values (
     %s,
     'nris_etl',
     'NRIS ETL',
     'idir',
-    'nris_etl'
+    'nris_etl',
+    'NRIS ETL',
+    '{C&E}'
 );
