b76b53f5e4a50965355d4745f45962cb9f59bb3c
Tristan Slater <1631008+trslater@users.noreply.github.com>
2026-04-22 19:15:49 -0700

    Make Portal Jest config compatible with Jest 30

--- portal-frontend/jest.config.js
+++ portal-frontend/jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   moduleDirectories: ['node_modules', 'src'],
   modulePathIgnorePatterns: ['/test'],
   coverageReporters: ['lcov'],
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
+  moduleNameMapper: {
+    '\\.(jpg|png|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
 };
